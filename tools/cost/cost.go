a04418d9937fdc72faf03ccea2213865671b0960
Glenn Fiedler <glenn@networknext.com>
2019-12-09 22:47:03 -0500

    add cost tool, stubbed out

---
+++ tools/cost/cost.go
@@ -0,0 +1,33 @@
+/*
+   Network Next. You control the network.
+   Copyright © 2017 - 2020 Network Next, Inc. All rights reserved.
+*/
+
+package main
+
+import (
+	"fmt"
+	// "io/ioutil"
+	// "os"
+)
+
+func main() {
+
+	fmt.Printf("\nWelcome to Network Next!\n\n")
+
+	/*
+	common.InitEnvironment()
+
+	common.UpdateEnvironment()
+
+	responseData, err := common.GetHttpAuth(common.MasterConfig.Address + "/admin/api/cost_matrix")
+	if err != nil {
+		fmt.Printf("error: failed to get cost matrix: %v\n", err)
+		os.Exit(1)
+	}
+
+	ioutil.WriteFile("cost.bin", responseData, 0644)
+
+	fmt.Printf("Wrote cost matrix to 'cost.bin'\n\n")
+	*/
+}
