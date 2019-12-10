997a3e723f6cbf42e5e278a7580c71e1e6f85318
Glenn Fiedler <glenn@networknext.com>
2019-12-10 11:32:26 -0500

    add functional client and server naming, so they sit properly in tools dir

--- tools/debug/debug.go
+++ tools/debug/debug.go
@@ -1,6 +1,6 @@
 /*
    Network Next. You control the network.
-   Copyright © 2017 - 2019 Network Next, Inc. All rights reserved.
+   Copyright © 2017 - 2020 Network Next, Inc. All rights reserved.
 */
 
 package main
