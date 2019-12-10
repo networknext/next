7e513dd84f40bddc43e28477a48d829d872dc821
Blain Smith <rebelgeek@blainsmith.com>
2019-12-09 19:35:17 -0500

    remade main go files since they got lost in translation

---
+++ cmd/relay_backend/relay_backend.go
@@ -0,0 +1,14 @@
+/*
+   Network Next. You control the network.
+   Copyright © 2017 - 2020 Network Next, Inc. All rights reserved.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("\nHello, relay_backend world!\n\n")
+}
