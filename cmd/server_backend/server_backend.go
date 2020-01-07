df15b412a0d79ffc1c4d8e6995531d6a340fd017
Blain Smith <rebelgeek@blainsmith.com>
2020-01-07 15:46:59 -0500

    move udp server mux into proper service binary

--- cmd/server_backend/server_backend.go
+++ cmd/server_backend/server_backend.go
@@ -6,9 +6,49 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"strconv"
+
+	"github.com/networknext/backend/transport"
 )
 
 func main() {
-	fmt.Printf("\nHello, server_backend world!\n\n")
+	var err error
+
+	var port int64
+	if port, err = strconv.ParseInt(os.Getenv("SERVER_BACKEND_PORT"), 10, 64); err != nil {
+		port = 30000
+		log.Printf("unable to parse port %s, defauling to 30000\n", os.Getenv("SERVER_BACKEND_PORT"))
+	}
+
+	{
+		addr := net.UDPAddr{
+			Port: int(port),
+			IP:   net.ParseIP("0.0.0.0"),
+		}
+
+		conn, err := net.ListenUDP("udp", &addr)
+		if err != nil {
+			log.Printf("error: could not listen on %s\n", addr.String())
+		}
+
+		mux := transport.UDPServerMux{
+			Conn:          conn,
+			MaxPacketSize: transport.DefaultMaxPacketSize,
+
+			ServerUpdateHandlerFunc:  transport.ServerUpdateHandlerFunc,
+			SessionUpdateHandlerFunc: transport.SessionUpdateHandlerFunc,
+		}
+
+		if err := mux.Start(); err != nil {
+			log.Println(err)
+		}
+	}
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+	<-sigint
 }
--- transport/server_test.go
+++ transport/server_test.go
@@ -1,21 +1,21 @@
 package transport_test
 
 import (
-	"bytes"
-	"github.com/networknext/backend/transport"
 	"net"
 	"testing"
+
+	"github.com/networknext/backend/transport"
 )
 
 func TestServerUpdateHandlerFunc(t *testing.T) {
-	packet := bytes.NewBuffer(nil)
+	packet := make([]byte, 0)
 	addr, _ := net.ResolveUDPAddr("udp", ":30000")
 
 	transport.ServerUpdateHandlerFunc(packet, addr)
 }
 
 func TestSessionUpdateHandlerFunc(t *testing.T) {
-	packet := bytes.NewBuffer(nil)
+	packet := make([]byte, 0)
 	addr, _ := net.ResolveUDPAddr("udp", ":30000")
 
 	transport.ServerUpdateHandlerFunc(packet, addr)
