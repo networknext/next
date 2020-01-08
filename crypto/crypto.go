36e0ed4e93098fcf5a0a7b2b8410759115c3c699
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-01-08 16:29:12 -0500

    minor touchup

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -8,12 +8,12 @@ import (
 	"fmt"
 )
 
-// Check TODO
+// Check checks encyption of the packet
 func Check(data []byte, nonce []byte, publicKey []byte, privateKey []byte) bool {
 	return C.crypto_box_open((*C.uchar)(&data[0]), (*C.uchar)(&data[0]), C.ulonglong(len(data)), (*C.uchar)(&nonce[0]), (*C.uchar)(&publicKey[0]), (*C.uchar)(&privateKey[0])) != 0
 }
 
-// CompareTokens TODO
+// CompareTokens compares two byte arrays (doesn't necessarily belong here)
 func CompareTokens(a []byte, b []byte) bool {
 	if len(a) != len(b) {
 		fmt.Printf("token length is wrong\n")
--- relayport/relay_ping_stats_json.go
+++
@@ -1,9 +0,0 @@
-package relayport
-
-// RelayPingStatsJSON is json for ping stats
-type RelayPingStatsJSON struct {
-	RelayID    uint64
-	RTT        float32
-	Jitter     float32
-	PacketLoss float32
-}
