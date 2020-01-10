a052f5faab046522e46e850aa870ff9f40ab5e1a
Glenn Fiedler <glenn@networknext.com>
2020-01-10 17:58:25 -0500

    continue moving stuff out of core, within reason

--- core/core.go
+++ core/core.go
@@ -5,7 +5,6 @@ package core
 import (
 	"crypto/sha256"
 	"encoding/base64"
-	"encoding/binary"
 	"fmt"
 	"hash/fnv"
 	"math"
@@ -117,68 +116,6 @@ func GetDatacenterId(id *EntityId) (DatacenterId, error) {
 
 // ============================================================================
 
-// todo: these read/write versions are old and we should not use them. the new ones from the func_backend.go are much better (with index) -- glenn
-
-func WriteString(buffer []byte, value string) int {
-	binary.LittleEndian.PutUint32(buffer, uint32(len(value)))
-	copy(buffer[4:], []byte(value))
-	return 4 + len([]byte(value))
-}
-
-func ReadString(buffer []byte) (string, int) {
-	stringLength := binary.LittleEndian.Uint32(buffer)
-	stringData := make([]byte, stringLength)
-	copy(stringData, buffer[4:4+stringLength])
-	return string(stringData), int(4 + stringLength)
-}
-
-func WriteBytes(buffer []byte, value []byte) int {
-	binary.LittleEndian.PutUint32(buffer, uint32(len(value)))
-	copy(buffer[4:], value)
-	return 4 + len(value)
-}
-
-func ReadBytes(buffer []byte) ([]byte, int) {
-	length := binary.LittleEndian.Uint32(buffer)
-	data := make([]byte, length)
-	copy(data, buffer[4:4+length])
-	return data, int(4 + length)
-}
-
-func WriteAddress(buffer []byte, address *net.UDPAddr) {
-	if address == nil {
-		buffer[0] = IPAddressNone
-		return
-	}
-	ipv4 := address.IP.To4()
-	port := address.Port
-	if ipv4 != nil {
-		buffer[0] = IPAddressIPv4
-		buffer[1] = ipv4[0]
-		buffer[2] = ipv4[1]
-		buffer[3] = ipv4[2]
-		buffer[4] = ipv4[3]
-		buffer[5] = (byte)(port & 0xFF)
-		buffer[6] = (byte)(port >> 8)
-	} else {
-		buffer[0] = IPAddressIPv6
-		copy(buffer[1:], address.IP)
-		buffer[17] = (byte)(port & 0xFF)
-		buffer[18] = (byte)(port >> 8)
-	}
-}
-
-func ReadAddress(buffer []byte) *net.UDPAddr {
-	addressType := buffer[0]
-	switch addressType {
-	case IPAddressIPv4:
-		return &net.UDPAddr{IP: net.IPv4(buffer[1], buffer[2], buffer[3], buffer[4]), Port: ((int)(binary.LittleEndian.Uint16(buffer[5:])))}
-	case IPAddressIPv6:
-		return &net.UDPAddr{IP: buffer[1:], Port: ((int)(binary.LittleEndian.Uint16(buffer[17:])))}
-	}
-	return nil
-}
-
 func ParseAddress(input string) *net.UDPAddr {
 	address := &net.UDPAddr{}
 	ip_string, port_string, err := net.SplitHostPort(input)
@@ -221,8 +158,6 @@ func Checksum(data []byte) []byte {
 	hasher.Write(data)
 	return hasher.Sum(nil)
 }
-
-// =====================================================================================================
 
 func HaversineDistance(lat1 float64, long1 float64, lat2 float64, long2 float64) float64 {
 	lat1 *= math.Pi / 180
