4002e67def0c50c051ee8639c5c650d382fd224f
Blain Smith <rebelgeek@blainsmith.com>
2020-01-15 12:03:22 -0500

    test the generate funcs

---
+++ crypto/crypto_test.go
@@ -0,0 +1,29 @@
+package crypto_test
+
+import (
+	"testing"
+
+	"github.com/networknext/backend/crypto"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerate(t *testing.T) {
+	t.Run("SessionID", func(t *testing.T) {
+		id := crypto.GenerateSessionID()
+		assert.NotZero(t, id)
+	})
+
+	t.Run("RelayKeyPair", func(t *testing.T) {
+		pub, priv, err := crypto.GenerateRelayKeyPair()
+		assert.NoError(t, err)
+		assert.Equal(t, crypto.PublicKeySize, len(pub))
+		assert.Equal(t, crypto.PrivateKeySize, len(priv))
+	})
+
+	t.Run("CustomerKeyPair", func(t *testing.T) {
+		pub, priv, err := crypto.GenerateCustomerKeyPair()
+		assert.NoError(t, err)
+		assert.Equal(t, crypto.CustomerPublicKeySize, len(pub))
+		assert.Equal(t, crypto.CustomerPrivateKeySize, len(priv))
+	})
+}
