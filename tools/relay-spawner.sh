487c07adb662c90ce65a99d78877a9b448a607c0
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-01-27 14:06:50 -0500

    made the relay spawner scirpt much better
    ... and functional

--- tools/relay-spawner.sh
+++ tools/relay-spawner.sh
@@ -6,37 +6,70 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 fi
 
 kill_relays=0
+flush_redis=0
+use_relay_count=0
 
-print_help() {
-    echo "Usage: relay-spawner.sh [-k] [-s port] [starting port] [ending port]"
-    printf "k\tKill all relays that were spawned using this script\n"
-    printf "h\tPrint this help menu\n"
-    return 0
-}
-
-while getopts "kh" opt
-do
-    case $opt in
-	(k) ((single_relay)) && printf "Cannot use -s with -k\n" && exit 1; kill_relays=1; shift ;;
-	(h) print_help && exit 0 ;;
-	(*) printf "Bad option '%s'\n" "$opt" && exit 1 ;;
+# because it must be sourced, reset OPTIND to 1
+OPTIND=1
+while getopts ":n:khr" opt; do
+    case "$opt" in
+	(n)
+	    use_relay_count=1
+	    relay_count="$OPTARG"
+	    ;;
+	(k) 
+	    kill_relays=1
+	    ;;
+	(h) 
+	    echo "Usage: relay-spawner.sh [-k] [-s port] [starting port] [ending port]"
+	    printf "k\tKill all relays that were spawned using this script\n"
+	    printf "h\tPrint this help menu\n"
+	    printf "r\tReset redis before any other operation\n"
+	    return 0
+	    ;;
+	(r) 
+	    flush_redis=1
+	    ;;
+	(\?) 
+	    echo "Bad option '$OPTARG'" && return 1 
+	    ;;
+	(:)
+	    echo "Bad param: '$OPTARG'" && return 1
+	    ;;
     esac
 done
 
-start_relay() {
-    port="$1"
-    RELAY_ADDRESS="127.0.0.1:$port" make dev-relay-v2 > /dev/null &
-    pid="$!"
-    export RUNNING_RELAYS="$RUNNING_RELAYS:$pid"
-    echo "Started Relay on port $port with pid: $pid"
-}
+shift $(( OPTIND - 1 ))
 
-start_relays() {
+if [[ "$flush_redis" -eq 1 ]]; then
+    echo "Clearing Redis: $(redis-cli FLUSHALL)"
+fi
+
+if [[ "$kill_relays" == 1 ]]; then
+    IFS=':' read -ra RELAYS <<< "$RUNNING_RELAYS"
+    for relay in ${RELAYS[@]}; do
+	kill "$relay" > /dev/null
+	echo "killed $relay"
+    done
+
+    export RUNNING_RELAYS=""
+else
+    if [[ "$#" == 0 ]]; then
+	if [[ "$flush_redis" == 0 ]]; then
+	    echo "You must supply a port number"
+	    return 1
+	else
+	    return 0
+	fi
+    fi
+
     begin_port="$1"
     end_port="$2"
 
-    # enable the option to just spawn a single relay
-    if [ -z "$end_port" ]; then
+    if [[ "$use_relay_count" == 1 ]]; then
+	end_port="$relay_count"
+    elif [ -z "$end_port" ]; then
+	# enable the option to just spawn a single relay
 	end_port="$begin_port"
     fi
 
@@ -47,24 +80,11 @@ start_relays() {
 
     echo "Spawning $(( end_port - begin_port + 1 )) relays between $begin_port and $end_port"
 
-    for ((port = $(($begin_port)) ; port <= $(($end_port)) ; port++ )); do
-	start_relay "$port"
+    for ((port=${begin_port} ; port<=${end_port} ; port++)); do
+	RELAY_ADDRESS="127.0.0.1:$port" make dev-relay-v2 > /dev/null &
+	pid="$!"
+	export RUNNING_RELAYS="$RUNNING_RELAYS:$pid"
+	echo "Started Relay on port $port with pid: $pid"
     done
-}
-
-kill_relays() {
-    IFS=':' read -ra RELAYS <<< "$RUNNING_RELAYS"
-    for relay in ${RELAYS[@]}; do
-        kill "$relay" > /dev/null
-        echo "killed $relay"
-    done
-
-    export RUNNING_RELAYS=""
-}
-
-if [[ "$kill_relays" -eq 1 ]]; then
-    kill_relays
-else
-    start_relays "$1" "$2"
 fi
 
