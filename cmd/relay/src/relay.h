42d68b10d478a3835527c94ea86cc4529bdf2bbe
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-01-28 16:27:51 -0500

    refactored a bunch more

--- cmd/relay/src/relay.h
+++ cmd/relay/src/relay.h
@@ -5,17 +5,10 @@
 
 #include <stdint.h>
 
+#include "sysinfo.hpp"
+
 #ifndef RELAY_H
 #define RELAY_H
-
-#define RELAY_OK 0
-#define RELAY_ERROR -1
-
-#define RELAY_ADDRESS_NONE 0
-#define RELAY_ADDRESS_IPV4 1
-#define RELAY_ADDRESS_IPV6 2
-
-#define RELAY_MAX_ADDRESS_STRING_LENGTH 256
 
 #if RELAY_PLATFORM == RELAY_PLATFORM_WINDOWS
 #include "relay_windows.h"
@@ -25,22 +18,7 @@
 #include "relay_linux.h"
 #endif
 
-struct relay_address_t
-{
-    union
-    {
-        uint8_t ipv4[4];
-        uint16_t ipv6[8];
-    } data;
-    uint16_t port;
-    uint8_t type;
-};
+#include "relay/relay_address.hpp"
 
 void relay_printf(const char* format, ...);
-
-int relay_address_parse(relay_address_t* address, const char* address_string_in);
-
-const char* relay_address_to_string(const relay_address_t* address, char* buffer);
-
-int relay_address_equal(const relay_address_t* a, const relay_address_t* b);
 #endif  // #ifndef RELAY_H
