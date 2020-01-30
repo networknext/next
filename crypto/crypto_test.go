d691da50f4135532c75e210fd533d4b9bfba77c0
Blain Smith <rebelgeek@blainsmith.com>
2020-01-30 10:20:42 -0500

    sanity check tests to verify assumptions that i am not crazy

--- crypto/crypto_test.go
+++ crypto/crypto_test.go
@@ -3,12 +3,24 @@ package crypto_test
 import (
 	"crypto/ed25519"
 	"crypto/rand"
+	"encoding/base64"
 	"testing"
 
 	"github.com/networknext/backend/crypto"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/crypto/nacl/box"
 )
+
+func TestSignVerify(t *testing.T) {
+	publicKey, _ := base64.StdEncoding.DecodeString("leN7D7+9vr24uT4f1Ba8PEEvIQA/UkGZLlT+sdeLRHKsVqaZq723Zw==")
+	privateKey, _ := base64.StdEncoding.DecodeString("leN7D7+9vr3TEZexVmvbYzdH1hbpwBvioc6y1c9Dhwr4ZaTkEWyX2Li5Ph/UFrw8QS8hAD9SQZkuVP6x14tEcqxWppmrvbdn")
+
+	msg := []byte("just some data to sign")
+
+	// We need to offset the keys by 8 bytes since the first 8 bytes is the CustomerID
+	sig := ed25519.Sign(privateKey[8:], msg)
+	assert.True(t, ed25519.Verify(publicKey[8:], msg, sig))
+}
 
 func TestOpenSeal(t *testing.T) {
 	// Generate a sender keypair
