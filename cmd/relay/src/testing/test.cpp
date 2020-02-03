71298e3540ca6e39719049338ab97f469a01713b
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-02-03 15:01:40 -0500

    reset a test back to where it was

--- cmd/relay/src/testing/test.cpp
+++ cmd/relay/src/testing/test.cpp
@@ -701,6 +701,37 @@ namespace testing
         check(memcmp(g, "hello", 6) == 0);
     }
 
+    void test_address_read_and_write()
+    {
+        legacy::relay_address_t a, b, c;
+
+        memset(&a, 0, sizeof(a));
+
+        legacy::relay_address_parse(&b, "127.0.0.1:50000");
+
+        legacy::relay_address_parse(&c, "[::1]:50000");
+
+        uint8_t buffer[1024];
+
+        uint8_t* p = buffer;
+
+        encoding::write_address(&p, &a);
+        encoding::write_address(&p, &b);
+        encoding::write_address(&p, &c);
+
+        struct legacy::relay_address_t read_a, read_b, read_c;
+
+        const uint8_t* q = buffer;
+
+        encoding::read_address(&q, &read_a);
+        encoding::read_address(&q, &read_b);
+        encoding::read_address(&q, &read_c);
+
+        check(legacy::relay_address_equal(&a, &read_a));
+        check(legacy::relay_address_equal(&b, &read_b));
+        check(legacy::relay_address_equal(&c, &read_c));
+    }
+
     static void test_platform_socket()
     {
         // non-blocking socket (ipv4)
@@ -1204,37 +1235,6 @@ namespace testing
         }
 
         relay_manager_destroy(manager);
-    }
-
-    void test_address_read_and_write()
-    {
-        legacy::relay_address_t a, b, c;
-
-        memset(&a, 0, sizeof(a));
-
-        legacy::relay_address_parse(&b, "127.0.0.1:50000");
-
-        legacy::relay_address_parse(&c, "[::1]:50000");
-
-        uint8_t buffer[1024];
-
-        uint8_t* p = buffer;
-
-        encoding::write_address(&p, &a);
-        encoding::write_address(&p, &b);
-        encoding::write_address(&p, &c);
-
-        struct legacy::relay_address_t read_a, read_b, read_c;
-
-        const uint8_t* q = buffer;
-
-        encoding::read_address(&q, &read_a);
-        encoding::read_address(&q, &read_b);
-        encoding::read_address(&q, &read_c);
-
-        check(legacy::relay_address_equal(&a, &read_a));
-        check(legacy::relay_address_equal(&b, &read_b));
-        check(legacy::relay_address_equal(&c, &read_c));
     }
 
     void relay_test()
