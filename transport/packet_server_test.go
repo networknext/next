87fa7ddea75d6d854f9a5d0955617ef6b403f4ec
Blain Smith <rebelgeek@blainsmith.com>
2020-02-13 08:33:09 -0500

    change tests to work with the packets internal version

--- transport/packet_server_test.go
+++ transport/packet_server_test.go
@@ -7,7 +7,6 @@ import (
 	"testing"
 
 	"github.com/networknext/backend/crypto"
-	"github.com/networknext/backend/encoding"
 	"github.com/networknext/backend/transport"
 	"github.com/stretchr/testify/assert"
 )
@@ -20,9 +19,6 @@ func TestServerUpdatePacket(t *testing.T) {
 		// Create a ServerUpdatePacket like the game server does
 		outgoing := transport.ServerUpdatePacket{
 			Sequence:             1,
-			VersionMajor:         1,
-			VersionMinor:         2,
-			VersionPatch:         3,
 			CustomerId:           4,
 			DatacenterId:         5,
 			NumSessionsPending:   6,
@@ -30,6 +26,8 @@ func TestServerUpdatePacket(t *testing.T) {
 			ServerAddress:        net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 2323},
 			ServerPrivateAddress: net.UDPAddr{IP: net.ParseIP("10.0.0.1"), Port: 2323},
 			ServerRoutePublicKey: make([]byte, crypto.KeySize),
+
+			Version: transport.SDKVersion{1, 2, 3},
 		}
 
 		// Sign the packet and set it to the signature
@@ -58,9 +56,6 @@ func TestServerUpdatePacket(t *testing.T) {
 		// Create a ServerUpdatePacket like the game server does
 		outgoing := transport.ServerUpdatePacket{
 			Sequence:             1,
-			VersionMajor:         1,
-			VersionMinor:         2,
-			VersionPatch:         3,
 			CustomerId:           4,
 			DatacenterId:         5,
 			NumSessionsPending:   6,
@@ -68,6 +63,8 @@ func TestServerUpdatePacket(t *testing.T) {
 			ServerAddress:        net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 2323},
 			ServerPrivateAddress: net.UDPAddr{IP: net.ParseIP("10.0.0.1"), Port: 2323},
 			ServerRoutePublicKey: make([]byte, crypto.KeySize),
+
+			Version: transport.SDKVersion{1, 2, 3},
 		}
 
 		// Sign the packet and set it to the signature
@@ -97,9 +94,6 @@ func TestServerUpdatePacket(t *testing.T) {
 		// Create a ServerUpdatePacket like the game server does
 		outgoing := transport.ServerUpdatePacket{
 			Sequence:             1,
-			VersionMajor:         1,
-			VersionMinor:         2,
-			VersionPatch:         3,
 			CustomerId:           4,
 			DatacenterId:         5,
 			NumSessionsPending:   6,
@@ -107,6 +101,8 @@ func TestServerUpdatePacket(t *testing.T) {
 			ServerAddress:        net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 2323},
 			ServerPrivateAddress: net.UDPAddr{IP: net.ParseIP("10.0.0.1"), Port: 2323},
 			ServerRoutePublicKey: make([]byte, crypto.KeySize),
+
+			Version: transport.SDKVersion{1, 2, 3},
 		}
 
 		// Sign the packet and set it to the signature
@@ -130,38 +126,15 @@ func TestServerUpdatePacket(t *testing.T) {
 	})
 }
 
-// Added as temp solution to test since "func (packet *SessionUpdatePacket) MarshalBinary()" currently forces MinSDKVersion
-// Will need to figure out how we want to determine SDK version of session update packets properly and replace this with regular Marshal call
-func MarshalSessionUpdatePacket(packet *transport.SessionUpdatePacket, version transport.SDKVersion) ([]byte, error) {
-	ws, err := encoding.CreateWriteStream(transport.DefaultMaxPacketSize)
-	if err != nil {
-		return nil, err
-	}
+func TestSessionUpdatePacket(t *testing.T) {
 
-	if err := packet.Serialize(ws, version); err != nil {
-		return nil, err
-	}
-	ws.Flush()
+	customerPublicKey, _ := base64.StdEncoding.DecodeString("leN7D7+9vr24uT4f1Ba8PEEvIQA/UkGZLlT+sdeLRHKsVqaZq723Zw==")
+	customerPrivateKey, _ := base64.StdEncoding.DecodeString("leN7D7+9vr3TEZexVmvbYzdH1hbpwBvioc6y1c9Dhwr4ZaTkEWyX2Li5Ph/UFrw8QS8hAD9SQZkuVP6x14tEcqxWppmrvbdn")
 
-	return ws.GetData(), nil
-}
+	SessionUpdatePackets := []transport.SessionUpdatePacket{
+		{
+			Version: transport.SDKVersion{3, 3, 2},
 
-// Added as temp solution to test since "func (packet *SessionUpdatePacket) UnmarshalBinary()" currently forces MinSDKVersion
-// Will need to figure out how we want to determine SDK version of session update packets properly and replace this with regular Marshal call
-func UnmarshalSessionUpdatePacket(data []byte, version transport.SDKVersion) (*transport.SessionUpdatePacket, error) {
-	packet := &transport.SessionUpdatePacket{}
-	if err := packet.Serialize(encoding.CreateReadStream(data), version); err != nil {
-		return nil, err
-	}
-	return packet, nil
-}
-
-var SessionUpdatePackets = []struct {
-	version transport.SDKVersion
-	packet  transport.SessionUpdatePacket
-}{
-	{
-		transport.SDKVersion{3, 3, 2}, transport.SessionUpdatePacket{
 			Sequence:   1,
 			CustomerId: 2,
 			SessionId:  3,
@@ -200,9 +173,9 @@ var SessionUpdatePackets = []struct {
 			PacketsLostClientToServer: 12,
 			PacketsLostServerToClient: 13,
 		},
-	},
-	{
-		transport.SDKVersion{3, 3, 3}, transport.SessionUpdatePacket{
+		{
+			Version: transport.SDKVersion{3, 3, 3},
+
 			Sequence:   1,
 			CustomerId: 2,
 			SessionId:  3,
@@ -241,9 +214,9 @@ var SessionUpdatePackets = []struct {
 			PacketsLostClientToServer: 12,
 			PacketsLostServerToClient: 13,
 		},
-	},
-	{
-		transport.SDKVersion{3, 3, 4}, transport.SessionUpdatePacket{
+		{
+			Version: transport.SDKVersion{3, 3, 4},
+
 			Sequence:         1,
 			CustomerId:       2,
 			SessionId:        3,
@@ -282,9 +255,9 @@ var SessionUpdatePackets = []struct {
 			PacketsLostClientToServer: 12,
 			PacketsLostServerToClient: 13,
 		},
-	},
-	{
-		transport.SDKVersion{3, 4, 0}, transport.SessionUpdatePacket{
+		{
+			Version: transport.SDKVersion{3, 4, 0},
+
 			Sequence:         1,
 			CustomerId:       2,
 			SessionId:        3,
@@ -323,37 +296,31 @@ var SessionUpdatePackets = []struct {
 			PacketsLostClientToServer: 12,
 			PacketsLostServerToClient: 13,
 		},
-	},
-}
+	}
 
-func TestSessionUpdatePacket(t *testing.T) {
-
-	customerPublicKey, _ := base64.StdEncoding.DecodeString("leN7D7+9vr24uT4f1Ba8PEEvIQA/UkGZLlT+sdeLRHKsVqaZq723Zw==")
-	customerPrivateKey, _ := base64.StdEncoding.DecodeString("leN7D7+9vr3TEZexVmvbYzdH1hbpwBvioc6y1c9Dhwr4ZaTkEWyX2Li5Ph/UFrw8QS8hAD9SQZkuVP6x14tEcqxWppmrvbdn")
-
-	for _, test := range SessionUpdatePackets {
-		t.Run(test.version.String(), func(t *testing.T) {
-			// Create a SessionUpdatePacket like the game server does
-			outgoing := test.packet
-
+	for _, packet := range SessionUpdatePackets {
+		t.Run(packet.Version.String(), func(t *testing.T) {
 			// Sign the packet
-			outgoing.Signature = crypto.Sign(customerPrivateKey[8:], outgoing.GetSignData(test.version))
+			packet.Signature = crypto.Sign(customerPrivateKey[8:], packet.GetSignData())
 
 			// Marshal the whole packet to binary to send it over the network
-			data, err := MarshalSessionUpdatePacket(&outgoing, test.version)
+			data, err := packet.MarshalBinary()
 			assert.NoError(t, err)
 
 			// Unmarshal the data from binary like the server backend receives it
-			incoming, err := UnmarshalSessionUpdatePacket(data, test.version)
+			var newpacket transport.SessionUpdatePacket
+			newpacket.Version = packet.Version
+
+			err = newpacket.UnmarshalBinary(data)
 			assert.NoError(t, err)
 
 			// Verify the incoming packet's signed data with the signature
 			// with the customer's public key we would get from configstore
-			verified := crypto.Verify(customerPublicKey[8:], incoming.GetSignData(test.version), incoming.Signature)
+			verified := crypto.Verify(customerPublicKey[8:], newpacket.GetSignData(), newpacket.Signature)
 			assert.True(t, verified)
 
 			// Make sure the data was preserved during serialization and deserialization
-			assert.EqualValues(t, outgoing, *incoming)
+			assert.EqualValues(t, packet, newpacket)
 		})
 	}
 }
