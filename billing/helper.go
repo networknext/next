0788a99b6ba8ebd272c10b1acfc8de0dc193d560
Roman Maksymyschyn <roman@networknext.com>
2020-02-25 17:22:45 -0500

    Start stubbing out helper function to build a billing entry, mostly there except for near relays now

--- billing/helper.go
+++ billing/helper.go
@@ -1,6 +1,11 @@
 package billing
 
-import "net"
+import (
+	"net"
+
+	"github.com/networknext/backend/routing"
+	"github.com/networknext/backend/transport"
+)
 
 func udpAddrToAddress(addr net.UDPAddr) *Address {
 	if addr.IP == nil {
@@ -37,5 +42,63 @@ func udpAddrToAddress(addr net.UDPAddr) *Address {
 		Type:      Address_IPV4,
 		Port:      uint32(addr.Port),
 		Formatted: addr.String(),
+	}
+}
+
+func makeEntityID(kind string, ID uint64) EntityId{
+	return EntityId{
+		Kind: kind,
+		Name: strconv.Itoa(ID)
+	}
+}
+
+// Convert new representation of data into old for billing entry
+func BuildRouteRequest(updatePacket transport.SessionUpdatePacket, buyer routing.Buyer, serverData transport.ServerCacheEntry, location routing.Location) RouteRequest {
+	return RouteRequest{
+		BuyerId:                   makeEntityID("Buyer", buyer.Key),
+		SessionId:                 updatePacket.SessionId,
+		UserHash:                  updatePacket.UserHash,
+		PlatformId:                updatePacket.PlatformId,
+		DirectRtt:                 updatePacket.DirectMinRtt,
+		DirectJitter:              updatePacket.DirectJitter,
+		DirectPacketLoss:          updatePacket.DirectPacketLoss,
+		NextRtt:                   updatePacket.NextMinRtt,
+		NextJitter:                updatePacket.NextJitter,
+		NextPacketLoss:            updatePacket.NextPacketLoss,
+		ClientIpAddress:           udpAddrToAddress(updatePacket.ClientAddress),
+		ServerIpAddress:           udpAddrToAddress(updatePacket.ServerAddress),
+		ServerPrivateIpAddress:    udpAddrToAddress(serverData.Server.Addr),
+		ClientRoutePublicKey:      updatePacket.ClientRoutePublicKey,
+		ServerRoutePublicKey:      serverData.Server.PublicKey,
+		Tag:                       updatePacket.Tag,
+		NearRelays:                nearRelays,
+		IssuedNearRelays:          issuedNearRelays,
+		ConnectionType:            SessionConnectionType(updatePacket.ConnectionType),
+		DatacenterId:              makeEntityID("Datacenter", serverData.Datacenter.ID),
+		SequenceNumber:            updatePacket.Sequence,
+		FallbackToDirect:          updatePacket.FallbackToDirect,
+		VersionMajor:              serverData.VersionMajor,
+		VersionMinor:              serverData.VersionMinor,
+		VersionPatch:              serverData.VersionPatch,
+		Location:                  Location{
+			// CountryCode: location.CountryCode,
+			Country: location.Country,
+			Region: location.Region,
+			City: location.City,
+			Latitude: location.Latitude,
+			Longitude: location.Longitude,
+			// Isp: location.ISP,
+			// Asn: location.Asn,
+			Continent: location.Continent,
+		},
+		UsageKbpsUp:               updatePacket.KbpsUp,
+		UsageKbpsDown:             updatePacket.KbpsDown,
+		Flagged:                   updatePacket.Flagged,
+		TryBeforeYouBuy:           updatePacket.TryBeforeYouBuy,
+		OnNetworkNext:             updatePacket.OnNetworkNext,
+		PacketsLostClientToServer: updatePacket.PacketsLostClientToServer,
+		PacketsLostServerToClient: updatePacket.PacketsLostServerToClient,
+		FallbackFlags:             updatePacket.Flags,
+		Committed:                 updatePacket.Committed,
 	}
 }
