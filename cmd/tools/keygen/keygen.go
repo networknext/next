1dd5395c6b982b4ddf407686ea27b9c8a43c4ec1
ryanj <ryan@networknext.com>
2020-03-06 16:39:31 -0500

    keygen now uses crypto function directly builds only for OS you're using

--- cmd/tools/keygen/keygen.go
+++ cmd/tools/keygen/keygen.go
@@ -6,17 +6,17 @@
 package main
 
 import (
-	"crypto/ed25519"
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
 	"log"
 
+	"github.com/networknext/backend/crypto"
 	"golang.org/x/crypto/nacl/box"
 )
 
 func main() {
-	signaturePublicKey, signaturePrivateKey, err := generateCustomerKeyPair()
+	signaturePublicKey, signaturePrivateKey, err := crypto.GenerateCustomerKeyPair()
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -29,37 +29,16 @@ func main() {
 	fmt.Printf("\nWelcome to Network Next!\n\n")
 
 	fmt.Printf("This is your public key for packet signature verification:\n\n")
-	fmt.Printf("    %s\n", base64.StdEncoding.EncodeToString(signaturePublicKey))
+	fmt.Printf("    %s\n\n", base64.StdEncoding.EncodeToString(signaturePublicKey))
 
 	fmt.Printf("This is your private key for packet signature verification:\n\n")
-	fmt.Printf("    %s\n", base64.StdEncoding.EncodeToString(signaturePrivateKey))
+	fmt.Printf("    %s\n\n", base64.StdEncoding.EncodeToString(signaturePrivateKey))
 
-	fmt.Printf("This is your public key for token encryption:\n\n")
-	fmt.Printf("    %s\n", base64.StdEncoding.EncodeToString(encryptionPublicKey[:]))
+	fmt.Printf("This is your public key for route token encryption:\n\n")
+	fmt.Printf("    %s\n\n", base64.StdEncoding.EncodeToString(encryptionPublicKey[:]))
 
-	fmt.Printf("This is your private key for token encryption:\n\n")
-	fmt.Printf("    %s\n", base64.StdEncoding.EncodeToString(encryptionPrivateKey[:]))
+	fmt.Printf("This is your private key for route token encryption:\n\n")
+	fmt.Printf("    %s\n\n", base64.StdEncoding.EncodeToString(encryptionPrivateKey[:]))
 
 	fmt.Printf("IMPORTANT: Save your private keys in a secure place and don't share them with anybody, not even us!\n\n")
-}
-
-// generateRelayKeyPair creates a public and private keypair using crypto/ed25519 and prepends a random 8 byte customer ID
-// This is copied from crypto to avoid libsodium dependency so that keygen can be built for multiple platforms.
-func generateCustomerKeyPair() ([]byte, []byte, error) {
-	customerID := make([]byte, 8)
-	rand.Read(customerID)
-
-	publicKey, privateKey, err := ed25519.GenerateKey(nil)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	customerPublicKey := make([]byte, 0)
-	customerPublicKey = append(customerPublicKey, customerID...)
-	customerPublicKey = append(customerPublicKey, publicKey...)
-	customerPrivateKey := make([]byte, 0)
-	customerPrivateKey = append(customerPrivateKey, customerID...)
-	customerPrivateKey = append(customerPrivateKey, privateKey...)
-
-	return customerPublicKey, customerPrivateKey, nil
 }
