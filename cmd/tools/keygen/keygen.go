fe1e7dc90130273a2a84ec5cf0ebfc6385c08123
ryanj <ryan@networknext.com>
2020-03-06 15:53:43 -0500

    Fixed up output

--- cmd/tools/keygen/keygen.go
+++ cmd/tools/keygen/keygen.go
@@ -29,26 +29,22 @@ func main() {
 	fmt.Printf("\nWelcome to Network Next!\n\n")
 
 	fmt.Printf("This is your public key for packet signature verification:\n\n")
-	fmt.Printf("    base64: %s\n", base64.StdEncoding.EncodeToString(signaturePublicKey))
-	fmt.Printf("    byte data: %v\n\n", signaturePublicKey)
+	fmt.Printf("    %s\n", base64.StdEncoding.EncodeToString(signaturePublicKey))
 
 	fmt.Printf("This is your private key for packet signature verification:\n\n")
-	fmt.Printf("    base64: %s\n", base64.StdEncoding.EncodeToString(signaturePrivateKey))
-	fmt.Printf("    byte data: %v\n\n", signaturePrivateKey)
+	fmt.Printf("    %s\n", base64.StdEncoding.EncodeToString(signaturePrivateKey))
 
 	fmt.Printf("This is your public key for token encryption:\n\n")
-	fmt.Printf("    base64: %s\n", base64.StdEncoding.EncodeToString(encryptionPublicKey[:]))
-	fmt.Printf("    byte data: %v\n\n", encryptionPublicKey[:])
+	fmt.Printf("    %s\n", base64.StdEncoding.EncodeToString(encryptionPublicKey[:]))
 
 	fmt.Printf("This is your private key for token encryption:\n\n")
-	fmt.Printf("    base64: %s\n", base64.StdEncoding.EncodeToString(encryptionPrivateKey[:]))
-	fmt.Printf("    byte data: %v\n\n", encryptionPrivateKey[:])
+	fmt.Printf("    %s\n", base64.StdEncoding.EncodeToString(encryptionPrivateKey[:]))
 
 	fmt.Printf("IMPORTANT: Save your private keys in a secure place and don't share them with anybody, not even us!\n\n")
 }
 
 // generateRelayKeyPair creates a public and private keypair using crypto/ed25519 and prepends a random 8 byte customer ID
-// This is copied from crypto to avoid libsodium dependency.
+// This is copied from crypto to avoid libsodium dependency so that keygen can be built for multiple platforms.
 func generateCustomerKeyPair() ([]byte, []byte, error) {
 	customerID := make([]byte, 8)
 	rand.Read(customerID)
