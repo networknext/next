dd0f0019bae300bb63373a607ec111ca6d04407a
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-03-10 13:14:31 -0400

    added more comments

--- cmd/relay_old/actual_build.sh
+++ cmd/relay_old/actual_build.sh
@@ -6,18 +6,18 @@ if [[ "$1" == "build" ]]; then
 	make # finally make the thing
 elif [[ "$1" == "run" ]]; then
 	export RELAYSPEED="1G"
-	export RELAYDEV="1" # 1 || 0 for enabling/disabling dev mode
-	export RELAYMASTER="127.0.0.1" # ip addr of the backend, unused with http. pretty sure this is an accurate assumption but I could be wrong
-	export RELAYUPDATEKEY="$RELAY_PUBLIC_KEY" # not correct, just satisfy's the internal use, not used with http
-	export RELAYADDRESS="127.0.0.1" # must not have port
+	export RELAYDEV="1" # 1 for enabling dev mode || !1 for disabling dev mode, dev mode doesn't kill your system
+	export RELAYMASTER="127.0.0.1" # ip addr of the backend, unused with http since that requires http + this and I was lazy
+	export RELAYUPDATEKEY="$RELAY_PUBLIC_KEY" # not correct, just satisfy's the base64 decryption, not used with http, comes from firestore
+	export RELAYADDRESS="127.0.0.1" # must not have port, port is attached after init is done and that is what is sent to the backend
 	export RELAYPUBLICKEY="$RELAY_PUBLIC_KEY" # actual relay public key
 	export RELAYPRIVATEKEY="$RELAY_PRIVATE_KEY" # actual relay private key
 	export RELAYROUTERPUBLICKEY="$RELAY_ROUTER_PUBLIC_KEY" # new to the codebase, made the var similar to the others for the sake of consitency, original router key is hardcoded at top of relay_internal.cpp
-	export RELAYBACKENDHOSTNAME="$RELAY_BACKEND_HOSTNAME" # ditto, except 
+	export RELAYBACKENDHOSTNAME="$RELAY_BACKEND_HOSTNAME" # ditto, except nothing is hard coded, just first two comma sections
 
 	# quick & lazy way to just spawn 2 relays on different ports
 	if [[ "$2" == "one" ]]; then
-		export RELAYPORT="20000"
+		export RELAYPORT="20000" # after init this is set on the each env's relay address port
 	elif [[ "$2" == "two" ]]; then
 		export RELAYPORT="20001"
 	fi
