388d7c7f913d0f8b1ae5b5e568b62a1796f23f0a
Blain Smith <rebelgeek@blainsmith.com>
2020-03-16 14:00:53 -0400

    trim the start up script and move to using update scripts

--- deploy/vm-startup.sh
+++ deploy/vm-startup.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-# When this is copied to the VM you need to specify the tar file for the relay or server backend
-artifact=gs://artifacts.network-next-v3-dev.appspot.com/ARTIFACT.tar.gz
-
-# Set up an /app directory in the root of the VM
 mkdir -p /app
 cd /app
-
-# Copy the required files for the service from GCP Storage
-gsutil cp gs://artifacts.network-next-v3-dev.appspot.com/GeoLite2-City.mmdb .
-gsutil cp ${artifact} artifact.tar.gz
-
-# Uncompress the artifact files into /app
-tar -xvf artifact.tar.gz
-
-# Set the app service binary to executable
-chmod +x app
-
-# Copy the Systemd service definition to the right location
-cp /app/app.service /etc/systemd/system/app.service
-
-# Finally start the service
-systemctl start app.service
+gsutil cp gs://artifacts.network-next-v3-dev.appspot.com/vm-update-app.sh .
+chmod +x vm-update-app.sh
+./vm-update-app.sh -a gs://artifacts.network-next-v3-dev.appspot.com/ARTIFACT.tar.gz
