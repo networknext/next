b478422b18f1131f59d4db092b5d40c5680a1182
Glenn Fiedler <glenn@networknext.com>
2020-03-26 12:51:20 -0400

    fix copyright messages

--- next.cpp
+++ next.cpp
@@ -1,8 +1,6 @@
 /*
     Network Next SDK. Copyright © 2017 - 2020 Network Next, Inc.
 
-    Copyright © 2017 - 2020 Network Next, Inc.
-
     Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following 
     conditions are met:
 
