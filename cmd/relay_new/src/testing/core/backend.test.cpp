5f9027fd63ceeba7d25b598582b236060c7080b1
godofprogramming <jon.p.dickert@gmail.com>
2020-04-01 16:05:32 -0400

    shortened test

--- cmd/relay_new/src/testing/core/backend.test.cpp
+++ cmd/relay_new/src/testing/core/backend.test.cpp
@@ -291,83 +291,16 @@ Test(core_Backend_update_shutting_down_true)
   core::SessionMap sessions;
   auto backend = std::move(makeBackend(routerInfo, manager, sessions));
 
-  sessions.set(1234, std::make_shared<core::Session>(clock));  // just add one thing to the map to make it non-zero
+  testing::StubbedCurlWrapper::Response = ::BasicValidUpdateResponse ;
 
-  // seed relay manager
-  {
-    const size_t numRelays = 1;
-    std::array<uint64_t, MAX_RELAYS> ids;
-    std::array<net::Address, MAX_RELAYS> addrs;
-    std::array<core::PingData, MAX_RELAYS> pingData;
-    ids[0] = 987654321;
-    net::Address addr;
-    check(addr.parse("127.0.0.1:12345"));
-    addrs[0] = addr;
-    manager.update(numRelays, ids, addrs);
-    check(manager.getPingData(pingData) == 1);
-    manager.processPong(pingData[0].Addr, pingData[0].Seq);
-  }
-
-  testing::StubbedCurlWrapper::Response = R"({
-     "version": 0,
-     "ping_data": [
-       {
-         "relay_id": 135792468,
-         "relay_address": "127.0.0.1:54321"
-       },
-       {
-         "relay_id": 246813579,
-         "relay_address": "127.0.0.1:13524"
-       }
-     ]
-   })";
-
-  const uint64_t bytesReceived = 10000000000;
-
-  check(backend.update(bytesReceived, true));
+  check(backend.update(0, true));
 
   util::JSON doc;
-
   check(doc.parse(testing::StubbedCurlWrapper::Request));
-
   check(doc.get<uint32_t>("version") == 0);
   check(doc.get<std::string>("relay_address") == RelayAddr);
   check(doc.get<std::string>("Metadata", "PublicKey") == Base64RelayPublicKey);
-  check(doc.get<uint64_t>("TrafficStats", "BytesMeasurementRx") == bytesReceived);
-  check(doc.get<size_t>("TrafficStats", "SessionCount") == sessions.size());
+  check(doc.get<uint64_t>("TrafficStats", "BytesMeasurementRx") == 0);
+  check(doc.get<size_t>("TrafficStats", "SessionCount") == 0);
   check(doc.get<bool>("shutting_down"));
-
-  auto pingStats = doc.get<util::JSON>("PingStats");
-
-  check(pingStats.isArray());
-
-  auto& value = pingStats[0];
-
-  check(value.HasMember("RelayId"));
-  check(value.HasMember("RTT"));
-  check(value.HasMember("Jitter"));
-  check(value.HasMember("PacketLoss"));
-
-  auto& relayID = value["RelayId"];
-  // auto& rtt = value["RTT"];
-  // auto& jitter = value["Jitter"];
-  // auto& packetLoss = value["PacketLoss"];
-
-  check(relayID.Get<uint64_t>() == 987654321);
-
-  // not set up right, the actual tests pass for this
-  // so not currently concerned with it passing here,
-  // need to learn how the logic actually works to
-  // set this up right
-
-  // check(rtt.Get<float>() != 10000.0f);
-  // check(jitter.Get<float>() == 0.0f);
-  // check(packetLoss.Get<float>() == 0.0f);
-
-  std::array<core::PingData, MAX_RELAYS> pingData;
-  auto count = manager.getPingData(pingData);
-
-  check(count == 2);
-  check(pingData[0].Addr.toString() == "127.0.0.1:54321");
-  check(pingData[1].Addr.toString() == "127.0.0.1:13524");
 }
