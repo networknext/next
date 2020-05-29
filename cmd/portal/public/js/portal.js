7eab86a138a3cfd330517a596db882f5741017be
Andrew Baumbach <andrew@networknext.com>
2020-05-29 16:11:57 -0400

    downloads page without anon commits

--- cmd/portal/public/js/portal.js
+++ cmd/portal/public/js/portal.js
@@ -338,6 +338,9 @@ WorkspaceHandler = {
 		this.sessionLoop ? clearInterval(this.sessionLoop) : null;
 
 		switch (page) {
+			case 'downloads':
+				this.loadDownloadsPage();
+				break;
 			case 'map':
 				MapHandler.initMap();
 				break;
@@ -506,7 +509,9 @@ WorkspaceHandler = {
 				console.log("Something went wrong fetching relays");
 				Sentry.captureException(e);
 			});
-	},
+		},
+		loadDownloadsPage() {
+		},
 	loadRelayPage() {
 		JSONRPCClient
 			.call('OpsService.Relays', {})
@@ -833,6 +838,9 @@ function createVueComponents() {
 				workspaceHandler: WorkspaceHandler,
 			},
 			pages: {
+				downloads: {
+					show: false
+				},
 				map: {
 					filter: {
 						buyerId: '',
