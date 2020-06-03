87b480c0c9688de0d3d1b2f99ac9a52b93be0759
Glenn Fiedler <glenn@networknext.com>
2020-06-03 11:56:22 -0400

    relax cost matrix constraints

--- cmd/relay_backend/relay_backend.go
+++ cmd/relay_backend/relay_backend.go
@@ -318,6 +318,7 @@ func main() {
 				sentry.CaptureMessage("no routes within route matrix")
 			}
 
+			// todo: should be one
 			time.Sleep(10 * time.Second)
 		}
 	}()
--- reference/relay/relay.cpp
+++ reference/relay/relay.cpp
@@ -56,8 +56,8 @@ using namespace std::chrono_literals;
 
 #define RELAY_PING_TIME                                          0.1
 
-#define RELAY_STATS_WINDOW                                       100
-#define RELAY_PING_SAFETY                                         10
+#define RELAY_STATS_WINDOW                                      10.0
+#define RELAY_PING_SAFETY                                        1.0
 
 #define RELAY_MAX_PACKET_BYTES                                  1500
 
