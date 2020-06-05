64f5f1cf7825f5c00bef59904cc21cc0ae1fddd3
Blain Smith <rebelgeek@blainsmith.com>
2020-06-05 14:36:10 -0400

    fix compilation error

--- transport/jsonrpc/buyer.go
+++ transport/jsonrpc/buyer.go
@@ -253,14 +253,14 @@ func (s *BuyersService) SessionDetails(r *http.Request, args *SessionDetailsArgs
 	isAnon = IsAnonymous(r)
 	isOps = CheckIsOps(r)
 
-	if !isAnon && !isOps{
+	if !isAnon && !isOps {
 		isAdmin, err = CheckRoles(r, "Admin")
 		if err != nil {
 			return err
 		}
 	}
 
-	if !isAdmin && !isOps{
+	if !isAdmin && !isOps {
 		isSameBuyer, err = s.IsSameBuyer(r, reply.Meta.CustomerID)
 		if err != nil {
 			return err
@@ -288,7 +288,7 @@ func (s *BuyersService) SessionDetails(r *http.Request, args *SessionDetailsArgs
 		reply.Meta.NearbyRelays[idx].Name = r.Name
 	}
 
-	if isAnon || (!isSameBuyer && !isAdmin && !isOPs) {
+	if isAnon || (!isSameBuyer && !isAdmin && !isOps) {
 		reply.Meta.Anonymise()
 	}
 
