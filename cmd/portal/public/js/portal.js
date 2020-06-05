dd8a60b590800b0639f6bc0400afa790130fce39
Andrew Baumbach <andrew@networknext.com>
2020-06-05 16:36:36 -0400

    Got the popup working and looking ok ish. now for creative to see

--- cmd/portal/public/js/portal.js
+++ cmd/portal/public/js/portal.js
@@ -124,14 +124,6 @@ MapHandler = {
 		this.mapLoop = setInterval(() => {
 			this.refreshMapSessions();
 		}, 10000);
-		if (UserHandler.isAnonymous())
-			setTimeout(() => {
-				$('#video-modal').modal('toggle')
-				$('#video-modal').on('hidden.bs.modal', function () {
-						let videoPlayer = document.getElementById("video-player");
-						videoPlayer.parentElement.removeChild(videoPlayer);
-				});
-			}, 60000)
 	},
 	updateFilter(filter) {
 		Object.assign(rootComponent.$data.pages.map, {filter: filter});
@@ -803,6 +795,15 @@ function startApp() {
 			console.log(e);
 			Sentry.captureException(e);
 		});
+	if (UserHandler.isAnonymous()) {
+		setTimeout(() => {
+			$('#video-modal').modal('toggle')
+			$('#video-modal').on('hidden.bs.modal', function () {
+					let videoPlayer = document.getElementById("video-player");
+					videoPlayer.parentElement.removeChild(videoPlayer);
+			});
+		}, 60000)
+	}
 }
 
 function createVueComponents() {
