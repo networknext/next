ed8eafd6c250d74c8d36b43529a4f57b17e6ffbb
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-06-18 18:28:50 -0400

    printf to echo

--- deploy/relay/install.sh
+++ deploy/relay/install.sh
@@ -65,7 +65,7 @@ install_relay() {
 
 	backup_existing "$bin_dest" "$env_dest" "$svc_dest"
 
-	printf 'installing relay...'
+	echo 'installing relay...'
 	sudo mv "$bin" "$bin_dest"
 	sudo mv "$env" "$env_dest"
 	sudo mv "$svc" "$svc_dest"
