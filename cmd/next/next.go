6a1cc0baa7d5a390193593277a6b8ebf4b33e3c0
Ryan Jones <rpj5582@rit.edu>
2020-06-22 11:44:12 -0400

    added --h

--- cmd/next/next.go
+++ cmd/next/next.go
@@ -1287,7 +1287,7 @@ func main() {
 	fmt.Printf("\n")
 
 	args := os.Args[1:]
-	if len(args) == 0 || args[0] == "-h" || args[0] == "-help" || args[0] == "--help" {
+	if len(args) == 0 || args[0] == "-h" || args[0] == "-help" || args[0] == "--h" || args[0] == "--help" {
 		args = []string{}
 	}
 
