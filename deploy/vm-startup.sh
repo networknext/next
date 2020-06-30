77bdd363a3bf3a52f93a699845412273e22a7812
Blain Smith <rebelgeek@blainsmith.com>
2020-06-30 09:04:05 -0400

    updated vm startup script with new bucket name and sans maxmind db

--- deploy/vm-startup.sh
+++ deploy/vm-startup.sh
@@ -4,7 +4,6 @@ apt-get install libsodium23
 
 mkdir -p /app
 cd /app
-gsutil cp gs://artifacts.network-next-v3-dev.appspot.com/GeoLite2-City.mmdb .
-gsutil cp gs://artifacts.network-next-v3-dev.appspot.com/vm-update-app.sh .
+gsutil cp gs://prod_artifacts/vm-update-app.sh .
 chmod +x vm-update-app.sh
-./vm-update-app.sh -a gs://artifacts.network-next-v3-dev.appspot.com/ARTIFACT.tar.gz
+./vm-update-app.sh -a gs://prod_artifacts/ARTIFACT.tar.gz
