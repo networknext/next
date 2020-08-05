1c00b61f0d727b1c9dcf3dc8155debd4df98ba89
John Bleichert <john@networknext.com>
2020-08-05 13:22:12 -0400

    Moved into one file to avoid make errors

--- util/update_ids.go
+++ util/update_ids.go
@@ -36,6 +36,14 @@ type relay struct {
 	Type               string                 `firestore:"machineType"`
 }
 
+type datacenter struct {
+	Name         string  `firestore:"name"`
+	Enabled      bool    `firestore:"enabled"`
+	Latitude     float64 `firestore:"latitude"`
+	Longitude    float64 `firestore:"longitude"`
+	SupplierName string  `firestore:"supplierName"`
+}
+
 func main() {
 
 	// don't forget to e.g.:
@@ -75,6 +83,36 @@ func main() {
 		fmt.Printf("%s : %016x : %d\n", r.Name, rid, ridSigned)
 
 		_, err = rdoc.Ref.Update(ctx, []firestore.Update{{Path: "signedID", Value: ridSigned}})
+
+		break
+
+	}
+
+	datacenterCollection := client.Collection("Datacenter").Documents(ctx)
+	defer datacenterCollection.Stop()
+	for {
+		ddoc, err := datacenterCollection.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			fmt.Printf(" Next() err: %v\n", err)
+			os.Exit(1)
+		}
+
+		var d datacenter
+		err = ddoc.DataTo(&d)
+		if err != nil {
+			fmt.Printf("DataTo() err: %v\n", err)
+			os.Exit(1)
+		}
+
+		did := crypto.HashID(d.Name)
+		didSigned := int64(did)
+
+		fmt.Printf("%s : %016x : %d\n", d.Name, did, didSigned)
+
+		_, err = ddoc.Ref.Update(ctx, []firestore.Update{{Path: "signedID", Value: didSigned}})
 
 		break
 
