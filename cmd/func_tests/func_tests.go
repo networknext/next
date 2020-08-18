a67bfb56d932bceed917046fc0b9e6536126a96d
Glenn Fiedler <glenn@networknext.com>
2020-08-18 16:27:08 -0400

    fix thread race in func backend that was breaking func tests randomly

--- cmd/func_tests/func_tests.go
+++ cmd/func_tests/func_tests.go
@@ -1482,6 +1482,7 @@ type test_function func()
 
 func main() {
 	allTests := []test_function{
+		/*
 		test_direct_default,
 		test_direct_upgrade,
 		test_network_next,
@@ -1490,7 +1491,9 @@ func main() {
 		test_disable_network_next_on_client,
 		test_server_under_load,
 		test_reconnect_direct,
+		*/
 		test_reconnect_next,
+		/*
 		test_connect_to_another_server_direct,
 		test_connect_to_another_server_next,
 		test_route_switching,
@@ -1503,6 +1506,7 @@ func main() {
 		test_packet_loss_direct,
 		test_packet_loss_next,
 		test_idempotent,
+		*/
 	}
 
 	// If there are command line arguments, use reflection to see what tests to run
@@ -1522,7 +1526,9 @@ func main() {
 		tests = allTests // No command line args, run all tests
 	}
 
-	for i := range tests {
-		tests[i]()
+	for {
+		for i := range tests {
+			tests[i]()
+		}
 	}
 }
