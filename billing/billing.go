775489d8a42f9caefa230145b2a943e80b0672a5
Ryan Jones <rpj5582@rit.edu>
2020-08-28 11:26:48 -0400

    added connection type, platform type, and sdk version removed unnecessary files form billing package

--- billing/billing.go
+++ billing/billing.go
@@ -4,6 +4,8 @@ import (
 	"context"
 )
 
+const BillingSliceSeconds = 10
+
 // Biller is a billing interface that handles sending billing entries to remote services
 type Biller interface {
 	Bill(ctx context.Context, billingEntry *BillingEntry) error
