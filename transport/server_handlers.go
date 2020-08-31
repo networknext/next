412a5a6eb05c8bc4f16892db6979d0425df082e9
Ryan Jones <rpj5582@rit.edu>
2020-08-31 14:56:45 -0400

    remove random route and random dest relay route selectors

--- transport/server_handlers.go
+++ transport/server_handlers.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/binary"
 	"hash/fnv"
+	"math/rand"
 	"os"
 	"strconv"
 	"syscall"
@@ -12,7 +13,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"math/rand"
 	"net"
 
 	"sync"
@@ -1134,37 +1134,16 @@ func GetNextRoute(routeMatrix RouteProvider, nearRelayData []routing.NearRelayDa
 		return nil
 	}
 
-	// Now pick the best route from all acceptable routes:
-
-	// 	1. Choose a random destination relay (since all destination relays are in the same datacenter and have effectively the same RTT from relay -> game server)
-	//		and only select routes with that destination relay
-	//	2. If we still don't only have 1 route, choose a random one.
-
-	selectorFuncs := []routing.SelectorFunc{
-		routing.SelectRoutesByRandomDestRelay(rand.NewSource(rand.Int63())),
-		routing.SelectRandomRoute(rand.NewSource(rand.Int63())),
-	}
-
-	for _, selectorFunc := range selectorFuncs {
-		routes = selectorFunc(routes)
-		if len(routes) == 0 {
-			break
-		}
-	}
-
-	if len(routes) == 0 {
-		metrics.ErrorMetrics.RouteSelectFailure.Add(1)
-		return nil
-	}
-
-	route := &routes[0]
+	// Now that we have a slice of acceptable routes, choose a random one
+	randRouteIndex := rand.Intn(len(routes))
+	route := &routes[randRouteIndex]
 
 	// Now that we've selected a route, populate it with all of the necessary data
 	// We do this at the end to avoid populating data on routes we won't use
 	for i := 0; i < route.NumRelays; i++ {
 		relay, err := storer.Relay(route.RelayIDs[i])
 		if err != nil {
-			metrics.ErrorMetrics.RouteSelectFailure.Add(1)
+			metrics.ErrorMetrics.RouteFailure.Add(1)
 			return nil
 		}
 
