997912cd232bf7a56262f09ecf5d1f3fb8cb52fc
Ryan Jones <rpj5582@rit.edu>
2020-09-10 16:24:22 -0400

    add log to see if we're actually responding

--- cmd/server_backend4/server_backend4.go
+++ cmd/server_backend4/server_backend4.go
@@ -774,7 +774,10 @@ func mainReturnWithCode() int {
 
 					if _, err := conn.WriteToUDP(response, fromAddr); err != nil {
 						level.Error(logger).Log("msg", "failed to write UDP response", "err", err)
+						continue
 					}
+
+					level.Debug(logger).Log("successfully responded")
 				}
 			}
 
