938a0699fe0ea0e54adba0e70161a0889c058cc8
Ryan Jones <rpj5582@rit.edu>
2020-09-11 14:13:18 -0400

    fix command final?

--- deploy/client-spawner.sh
+++ deploy/client-spawner.sh
@@ -34,10 +34,11 @@ done
 
 trap "kill 0" EXIT
 
+ips=$(gcloud compute instances list --filter='load-test-server-staging AND status=RUNNING' --format='get(networkInterfaces[0].networkIP)')
+
 for ((r=0 ; r<${num_clients} ; r++)); do
 export NEXT_LOG_LEVEL=0
 export CORES=50
-ips=$(gcloud compute instances list --filter='load-test-server-staging AND status=RUNNING' --format='get(networkInterfaces[0].networkIP)')
 echo $ips | /app/app &
 pid="$!"
 printf "PID ${pid}: Client opened\n"
