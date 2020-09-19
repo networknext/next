6c7d5a1bf5dfd302637c3e26f76107d09473d4ba
Glenn Fiedler <glenn@networknext.com>
2020-09-19 19:29:20 -0400

    go

--- source/next.cpp
+++ source/next.cpp
@@ -2520,10 +2520,7 @@ bool is_backend_packet( const uint8_t * packet_data, int packet_bytes )
     next_assert( message_length > 0 );
     next_assert( message_length <= 32 );
 
-    uint8_t hash[NEXT_BACKEND_PACKET_HASH_BYTES];
-    crypto_generichash( hash, NEXT_BACKEND_PACKET_HASH_BYTES, message, message_length, next_backend_packet_hash_key, crypto_generichash_KEYBYTES );
-
-    return memcmp( hash, packet_data + 1, NEXT_BACKEND_PACKET_HASH_BYTES ) == 0;
+    crypto_generichash( packet_data[1:], NEXT_BACKEND_PACKET_HASH_BYTES, message, message_length, next_backend_packet_hash_key, crypto_generichash_KEYBYTES );
 }
 
 void hash_backend_packet( uint8_t * packet_data, size_t packet_bytes )
