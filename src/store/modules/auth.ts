6b4a89094450d5cc098e416fa58ce771967885d8
Andrew Baumbach <andrew@networknext.com>
2020-09-28 12:18:17 -0400

    Fixed filters not showing for non admins

--- src/store/modules/auth.ts
+++ src/store/modules/auth.ts
@@ -26,7 +26,7 @@ export default {
     isAnonymous: (state: any) => state.userProfile === null,
     isAnonymousPlus: (state: any, getters: any) => !getters.isAnonymous ? !state.userProfile.verified : false,
     isABTester: (state: any, getters: any) => (getters.isBuyer && state.abTesters.includes(getters.userProfile.id)) || getters.isAdmin,
-    isBuyer: (state: any, getters: any) => (state.userProfile ? state.userProfile.pubKey != '' : false),
+    isBuyer: (state: any) => (state.userProfile ? state.userProfile.pubKey !== '' : false),
     userProfile: (state: any) => state.userProfile,
     allBuyers: (state: any) => state.allBuyers,
     registeredToCompany: (state: any) => (state.userProfile ? state.userProfile.companyCode !== '' : false)
