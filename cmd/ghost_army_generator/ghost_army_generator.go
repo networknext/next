f645a89d3794d4f6a5e671ef21d4f723c6deeef0
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-09-29 09:44:14 -0400

    switching to other branch

--- cmd/ghost_army_generator/ghost_army_generator.go
+++ cmd/ghost_army_generator/ghost_army_generator.go
@@ -9,6 +9,8 @@ import (
 	"os"
 	"sort"
 	"strconv"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/networknext/backend/encoding"
@@ -32,260 +34,222 @@ func (self sortableEntries) Less(i, j int) bool {
 // Input files can be in any order
 
 func main() {
-	if len(os.Args) < 4 {
-		fmt.Println("you must supply at least 3 arguments: <datacenter csv> <output file> <input file name(s)>")
+	if len(os.Args) < 6 {
+		fmt.Println("you must supply at least 5 arguments: <processing threads> <datacenter csv> <ISPs file> <output file> <input file name(s)>")
 		os.Exit(1)
 	}
 
-	datacenterCSV := os.Args[1]
-	outfile := os.Args[2]
-	infiles := os.Args[3:]
-
-	// parse datacenter csv
-	inputfile, err := os.Open(datacenterCSV)
-	if err != nil {
-		fmt.Printf("could not open '%s': %v\n", datacenterCSV, err)
-		os.Exit(1)
-	}
-
-	lines, err := csv.NewReader(inputfile).ReadAll()
-	if err != nil {
-		fmt.Printf("could not read csv data: %v\n", err)
-		os.Exit(1)
-	}
-	inputfile.Close()
-
-	dcmap := make(map[uint64]ghostarmy.StrippedDatacenter)
-
-	for lineNum, line := range lines {
-		if lineNum == 0 {
-			continue
-		}
-
-		var datacenter ghostarmy.StrippedDatacenter
-		datacenter.Name = line[0]
-		id, err := strconv.ParseUint(line[1], 10, 64)
-		if err != nil {
-			fmt.Printf("could not parse id for dc %s", datacenter.Name)
-			continue
-		}
-		datacenter.Lat, err = strconv.ParseFloat(line[2], 64)
-		if err != nil {
-			fmt.Printf("could not parse lat for dc %s", datacenter.Name)
-			continue
-		}
-		datacenter.Long, err = strconv.ParseFloat(line[3], 64)
-		if err != nil {
-			fmt.Printf("could not parse long for dc %s", datacenter.Name)
-			continue
-		}
-
-		dcmap[id] = datacenter
-	}
-
-	var ispsFilename string
-	if v, ok := os.LookupEnv("ISPS_FILE"); ok {
-		ispsFilename = v
+	var processingThreads uint64
+	if v, err := strconv.ParseUint(os.Args[1], 10, 64); err == nil {
+		processingThreads = v
 	} else {
-		fmt.Println("you must set ISPS_FILE to a file")
-		os.Exit(1)
+		fmt.Printf("could not parse processing thread count '%s': %v\n", os.Args[1], err)
 	}
-
-	file, err := os.Open(ispsFilename)
-	if err != nil {
-		fmt.Printf("could not open %s: %v", ispsFilename, err)
-		os.Exit(1)
-	}
-
-	var isps []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		isps = append(isps, scanner.Text())
-	}
-	file.Close()
+	fmt.Printf("%d\n", processingThreads)
+	dcmap := parseDatacenterCSV(os.Args[2])
+	isps := parseISPCSV(os.Args[3])
+	outfile := os.Args[4]
+	infiles := os.Args[5:]
 
 	// convert to binary
 
-	var entries sortableEntries
-	entries = make([]ghostarmy.Entry, 0)
+	var entryMutex sync.Mutex
+	var entries = make(sortableEntries, 0)
 
-	for _, infile := range infiles {
-		// read in exported data
-		fmt.Printf("reading %s\n", infile)
-		inputfile, err := os.Open(infile)
-		if err != nil {
-			fmt.Printf("could not open '%s': %v\n", infile, err)
-			os.Exit(1)
-		}
+	var wg sync.WaitGroup
+	wg.Add(len(infiles))
 
-		lines, err := csv.NewReader(inputfile).ReadAll()
-		if err != nil {
-			fmt.Printf("could not read csv data: %v\n", err)
-			os.Exit(1)
-		}
+	processedFiles := uint64(0)
+	for i := uint64(0); i < processingThreads; i++ {
+		go func(processedFiles *uint64) {
+			index := atomic.AddUint64(processedFiles, 1) - 1
+			for index < uint64(len(infiles)) {
+				localentries := make(sortableEntries, 0)
+				infile := infiles[index]
+				fmt.Printf("reading %s\n", infile)
+				inputfile, err := os.Open(infile)
+				if err != nil {
+					fmt.Printf("could not open '%s': %v\n", infile, err)
+					os.Exit(1)
+				}
 
-		inputfile.Close()
+				lines, err := csv.NewReader(inputfile).ReadAll()
+				if err != nil {
+					fmt.Printf("could not read csv data: %v\n", err)
+					os.Exit(1)
+				}
 
-		for lineNum, line := range lines {
-			if lineNum == 0 {
-				continue
+				inputfile.Close()
+
+				for lineNum, line := range lines {
+					if lineNum == 0 {
+						continue
+					}
+					var err error
+					var entry ghostarmy.Entry
+
+					i := 0
+
+					// line into Entry
+					entry.SessionID, err = strconv.ParseInt(line[i], 10, 64)
+					checkErr(err, lineNum)
+					i++
+
+					t, err := time.Parse("2006-1-2 15:04:05", line[i])
+					checkErr(err, lineNum)
+					i++
+
+					year, month, day := t.Date()
+					t2 := time.Date(year, month, day, 0, 0, 0, 0, t.Location())
+					secsIntoDay := int64(t.Sub(t2).Seconds())
+
+					entry.Timestamp = secsIntoDay
+
+					entry.BuyerID, err = strconv.ParseInt(line[i], 10, 64)
+					checkErr(err, lineNum)
+					i++
+
+					entry.SliceNumber, err = strconv.ParseInt(line[i], 10, 64)
+					checkErr(err, lineNum)
+					i++
+
+					entry.Next, err = strconv.ParseBool(line[i])
+					checkErr(err, lineNum)
+					i++
+
+					entry.DirectRTT, err = strconv.ParseFloat(line[i], 64)
+					checkErr(err, lineNum)
+					i++
+
+					entry.DirectJitter, err = strconv.ParseFloat(line[i], 64)
+					checkErr(err, lineNum)
+					i++
+
+					entry.DirectPacketLoss, err = strconv.ParseFloat(line[i], 64)
+					checkErr(err, lineNum)
+					i++
+
+					entry.NextRTT, err = strconv.ParseFloat(line[i], 64)
+					checkErr(err, lineNum)
+					i++
+
+					entry.NextJitter, err = strconv.ParseFloat(line[i], 64)
+					checkErr(err, lineNum)
+					i++
+
+					entry.NextPacketLoss, err = strconv.ParseFloat(line[i], 64)
+					checkErr(err, lineNum)
+					i++
+
+					err = json.Unmarshal([]byte(line[i]), &entry.NextRelays)
+					checkErr(err, lineNum)
+					i++
+
+					entry.TotalPrice, err = strconv.ParseInt(line[i], 10, 64)
+					checkErr(err, lineNum)
+					i++
+
+					if line[i] != "" {
+						ctspl, err := strconv.ParseFloat(line[i], 64)
+						checkErr(err, lineNum)
+						entry.ClientToServerPacketsLost = int64(ctspl)
+					}
+					i++
+
+					if line[i] != "" {
+						stcpl, err := strconv.ParseFloat(line[i], 64)
+						checkErr(err, lineNum)
+						entry.ServerToClientPacketsLost = int64(stcpl)
+					}
+					i++
+
+					entry.Committed, err = strconv.ParseBool(line[i])
+					checkErr(err, lineNum)
+					i++
+
+					entry.Flagged, err = strconv.ParseBool(line[i])
+					checkErr(err, lineNum)
+					i++
+
+					entry.Multipath, err = strconv.ParseBool(line[i])
+					checkErr(err, lineNum)
+					i++
+
+					entry.NextBytesUp, err = strconv.ParseInt(line[i], 10, 64)
+					checkErr(err, lineNum)
+					i++
+
+					entry.NextBytesDown, err = strconv.ParseInt(line[i], 10, 64)
+					checkErr(err, lineNum)
+					i++
+
+					entry.Initial, err = strconv.ParseBool(line[i])
+					checkErr(err, lineNum)
+					i++
+
+					entry.DatacenterID, err = strconv.ParseInt(line[i], 10, 64)
+					checkErr(err, lineNum)
+					i++
+
+					entry.RttReduction, err = strconv.ParseBool(line[i])
+					checkErr(err, lineNum)
+					i++
+
+					entry.PacketLossReduction, err = strconv.ParseBool(line[i])
+					checkErr(err, lineNum)
+					i++
+
+					err = json.Unmarshal([]byte(line[i]), &entry.NextRelaysPrice)
+					checkErr(err, lineNum)
+					i++
+
+					entry.Userhash, err = strconv.ParseInt(line[i], 10, 64)
+					checkErr(err, lineNum)
+					i++
+
+					if line[i] == "" {
+						// get latitude from datacenter
+						entry.Latitude = dcmap[uint64(entry.DatacenterID)].Lat
+					} else {
+						// use actual latitude
+						lat, err := strconv.ParseFloat(line[i], 64)
+						checkErr(err, lineNum)
+						entry.Latitude = lat
+					}
+					i++
+
+					if line[i] == "" {
+						// get longitude from datacenter
+						entry.Longitude = dcmap[uint64(entry.DatacenterID)].Long
+					} else {
+						// use actual longitude
+						long, err := strconv.ParseFloat(line[i], 64)
+						checkErr(err, lineNum)
+						entry.Longitude = long
+					}
+					i++
+
+					if line[i] == "" {
+						// get random isp
+						entry.ISP = isps[uint64(entry.Userhash)%uint64(len(isps))]
+					} else {
+						// use actual isp
+						entry.ISP = line[i]
+					}
+
+					// push back entry
+					localentries = append(localentries, entry)
+				}
+				entryMutex.Lock()
+				entries = append(entries, localentries...)
+				entryMutex.Unlock()
+				index := atomic.AddUint64(processedFiles, 1) - 1
+				wg.Done()
 			}
-			var err error
-			var entry ghostarmy.Entry
-
-			i := 0
-
-			// line into Entry
-			entry.SessionID, err = strconv.ParseInt(line[i], 10, 64)
-			checkErr(err, lineNum)
-			i++
-
-			t, err := time.Parse("2006-1-2 15:04:05", line[i])
-			checkErr(err, lineNum)
-			i++
-
-			year, month, day := t.Date()
-			t2 := time.Date(year, month, day, 0, 0, 0, 0, t.Location())
-			secsIntoDay := int64(t.Sub(t2).Seconds())
-
-			entry.Timestamp = secsIntoDay
-
-			entry.BuyerID, err = strconv.ParseInt(line[i], 10, 64)
-			checkErr(err, lineNum)
-			i++
-
-			entry.SliceNumber, err = strconv.ParseInt(line[i], 10, 64)
-			checkErr(err, lineNum)
-			i++
-
-			entry.Next, err = strconv.ParseBool(line[i])
-			checkErr(err, lineNum)
-			i++
-
-			entry.DirectRTT, err = strconv.ParseFloat(line[i], 64)
-			checkErr(err, lineNum)
-			i++
-
-			entry.DirectJitter, err = strconv.ParseFloat(line[i], 64)
-			checkErr(err, lineNum)
-			i++
-
-			entry.DirectPacketLoss, err = strconv.ParseFloat(line[i], 64)
-			checkErr(err, lineNum)
-			i++
-
-			entry.NextRTT, err = strconv.ParseFloat(line[i], 64)
-			checkErr(err, lineNum)
-			i++
-
-			entry.NextJitter, err = strconv.ParseFloat(line[i], 64)
-			checkErr(err, lineNum)
-			i++
-
-			entry.NextPacketLoss, err = strconv.ParseFloat(line[i], 64)
-			checkErr(err, lineNum)
-			i++
-
-			err = json.Unmarshal([]byte(line[i]), &entry.NextRelays)
-			checkErr(err, lineNum)
-			i++
-
-			entry.TotalPrice, err = strconv.ParseInt(line[i], 10, 64)
-			checkErr(err, lineNum)
-			i++
-
-			if line[i] != "" {
-				ctspl, err := strconv.ParseFloat(line[i], 64)
-				checkErr(err, lineNum)
-				entry.ClientToServerPacketsLost = int64(ctspl)
-			}
-			i++
-
-			if line[i] != "" {
-				stcpl, err := strconv.ParseFloat(line[i], 64)
-				checkErr(err, lineNum)
-				entry.ServerToClientPacketsLost = int64(stcpl)
-			}
-			i++
-
-			entry.Committed, err = strconv.ParseBool(line[i])
-			checkErr(err, lineNum)
-			i++
-
-			entry.Flagged, err = strconv.ParseBool(line[i])
-			checkErr(err, lineNum)
-			i++
-
-			entry.Multipath, err = strconv.ParseBool(line[i])
-			checkErr(err, lineNum)
-			i++
-
-			entry.NextBytesUp, err = strconv.ParseInt(line[i], 10, 64)
-			checkErr(err, lineNum)
-			i++
-
-			entry.NextBytesDown, err = strconv.ParseInt(line[i], 10, 64)
-			checkErr(err, lineNum)
-			i++
-
-			entry.Initial, err = strconv.ParseBool(line[i])
-			checkErr(err, lineNum)
-			i++
-
-			entry.DatacenterID, err = strconv.ParseInt(line[i], 10, 64)
-			checkErr(err, lineNum)
-			i++
-
-			entry.RttReduction, err = strconv.ParseBool(line[i])
-			checkErr(err, lineNum)
-			i++
-
-			entry.PacketLossReduction, err = strconv.ParseBool(line[i])
-			checkErr(err, lineNum)
-			i++
-
-			err = json.Unmarshal([]byte(line[i]), &entry.NextRelaysPrice)
-			checkErr(err, lineNum)
-			i++
-
-			entry.Userhash, err = strconv.ParseInt(line[i], 10, 64)
-			checkErr(err, lineNum)
-			i++
-
-			if line[i] == "" {
-				// get latitude from datacenter
-				entry.Latitude = dcmap[uint64(entry.DatacenterID)].Lat
-			} else {
-				// use actual latitude
-				lat, err := strconv.ParseFloat(line[i], 64)
-				checkErr(err, lineNum)
-				entry.Latitude = lat
-			}
-			i++
-
-			if line[i] == "" {
-				// get longitude from datacenter
-				entry.Longitude = dcmap[uint64(entry.DatacenterID)].Long
-			} else {
-				// use actual longitude
-				long, err := strconv.ParseFloat(line[i], 64)
-				checkErr(err, lineNum)
-				entry.Longitude = long
-			}
-			i++
-
-			if line[i] == "" {
-				// get random isp
-				entry.ISP = isps[uint64(entry.Userhash)%uint64(len(isps))]
-			} else {
-				// use actual isp
-				entry.ISP = line[i]
-			}
-
-			// push back entry
-			entries = append(entries, entry)
-		}
+		}(&processedFiles)
 	}
+
+	wg.Wait()
 
 	// sort on timestamp
 	fmt.Printf("sorting %d entries...\n", len(entries))
@@ -325,4 +289,65 @@ func checkErr(err error, lineNum int) {
 		fmt.Printf("paniced on line %d of csv\n", lineNum)
 		panic(err)
 	}
+}
+
+func parseDatacenterCSV(filename string) map[uint64]ghostarmy.StrippedDatacenter {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("could not open '%s': %v\n", filename, err)
+		os.Exit(1)
+	}
+
+	lines, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		fmt.Printf("could not read csv data: %v\n", err)
+		os.Exit(1)
+	}
+	file.Close()
+
+	dcmap := make(map[uint64]ghostarmy.StrippedDatacenter)
+
+	for lineNum, line := range lines {
+		if lineNum == 0 {
+			continue
+		}
+
+		var datacenter ghostarmy.StrippedDatacenter
+		datacenter.Name = line[0]
+		id, err := strconv.ParseUint(line[1], 10, 64)
+		if err != nil {
+			fmt.Printf("could not parse id for dc %s", datacenter.Name)
+			continue
+		}
+		datacenter.Lat, err = strconv.ParseFloat(line[2], 64)
+		if err != nil {
+			fmt.Printf("could not parse lat for dc %s", datacenter.Name)
+			continue
+		}
+		datacenter.Long, err = strconv.ParseFloat(line[3], 64)
+		if err != nil {
+			fmt.Printf("could not parse long for dc %s", datacenter.Name)
+			continue
+		}
+
+		dcmap[id] = datacenter
+	}
+
+	return dcmap
+}
+
+func parseISPCSV(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("could not open %s: %v", filename, err)
+		os.Exit(1)
+	}
+
+	var isps []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		isps = append(isps, scanner.Text())
+	}
+	file.Close()
+	return isps
 }
