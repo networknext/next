908567bb941beddc96e3e6e928d122eee5dcebd0
Andrew Baumbach <andrew@networknext.com>
2020-10-01 13:30:13 -0400

    Got auth tests working again

--- transport/jsonrpc/auth_test.go
+++ transport/jsonrpc/auth_test.go
@@ -1,7 +1,19 @@
 package jsonrpc_test
 
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/networknext/backend/routing"
+	"github.com/networknext/backend/storage"
+	"github.com/networknext/backend/transport/jsonrpc"
+	"github.com/stretchr/testify/assert"
+)
+
 // All tests listed below depend on test@networknext.com being a user in auth0
-/* func TestAuthMiddleware(t *testing.T) {
+func TestAuthMiddleware(t *testing.T) {
 	// JWT obtained from Portal Login Dev SPA (Auth0)
 	// Note: 5 year expiration time (expires on 18 May 2025)
 	// test@networknext.com => Delete this in auth0 and these tests will break
@@ -12,7 +24,7 @@ package jsonrpc_test
 	}
 
 	t.Run("skip auth", func(t *testing.T) {
-		authMiddleware := jsonrpc.AuthMiddleware("", http.HandlerFunc(noopHandler))
+		authMiddleware := jsonrpc.AuthMiddleware("", http.HandlerFunc(noopHandler), false)
 
 		req := httptest.NewRequest(http.MethodGet, "/", nil)
 		res := httptest.NewRecorder()
@@ -22,7 +34,7 @@ package jsonrpc_test
 	})
 
 	t.Run("check auth claims", func(t *testing.T) {
-		authMiddleware := jsonrpc.AuthMiddleware("oQJH3YPHdvZJnxCPo1Irtz5UKi5zrr6n", http.HandlerFunc(noopHandler))
+		authMiddleware := jsonrpc.AuthMiddleware("oQJH3YPHdvZJnxCPo1Irtz5UKi5zrr6n", http.HandlerFunc(noopHandler), false)
 
 		req := httptest.NewRequest(http.MethodGet, "/", nil)
 		req.Header.Add("Authorization", "Bearer "+jwtSideload)
@@ -33,7 +45,7 @@ package jsonrpc_test
 	})
 
 	t.Run("anonymous auth", func(t *testing.T) {
-		authMiddleware := jsonrpc.AuthMiddleware("oQJH3YPHdvZJnxCPo1Irtz5UKi5zrr6n", http.HandlerFunc(noopHandler))
+		authMiddleware := jsonrpc.AuthMiddleware("oQJH3YPHdvZJnxCPo1Irtz5UKi5zrr6n", http.HandlerFunc(noopHandler), false)
 
 		req := httptest.NewRequest(http.MethodGet, "/", nil)
 		res := httptest.NewRecorder()
@@ -43,219 +55,19 @@ package jsonrpc_test
 	})
 }
 
-func TestAuthClient(t *testing.T) {
-	t.Skip()
-	// test@networknext.com => Delete this in auth0 and these tests will break
-	jwtSideload := "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5rWXpOekkwTkVVNVFrSTVNRVF4TURRMk5UWTBNakkzTmpOQlJESkVNa1E0TnpGRFF6QkdRdyJ9.eyJuaWNrbmFtZSI6InRlc3QiLCJuYW1lIjoidGVzdEBuZXR3b3JrbmV4dC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvMmRhNWMwMjU5ZTQ3NmI1MDg0MTBlZWY3ZjI5Zjc1NGE_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZ0ZS5wbmciLCJ1cGRhdGVkX2F0IjoiMjAyMC0wNi0yM1QxMzozOToyMS44ODFaIiwiZW1haWwiOiJ0ZXN0QG5ldHdvcmtuZXh0LmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL25ldHdvcmtuZXh0LmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1Yjk2ZjYxY2YxNjQyNzIxYWQ4NGVlYjYiLCJhdWQiOiJvUUpIM1lQSGR2WkpueENQbzFJcnR6NVVLaTV6cnI2biIsImlhdCI6MTU5MjkxOTU2NSwiZXhwIjoxNzUwNzA0MzI1LCJub25jZSI6ImRHZFNUWEpRTnpkdE5GcHNjR0Z1YVg1dlQxVlNhVFZXUjJoK2VHdG1hMnB2TkcweFZuNTFZalJJZmc9PSJ9.BvMe5fWJcheGzKmt3nCIeLjMD-C5426cpjtJiR55i7lmbT0k4h8Z2X6rynZ_aKR-gaCTY7FG5gI-Ty9ZY1zboWcIkxaTi0VKQzdMUTYVMXVEK2cQ1NVbph7_RSJhLfgO5y7PkmuMZXJEFdrI_2PkO4b3tOU-vpUHFUPtTsESV79a81kXn2C5j_KkKzCOPZ4zol1aEU3WliaaJNT38iSz3NX9URshrrdCE39JRClx6wbUgrfCGnVtfens-Sg7atijivaOx8IlUGOxLMEciYwBL2aY5EXaa7tp7c8ZvoEEj7uZH2R35fV7eUzACwShU-JLR9oOsNEhS4XO1AzTMtNHQA"
-	noopHandler := func(w http.ResponseWriter, _ *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}
-	logger := log.NewNopLogger()
-
-	t.Run("create auth0Client", func(t *testing.T) {
-		manager, err := management.New(
-			"networknext.auth0.com",
-			"0Hn8oZfUwy5UPo6bUk0hYCQ2hMJnwQYg",
-			"l2namTU5jKVAkuCwV3votIPcP87jcOuJREtscx07aLgo8EykReX69StUVBfJOzx5",
-		)
-		assert.NoError(t, err)
-		auth0 := storage.Auth0{
-			Manager: manager,
-			Logger:  logger,
-		}
-		assert.NotEmpty(t, auth0)
-	})
-
-	manager, err := management.New(
-		"networknext.auth0.com",
-		"0Hn8oZfUwy5UPo6bUk0hYCQ2hMJnwQYg",
-		"l2namTU5jKVAkuCwV3votIPcP87jcOuJREtscx07aLgo8EykReX69StUVBfJOzx5",
-	)
-	assert.NoError(t, err)
-
-	auth0Client := storage.Auth0{
-		Manager: manager,
-		Logger:  logger,
-	}
-	db := storage.InMemory{}
-	db.AddBuyer(context.Background(), routing.Buyer{ID: 111, Domain: "networknext.com"})
-
-	svc := jsonrpc.AuthService{
-		Auth0:   auth0Client,
-		Storage: &db,
-		Logger:  logger,
-	}
-	authMiddleware := jsonrpc.AuthMiddleware("oQJH3YPHdvZJnxCPo1Irtz5UKi5zrr6n", http.HandlerFunc(noopHandler))
-
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
-	req.Header.Add("Authorization", "Bearer "+jwtSideload)
-	res := httptest.NewRecorder()
-
-	authMiddleware.ServeHTTP(res, req)
-	assert.Equal(t, http.StatusOK, res.Code)
-	user := req.Context().Value("user")
-	assert.NotEqual(t, user, nil)
-	claims := user.(*jwt.Token).Claims.(jwt.MapClaims)
-
-	requestID, ok := claims["sub"]
-
-	assert.True(t, ok)
-	assert.Equal(t, "auth0|5b96f61cf1642721ad84eeb6", requestID)
-
-// 	roles, err := auth0Client.Manager.User.Roles(requestID.(string))
-
-// 	assert.NoError(t, err)
-// 	req = jsonrpc.SetRoles(req, *roles)
-
-// 	t.Run("fetch all auth0 accounts", func(t *testing.T) {
-// 		var reply jsonrpc.AccountsReply
-
-// 		err = svc.AllAccounts(req, &jsonrpc.AccountsArgs{}, &reply)
-// 		assert.NoError(t, err)
-// 	})
-
-// 	t.Run("fetch user no user id", func(t *testing.T) {
-// 		var reply jsonrpc.AccountReply
-
-// 		err := svc.UserAccount(req, &jsonrpc.AccountArgs{}, &reply)
-// 		assert.Error(t, err)
-// 	})
-
-// 	t.Run("fetch user auth0 account", func(t *testing.T) {
-// 		var reply jsonrpc.AccountReply
-
-// 		err := svc.UserAccount(req, &jsonrpc.AccountArgs{UserID: "auth0|5b96f61cf1642721ad84eeb6"}, &reply)
-// 		assert.NoError(t, err)
-
-// 		assert.Equal(t, reply.UserAccount.Name, "test@networknext.com")
-// 		assert.Equal(t, reply.UserAccount.Email, "test@networknext.com")
-// 		assert.Equal(t, reply.UserAccount.UserID, "5b96f61cf1642721ad84eeb6")
-// 		assert.Equal(t, reply.UserAccount.ID, "111")
-// 		assert.Equal(t, reply.UserAccount.CompanyName, "")
-// 	})
-
-// 	t.Run("fetch user roles no user id", func(t *testing.T) {
-// 		var reply jsonrpc.RolesReply
-
-// 		err := svc.UserRoles(req, &jsonrpc.RolesArgs{}, &reply)
-// 		assert.Error(t, err)
-// 	})
-
-// 	t.Run("fetch all auth0 roles", func(t *testing.T) {
-// 		var reply jsonrpc.RolesReply
-
-// 		err := svc.AllRoles(req, &jsonrpc.RolesArgs{}, &reply)
-// 		assert.NoError(t, err)
-
-// 		assert.NotEqual(t, len(reply.Roles), 0)
-// 	})
-
-// 	t.Run("Remove all auth0 roles", func(t *testing.T) {
-// 		var reply jsonrpc.RolesReply
-
-		id := "rol_YfFrtom32or4vH89"
-		name := "Admin"
-		description := "Can manage the Network Next system, including access to configstore."
-
-		// Need to keep Admin role as a minimum to not break further tests
-		roles := []*management.Role{
-			{
-				ID:          &id,
-				Name:        &name,
-				Description: &description,
-			},
-		}
-
-// 		err := svc.UpdateUserRoles(req, &jsonrpc.RolesArgs{UserID: "auth0|5b96f61cf1642721ad84eeb6", Roles: roles}, &reply)
-// 		assert.NoError(t, err)
-
-		assert.Equal(t, len(reply.Roles), 1)
-	})
-
-	t.Run("add all auth0 roles", func(t *testing.T) {
-		var reply jsonrpc.RolesReply
-
-		roleNames := []string{
-			"rol_8r0281hf2oC4cvrD",
-			"rol_ScQpWhLvmTKRlqLU",
-		}
-		roleTypes := []string{
-			"Owner",
-			"Viewer",
-		}
-		roleDescriptions := []string{
-			"Can access and manage everything in an account.",
-			"Can see current sessions and the map.",
-		}
-
-		// Need to keep Admin role as a minimum to not break further tests
-		roles := []*management.Role{
-			{
-				ID:          &roleNames[0],
-				Name:        &roleTypes[0],
-				Description: &roleDescriptions[0],
-			},
-			{
-				ID:          &roleNames[1],
-				Name:        &roleTypes[1],
-				Description: &roleDescriptions[1],
-			},
-		}
-
-		err := svc.UpdateUserRoles(req, &jsonrpc.RolesArgs{UserID: "auth0|5b96f61cf1642721ad84eeb6", Roles: roles}, &reply)
-		assert.NoError(t, err)
-
-		assert.Equal(t, 2, len(reply.Roles))
-	})
-}
-
-// Need more test accounts to cover unverified and anonymous users
-// or just figure out a better auth0 manager that can handle http intercepts
-
-// Tests depend on values stored in the JWT
 func TestRoleVerification(t *testing.T) {
-	// test@networknext.com => Delete this in auth0 and these tests will break
-	jwtSideload := "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5rWXpOekkwTkVVNVFrSTVNRVF4TURRMk5UWTBNakkzTmpOQlJESkVNa1E0TnpGRFF6QkdRdyJ9.eyJuaWNrbmFtZSI6InRlc3QiLCJuYW1lIjoidGVzdEBuZXR3b3JrbmV4dC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvMmRhNWMwMjU5ZTQ3NmI1MDg0MTBlZWY3ZjI5Zjc1NGE_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZ0ZS5wbmciLCJ1cGRhdGVkX2F0IjoiMjAyMC0wNi0yM1QxMzozOToyMS44ODFaIiwiZW1haWwiOiJ0ZXN0QG5ldHdvcmtuZXh0LmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL25ldHdvcmtuZXh0LmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1Yjk2ZjYxY2YxNjQyNzIxYWQ4NGVlYjYiLCJhdWQiOiJvUUpIM1lQSGR2WkpueENQbzFJcnR6NVVLaTV6cnI2biIsImlhdCI6MTU5MjkxOTU2NSwiZXhwIjoxNzUwNzA0MzI1LCJub25jZSI6ImRHZFNUWEpRTnpkdE5GcHNjR0Z1YVg1dlQxVlNhVFZXUjJoK2VHdG1hMnB2TkcweFZuNTFZalJJZmc9PSJ9.BvMe5fWJcheGzKmt3nCIeLjMD-C5426cpjtJiR55i7lmbT0k4h8Z2X6rynZ_aKR-gaCTY7FG5gI-Ty9ZY1zboWcIkxaTi0VKQzdMUTYVMXVEK2cQ1NVbph7_RSJhLfgO5y7PkmuMZXJEFdrI_2PkO4b3tOU-vpUHFUPtTsESV79a81kXn2C5j_KkKzCOPZ4zol1aEU3WliaaJNT38iSz3NX9URshrrdCE39JRClx6wbUgrfCGnVtfens-Sg7atijivaOx8IlUGOxLMEciYwBL2aY5EXaa7tp7c8ZvoEEj7uZH2R35fV7eUzACwShU-JLR9oOsNEhS4XO1AzTMtNHQA"
-	noopHandler := func(w http.ResponseWriter, _ *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}
-	logger := log.NewNopLogger()
-
-	manager, err := management.New(
-		"networknext.auth0.com",
-		"0Hn8oZfUwy5UPo6bUk0hYCQ2hMJnwQYg",
-		"l2namTU5jKVAkuCwV3votIPcP87jcOuJREtscx07aLgo8EykReX69StUVBfJOzx5",
-	)
-	assert.NoError(t, err)
-
-	auth0Client := storage.Auth0{
-		Manager: manager,
-		Logger:  logger,
-	}
 	db := storage.InMemory{}
-	db.AddBuyer(context.Background(), routing.Buyer{ID: 111, Domain: "networknext.com"})
-
-	authMiddleware := jsonrpc.AuthMiddleware("oQJH3YPHdvZJnxCPo1Irtz5UKi5zrr6n", http.HandlerFunc(noopHandler))
+	db.AddCustomer(context.Background(), routing.Customer{Code: "local", Name: "Local"})
+	db.AddBuyer(context.Background(), routing.Buyer{ID: 111, CompanyCode: "local"})
 
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
-	req.Header.Add("Authorization", "Bearer "+jwtSideload)
-	res := httptest.NewRecorder()
 
-	authMiddleware.ServeHTTP(res, req)
-	assert.Equal(t, http.StatusOK, res.Code)
-	user := req.Context().Value("user")
-	assert.NotEqual(t, user, nil)
-	claims := user.(*jwt.Token).Claims.(jwt.MapClaims)
-
-	requestID, ok := claims["sub"]
-
-	assert.True(t, ok)
-	assert.Equal(t, "auth0|5b96f61cf1642721ad84eeb6", requestID)
-
-	roles, err := auth0Client.Manager.User.Roles(requestID.(string))
-
-	assert.NoError(t, err)
-	req = jsonrpc.SetRoles(req, *roles)
+	reqContext := req.Context()
+	reqContext = context.WithValue(reqContext, jsonrpc.Keys.CompanyKey, "local")
+	reqContext = context.WithValue(reqContext, jsonrpc.Keys.RolesKey, []string{
+		"Admin",
+	})
+	req = req.WithContext(reqContext)
 
 	t.Run("admin role function", func(t *testing.T) {
 		verified, err := jsonrpc.AdminRole(req)
@@ -263,11 +75,24 @@ func TestRoleVerification(t *testing.T) {
 		assert.True(t, verified)
 	})
 
+	reqContext = req.Context()
+	reqContext = context.WithValue(reqContext, jsonrpc.Keys.RolesKey, []string{
+		"Owner",
+	})
+	req = req.WithContext(reqContext)
+
 	t.Run("owner role function", func(t *testing.T) {
 		verified, err := jsonrpc.OwnerRole(req)
 		assert.NoError(t, err)
 		assert.True(t, verified)
 	})
+
+	reqContext = req.Context()
+	reqContext = context.WithValue(reqContext, jsonrpc.Keys.RolesKey, []string{
+		"Admin",
+		"Owner",
+	})
+	req = req.WithContext(reqContext)
 
 	t.Run("verify all roles function", func(t *testing.T) {
 		verified := jsonrpc.VerifyAllRoles(req, jsonrpc.AdminRole, jsonrpc.OwnerRole)
@@ -276,12 +101,16 @@ func TestRoleVerification(t *testing.T) {
 		assert.False(t, verified)
 	})
 
+	reqContext = req.Context()
+	reqContext = context.WithValue(reqContext, jsonrpc.Keys.RolesKey, []string{
+		"Admin",
+	})
+	req = req.WithContext(reqContext)
+
 	t.Run("verify any role function", func(t *testing.T) {
 		verified := jsonrpc.VerifyAnyRole(req, jsonrpc.AdminRole, jsonrpc.OwnerRole)
 		assert.True(t, verified)
 		verified = jsonrpc.VerifyAnyRole(req, jsonrpc.AdminRole, jsonrpc.AnonymousRole)
 		assert.True(t, verified)
-
 	})
 }
-*/
