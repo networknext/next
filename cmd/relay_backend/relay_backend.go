c53a405cce2c32b6db6e8edbecc01e18efa82866
Ryan Jones <rpj5582@rit.edu>
2020-10-01 12:29:57 -0400

    add logs to relay backend to see cost and route matrices

--- cmd/relay_backend/relay_backend.go
+++ cmd/relay_backend/relay_backend.go
@@ -856,6 +856,8 @@ func main() {
 				}
 			}
 
+			fmt.Println("cost matrix:", costMatrix4)
+
 			routeEntries := core.Optimize(numRelays, numSegments, costMatrix4, 5, relayDatacenterIDs)
 			if len(routeEntries) == 0 {
 				level.Warn(logger).Log("matrix", "cost", "op", "optimize", "warn", "no route entries generated from cost matrix")
@@ -871,6 +873,26 @@ func main() {
 				RelayLongitudes:    relayLongitudes,
 				RelayDatacenterIDs: relayDatacenterIDs,
 				RouteEntries:       routeEntries,
+			}
+
+			fmt.Printf("%d route entries\n\n", len(routeEntries))
+			for _, routeEntry := range routeEntries {
+				for i := int32(0); i < routeEntry.NumRoutes; i++ {
+					routeCost := routeEntry.RouteCost[i]
+					routeString := "relays: "
+					for j := int32(0); j < routeEntry.RouteNumRelays[i]; j++ {
+						relayIndex := routeEntry.RouteRelays[i][j]
+						relayName := routeMatrix4New.RelayNames[relayIndex]
+						routeString += relayName
+						if j < routeEntry.RouteNumRelays[i]-1 {
+							routeString += " -> "
+						}
+					}
+
+					fmt.Printf("route cost: %d\n", routeCost)
+					fmt.Printf("route:\n")
+					fmt.Printf("\t%s\n\n", routeString)
+				}
 			}
 
 			routeMatrix4Mutex.Lock()
