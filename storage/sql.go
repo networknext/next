812489fa850031d8e7b01921d63c0f5f6c575194
John Bleichert <john@networknext.com>
2020-10-09 14:49:38 -0400

    Filled in simple read methods

--- storage/sql.go
+++ storage/sql.go
@@ -3,6 +3,8 @@ package storage
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"sort"
 	"sync"
 
 	"github.com/go-kit/kit/log"
@@ -32,18 +34,43 @@ type SQL struct {
 	sequenceNumberMutex sync.RWMutex
 }
 
-func (db *SQL) Customer(code string) (routing.Customer, error) {
+// Customer retrieves a Customer record using the company code
+func (db *SQL) Customer(companyCode string) (routing.Customer, error) {
 
-	return routing.Customer{}, nil
+	db.customerMutex.RLock()
+	defer db.customerMutex.RUnlock()
+
+	c, found := db.customers[companyCode]
+	if !found {
+		return routing.Customer{}, &DoesNotExistError{resourceType: "customer", resourceRef: fmt.Sprintf("%s", companyCode)}
+	}
+
+	return c, nil
 }
 
+// CustomerWithName retrieves a record using the customer's name
 func (db *SQL) CustomerWithName(name string) (routing.Customer, error) {
+	db.customerMutex.RLock()
+	defer db.customerMutex.RUnlock()
 
-	return routing.Customer{}, nil
+	for _, customer := range db.customers {
+		if customer.Name == name {
+			return customer, nil
+		}
+	}
+
+	return routing.Customer{}, &DoesNotExistError{resourceType: "customer", resourceRef: name}
 }
 
+// Customers retrieves the full list
 func (db *SQL) Customers() []routing.Customer {
-	return []routing.Customer{}
+	var customers []routing.Customer
+	for _, customer := range db.customers {
+		customers = append(customers, customer)
+	}
+
+	sort.Slice(customers, func(i int, j int) bool { return customers[i].Name < customers[j].Name })
+	return customers
 }
 
 func (db *SQL) AddCustomer(ctx context.Context, customer routing.Customer) error {
@@ -61,17 +88,42 @@ func (db *SQL) SetCustomer(ctx context.Context, customer routing.Customer) error
 // Buyer gets a copy of a buyer with the specified buyer ID,
 // and returns an empty buyer and an error if a buyer with that ID doesn't exist in storage.
 func (db *SQL) Buyer(id uint64) (routing.Buyer, error) {
-	return routing.Buyer{}, nil
+	db.buyerMutex.RLock()
+	defer db.buyerMutex.RUnlock()
+
+	b, found := db.buyers[id]
+	if !found {
+		return routing.Buyer{}, &DoesNotExistError{resourceType: "buyer", resourceRef: fmt.Sprintf("%x", id)}
+	}
+
+	return b, nil
 }
 
 // BuyerWithCompanyCode gets the Buyer with the matching company code
 func (db *SQL) BuyerWithCompanyCode(code string) (routing.Buyer, error) {
-	return routing.Buyer{}, nil
+	db.buyerMutex.RLock()
+	defer db.buyerMutex.RUnlock()
+
+	for _, buyer := range db.buyers {
+		if buyer.CompanyCode == code {
+			return buyer, nil
+		}
+	}
+	return routing.Buyer{}, &DoesNotExistError{resourceType: "buyer", resourceRef: code}
 }
 
 // Buyers returns a copy of all stored buyers.
 func (db *SQL) Buyers() []routing.Buyer {
-	return []routing.Buyer{}
+	db.buyerMutex.RLock()
+	defer db.buyerMutex.RUnlock()
+
+	var buyers []routing.Buyer
+	for _, buyer := range db.buyers {
+		buyers = append(buyers, buyer)
+	}
+
+	sort.Slice(buyers, func(i int, j int) bool { return buyers[i].ID < buyers[j].ID })
+	return buyers
 }
 
 // AddBuyer adds the provided buyer to storage and returns an error if the buyer could not be added.
@@ -92,12 +144,29 @@ func (db *SQL) SetBuyer(ctx context.Context, buyer routing.Buyer) error {
 // Seller gets a copy of a seller with the specified seller ID,
 // and returns an empty seller and an error if a seller with that ID doesn't exist in storage.
 func (db *SQL) Seller(id string) (routing.Seller, error) {
-	return routing.Seller{}, nil
+	db.sellerMutex.RLock()
+	defer db.sellerMutex.RUnlock()
+
+	s, found := db.sellers[id]
+	if !found {
+		return routing.Seller{}, &DoesNotExistError{resourceType: "seller", resourceRef: id}
+	}
+
+	return s, nil
 }
 
 // Sellers returns a copy of all stored sellers.
 func (db *SQL) Sellers() []routing.Seller {
-	return []routing.Seller{}
+	db.sellerMutex.RLock()
+	defer db.sellerMutex.RUnlock()
+
+	var sellers []routing.Seller
+	for _, seller := range db.sellers {
+		sellers = append(sellers, seller)
+	}
+
+	sort.Slice(sellers, func(i int, j int) bool { return sellers[i].ID < sellers[j].ID })
+	return sellers
 }
 
 // AddSeller adds the provided seller to storage and returns an error if the seller could not be added.
@@ -128,7 +197,15 @@ func (db *SQL) SellerIDFromCustomerName(ctx context.Context, customerName string
 }
 
 func (db *SQL) SellerWithCompanyCode(code string) (routing.Seller, error) {
-	return routing.Seller{}, nil
+	db.sellerMutex.RLock()
+	defer db.sellerMutex.RUnlock()
+
+	for _, seller := range db.sellers {
+		if seller.CompanyCode == code {
+			return seller, nil
+		}
+	}
+	return routing.Seller{}, &DoesNotExistError{resourceType: "seller", resourceRef: code}
 }
 
 // SetCustomerLink update the customer's buyer and seller references.
@@ -139,12 +216,29 @@ func (db *SQL) SetCustomerLink(ctx context.Context, customerName string, buyerID
 // Relay gets a copy of a relay with the specified relay ID
 // and returns an empty relay and an error if a relay with that ID doesn't exist in storage.
 func (db *SQL) Relay(id uint64) (routing.Relay, error) {
-	return routing.Relay{}, nil
+	db.relayMutex.RLock()
+	defer db.relayMutex.RUnlock()
+
+	relay, found := db.relays[id]
+	if !found {
+		return routing.Relay{}, &DoesNotExistError{resourceType: "relay", resourceRef: fmt.Sprintf("%x", id)}
+	}
+
+	return relay, nil
 }
 
 // Relays returns a copy of all stored relays.
 func (db *SQL) Relays() []routing.Relay {
-	return []routing.Relay{}
+	db.relayMutex.RLock()
+	defer db.relayMutex.RUnlock()
+
+	var relays []routing.Relay
+	for _, relay := range db.relays {
+		relays = append(relays, relay)
+	}
+
+	sort.Slice(relays, func(i int, j int) bool { return relays[i].ID < relays[j].ID })
+	return relays
 }
 
 // AddRelay adds the provided relay to storage and returns an error if the relay could not be added.
@@ -165,12 +259,29 @@ func (db *SQL) SetRelay(ctx context.Context, relay routing.Relay) error {
 // Datacenter gets a copy of a datacenter with the specified datacenter ID
 // and returns an empty datacenter and an error if a datacenter with that ID doesn't exist in storage.
 func (db *SQL) Datacenter(datacenterID uint64) (routing.Datacenter, error) {
-	return routing.Datacenter{}, nil
+	db.datacenterMutex.RLock()
+	defer db.datacenterMutex.RUnlock()
+
+	d, found := db.datacenters[datacenterID]
+	if !found {
+		return routing.Datacenter{}, &DoesNotExistError{resourceType: "datacenter", resourceRef: datacenterID}
+	}
+
+	return d, nil
 }
 
 // Datacenters returns a copy of all stored datacenters.
 func (db *SQL) Datacenters() []routing.Datacenter {
-	return []routing.Datacenter{}
+	db.datacenterMutex.RLock()
+	defer db.datacenterMutex.RUnlock()
+
+	var datacenters []routing.Datacenter
+	for _, datacenter := range db.datacenters {
+		datacenters = append(datacenters, datacenter)
+	}
+
+	sort.Slice(datacenters, func(i int, j int) bool { return datacenters[i].ID < datacenters[j].ID })
+	return datacenters
 }
 
 // AddDatacenter adds the provided datacenter to storage and returns an error if the datacenter could not be added.
