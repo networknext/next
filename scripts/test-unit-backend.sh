b161b3917ee3750e262f1ee3603476b9744f90f1
John Bleichert <john@networknext.com>
2020-10-28 14:34:58 -0400

    removed test -v

--- scripts/test-unit-backend.sh
+++ scripts/test-unit-backend.sh
@@ -28,7 +28,7 @@ if [[ ! -z "$PUBSUB_EMULATOR_HOST" ]]; then
 fi
 
 printf "\nRunning unit tests:\n\n"
-go test -v ./... -coverprofile ./cover.out -timeout 30s
+go test ./... -coverprofile ./cover.out -timeout 30s
 testResult=$?
 if [ ! $testResult -eq 0 ]; then
     exit $testResult
