c9c49b08da1c665cc20ad00d6ede2a6e883f17e6
Andrew Baumbach <andrew@networknext.com>
2020-11-09 17:23:01 -0500

    Fixed tests

--- tests/unit/SettingsWorkspace.spec.ts
+++ tests/unit/SettingsWorkspace.spec.ts
@@ -2,12 +2,39 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import SettingsWorkspace from '@/workspaces/SettingsWorkspace.vue'
 import Vuex from 'vuex'
 import { JSONRPCPlugin } from '@/plugins/jsonrpc'
+import { FlagPlugin } from '@/plugins/flags'
 
 describe('SettingsWorkspace.vue', () => {
   const localVue = createLocalVue()
 
   localVue.use(Vuex)
   localVue.use(JSONRPCPlugin)
+  localVue.use(FlagPlugin, {
+    flags: [
+      {
+        name: 'FEATURE_EXPLORE',
+        description: 'Integrate Looker into the portal under a new navigation tab called "Explore"',
+        value: 'false'
+      },
+      {
+        name: 'FEATURE_INTERCOM',
+        description: 'Integrate intercom',
+        value: 'false'
+      },
+      {
+        name: 'FEATURE_ROUTE_SHADER',
+        description: 'Route shader page for users to update their route shader',
+        value: 'false'
+      },
+      {
+        name: 'FEATURE_IMPERSONATE',
+        description: 'Feature to allow admins to impersonate a customer in a read only state',
+        value: 'false'
+      }
+    ],
+    useAPI: process.env.VUE_APP_USE_API_FLAGS
+    // apiService: Vue.prototype.$apiService
+  })
 
   const defaultStore = new Vuex.Store({
     state: {
