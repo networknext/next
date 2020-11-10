e5347144f45afba31a8015ef58ccf7f705ab6a5a
Ryan Jones <rpj5582@rit.edu>
2020-11-10 12:27:28 -0500

    back to no reconnect

--- cmd/load_test_client/load_test_client.cpp
+++ cmd/load_test_client/load_test_client.cpp
@@ -113,16 +113,16 @@ int main()
     uint8_t packet_data[32];
     memset( packet_data, 0, sizeof( packet_data ) );
 
-    double connect_time = 0;
-    double game_length = 0;
+    double wait_time = rand() % 120;
+
+    bool initial_connect = false;
 
     while ( !quit )
     {
-        if ( next_time() - connect_time > game_length )
+        if ( !initial_connect && next_time() > wait_time )
         {
             next_client_open_session( client, server_addrs[rand() % cores].c_str() );
-            connect_time = next_time();
-            game_length = (rand() % 300) + 300;
+            initial_connect = true;
         }
 
         next_client_send_packet( client, packet_data, sizeof( packet_data ) );
