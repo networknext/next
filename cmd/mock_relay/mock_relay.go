ce89c7ef8abee44f1ef61bbdcf82b45070a0a37f
Glenn Fiedler <glenn@networknext.com>
2020-11-18 13:27:41 -0500

    more work on the mock relay

--- cmd/mock_relay/mock_relay.go
+++ cmd/mock_relay/mock_relay.go
@@ -3,9 +3,109 @@ package main
 import (
 	"fmt"
 	"os"
+	"math"
 	"encoding/base64"
+	"encoding/binary"
 	"github.com/networknext/backend/modules/core"	
 )
+
+const InitRequestMagic = uint32(0x9083708f)
+const InitRequestVersion = 0
+const InitResponseVersion = 0
+const UpdateRequestVersion = 0
+const UpdateResponseVersion = 0
+const MaxRelayAddressLength = 256
+const RelayTokenBytes = 32
+const MaxRelays = 5
+
+func ReadUint32(data []byte, index *int, value *uint32) bool {
+	if *index+4 > len(data) {
+		return false
+	}
+	*value = binary.LittleEndian.Uint32(data[*index:])
+	*index += 4
+	return true
+}
+
+func ReadUint64(data []byte, index *int, value *uint64) bool {
+	if *index+8 > len(data) {
+		return false
+	}
+	*value = binary.LittleEndian.Uint64(data[*index:])
+	*index += 8
+	return true
+}
+
+func ReadFloat32(data []byte, index *int, value *float32) bool {
+	var int_value uint32
+	if !ReadUint32(data, index, &int_value) {
+		return false
+	}
+	*value = math.Float32frombits(int_value)
+	return true
+}
+
+func ReadString(data []byte, index *int, value *string, maxStringLength uint32) bool {
+	var stringLength uint32
+	if !ReadUint32(data, index, &stringLength) {
+		return false
+	}
+	if stringLength > maxStringLength {
+		return false
+	}
+	if *index+int(stringLength) > len(data) {
+		return false
+	}
+	stringData := make([]byte, stringLength)
+	for i := uint32(0); i < stringLength; i++ {
+		stringData[i] = data[*index]
+		*index += 1
+	}
+	*value = string(stringData)
+	return true
+}
+
+func ReadBytes(data []byte, index *int, value *[]byte, bytes uint32) bool {
+	if *index+int(bytes) > len(data) {
+		return false
+	}
+	*value = make([]byte, bytes)
+	for i := uint32(0); i < bytes; i++ {
+		(*value)[i] = data[*index]
+		*index += 1
+	}
+	return true
+}
+
+func WriteUint32(data []byte, index *int, value uint32) {
+	binary.LittleEndian.PutUint32(data[*index:], value)
+	*index += 4
+}
+
+func WriteUint64(data []byte, index *int, value uint64) {
+	binary.LittleEndian.PutUint64(data[*index:], value)
+	*index += 8
+}
+
+func WriteString(data []byte, index *int, value string, maxStringLength uint32) {
+	stringLength := uint32(len(value))
+	if stringLength > maxStringLength {
+		panic("string is too long!\n")
+	}
+	binary.LittleEndian.PutUint32(data[*index:], stringLength)
+	*index += 4
+	for i := 0; i < int(stringLength); i++ {
+		data[*index] = value[i]
+		*index++
+	}
+}
+
+func WriteBytes(data []byte, index *int, value []byte, numBytes int) {
+	for i := 0; i < numBytes; i++ {
+		data[*index] = value[i]
+		*index++
+	}
+}
 
 func main() {
 
@@ -91,14 +191,9 @@ func main() {
 
 	// init the relay
 
-	const InitRequestMagic = uint32(0x9083708f)
+	initData := make([]byte, 1024)
 
-	initRequestVersion := uint32(0)
-
-	initVersion := make([]byte, 1024)
-
-	_ = initRequestVersion
-	_ = initVersion
+	_ = initData
 
 	// todo: nonce
 	/*
