fee9564930bd493cb8dfd3cd8e6723d844ef367c
Ryan Jones <rpj5582@rit.edu>
2020-12-08 15:58:46 -0500

    hack for ESL and local buyer to use pro mode

--- modules/transport/server_handlers.go
+++ modules/transport/server_handlers.go
@@ -488,6 +488,19 @@ func SessionUpdateHandlerFunc(logger log.Logger, getIPLocator func(sessionID uin
 			debug = new(string)
 		}
 
+		// Hack to support ESL - if a player has a "pro" tag, use pro mode in the route shader
+		// Do this for ESL or local, for testing
+		if buyer.ID == 0x1e4e8804454033c8 || buyer.ID == 0xbdbebdbf0f7be395 {
+			if packet.NumTags > 0 {
+				for i := int32(0); i < packet.NumTags; i++ {
+					if packet.Tags[i] == crypto.HashID("pro") {
+						buyer.RouteShader.ProMode = true
+						break
+					}
+				}
+			}
+		}
+
 		if datacenter, err = getDatacenter(storer, packet.CustomerID, packet.DatacenterID, ""); err != nil {
 			level.Error(logger).Log("handler", "session_update", "err", err)
 
