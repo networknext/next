3cc126ec5ff68c8c96eb5c397e8f1c9be7bef16c
Glenn Fiedler <glenn@networknext.com>
2020-12-29 13:59:58 -0500

    add beacon service

---
+++ cmd/beacon/beacon.go
@@ -0,0 +1,177 @@
+/*
+   Network Next. You control the network.
+   Copyright © 2017 - 2020 Network Next, Inc. All rights reserved.
+*/
+
+package main
+
+import (
+	"context"
+	"expvar"
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+	"syscall"
+
+	"os"
+	"os/signal"
+
+	"github.com/go-kit/kit/log/level"
+	"github.com/gorilla/mux"
+
+	"github.com/networknext/backend/modules/backend"
+	"github.com/networknext/backend/modules/envvar"
+	"github.com/networknext/backend/modules/transport"
+	"golang.org/x/sys/unix"
+)
+
+// Allows us to return an exit code and allows log flushes and deferred functions
+// to finish before exiting.
+func main() {
+	os.Exit(mainReturnWithCode())
+}
+
+func mainReturnWithCode() int {
+
+	serviceName := "beacon"
+
+	ctx := context.Background()
+
+	gcpProjectID := backend.GetGCPProjectID()
+
+	logger, err := backend.GetLogger(ctx, gcpProjectID, serviceName)
+	if err != nil {
+		level.Error(logger).Log("err", err)
+		return 1
+	}
+
+	env, err := backend.GetEnv()
+	if err != nil {
+		level.Error(logger).Log("err", err)
+		return 1
+	}
+
+	if gcpProjectID != "" {
+		if err := backend.InitStackDriverProfiler(gcpProjectID, serviceName, env); err != nil {
+			level.Error(logger).Log("msg", "failed to initialze StackDriver profiler", "err", err)
+			return 1
+		}
+	}
+
+	// Start HTTP server
+	{
+		router := mux.NewRouter()
+		router.HandleFunc("/health", transport.HealthHandlerFunc())
+		router.Handle("/debug/vars", expvar.Handler())
+
+		go func() {
+			httpPort := envvar.Get("HTTP_PORT", "40001")
+
+			err := http.ListenAndServe(":"+httpPort, router)
+			if err != nil {
+				level.Error(logger).Log("err", err)
+				return
+			}
+		}()
+	}
+
+	numThreads, err := envvar.GetInt("NUM_THREADS", 1)
+	if err != nil {
+		level.Error(logger).Log("err", err)
+		return 1
+	}
+
+	readBuffer, err := envvar.GetInt("READ_BUFFER", 100000)
+	if err != nil {
+		level.Error(logger).Log("err", err)
+		return 1
+	}
+
+	writeBuffer, err := envvar.GetInt("WRITE_BUFFER", 100000)
+	if err != nil {
+		level.Error(logger).Log("err", err)
+		return 1
+	}
+
+	udpPort := envvar.Get("UDP_PORT", "30000")
+
+	var wg sync.WaitGroup
+
+	wg.Add(numThreads)
+
+	lc := net.ListenConfig{
+		Control: func(network string, address string, c syscall.RawConn) error {
+			err := c.Control(func(fileDescriptor uintptr) {
+				err := unix.SetsockoptInt(int(fileDescriptor), unix.SOL_SOCKET, unix.SO_REUSEADDR, 1)
+				if err != nil {
+					panic(fmt.Sprintf("failed to set reuse address socket option: %v", err))
+				}
+
+				err = unix.SetsockoptInt(int(fileDescriptor), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+				if err != nil {
+					panic(fmt.Sprintf("failed to set reuse port socket option: %v", err))
+				}
+			})
+
+			return err
+		},
+	}
+
+	for i := 0; i < numThreads; i++ {
+		go func(thread int) {
+			lp, err := lc.ListenPacket(ctx, "udp", "0.0.0.0:"+udpPort)
+			if err != nil {
+				panic(fmt.Sprintf("could not bind socket: %v", err))
+			}
+
+			conn := lp.(*net.UDPConn)
+			defer conn.Close()
+
+			if err := conn.SetReadBuffer(readBuffer); err != nil {
+				panic(fmt.Sprintf("could not set connection read buffer size: %v", err))
+			}
+
+			if err := conn.SetWriteBuffer(writeBuffer); err != nil {
+				panic(fmt.Sprintf("could not set connection write buffer size: %v", err))
+			}
+
+			dataArray := [transport.DefaultMaxPacketSize]byte{}
+			for {
+				data := dataArray[:]
+				size, fromAddr, err := conn.ReadFromUDP(data)
+				if err != nil {
+					level.Error(logger).Log("msg", "failed to read UDP packet", "err", err)
+					break
+				}
+
+				if size <= 1 {
+					continue
+				}
+
+				data = data[:size]
+
+				if data[0] != 118 {
+					continue
+				}
+
+				fmt.Printf("beacon packet\n")
+
+				// todo
+				_ = fromAddr
+
+			}
+
+			wg.Done()
+		}(i)
+	}
+
+	level.Info(logger).Log("msg", "waiting for incoming connections")
+
+	// Wait for interrupt signal
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+	<-sigint
+
+	return 0
+}
