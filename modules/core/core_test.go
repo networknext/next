e39885b63380312a5982e0bb584209f3c4ed9be2
Glenn Fiedler <glenn@networknext.com>
2021-01-02 17:58:15 -0500

    done with take network next tests conversion

--- modules/core/core_test.go
+++ modules/core/core_test.go
@@ -3314,50 +3314,31 @@ func TestTakeNetworkNext_ProMode_MultipathVeto(t *testing.T) {
 
 	env.SetCost("losangeles", "chicago", 10)
 
-	costMatrix, numRelays := env.GetCostMatrix()
+	test := NewTestData(env)
 
-	relayDatacenters := env.GetRelayDatacenters()
+	test.directLatency = int32(20)
+	test.directPacketLoss = float32(0.0)
 
-	numSegments := numRelays
+	test.sourceRelays = []int32{0}
+	test.sourceRelayCosts = []int32{10}
 
-	routeMatrix := Optimize(numRelays, numSegments, costMatrix, 5, relayDatacenters)
+	test.destRelays = []int32{1}
 
-	directLatency := int32(20)
-	directPacketLoss := float32(0.0)
+	test.routeShader.ProMode = true
 
-	sourceRelays := []int32{0}
-	sourceRelayCosts := []int32{10}
+	test.routeState.UserID = 100
 
-	destRelays := []int32{1}
+	test.multipathVetoUsers[test.routeState.UserID] = true
 
-	routeCost := int32(0)
-	routeNumRelays := int32(0)
-	routeRelays := [MaxRelaysPerRoute]int32{}
-
-	routeShader := NewRouteShader()
-	routeState := RouteState{}
-	multipathVetoUsers := map[uint64]bool{}
-	internal := NewInternalConfig()
-
-	routeShader.ProMode = true
-
-	routeState.UserID = 100
-
-	multipathVetoUsers[routeState.UserID] = true
-
-	debug := ""
-
-	routeDiversity := int32(0)
-
-	result := MakeRouteDecision_TakeNetworkNext(routeMatrix, &routeShader, &routeState, multipathVetoUsers, &internal, directLatency, directPacketLoss, sourceRelays, sourceRelayCosts, destRelays, &routeCost, &routeNumRelays, routeRelays[:], &routeDiversity, &debug)
+	result := test.TakeNetworkNext()
 
 	assert.False(t, result)
 
 	expectedRouteState := RouteState{}
 	expectedRouteState.UserID = 100
 
-	assert.Equal(t, expectedRouteState, routeState)
-	assert.Equal(t, int32(0), routeDiversity)
+	assert.Equal(t, expectedRouteState, test.routeState)
+	assert.Equal(t, int32(0), test.routeDiversity)
 }
 
 // -----------------------------------------------------------------------------
