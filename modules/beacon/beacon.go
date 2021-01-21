639663e18896d7f95c57f5b4b2c02f3c04c9c185
nbopardi <nikhilb@gmail.com>
2021-01-21 13:23:13 -0800

    setup beacon bigquery

--- modules/beacon/beacon.go
+++ modules/beacon/beacon.go
@@ -9,14 +9,36 @@ import (
 	"github.com/networknext/backend/modules/encoding"
 )
 
+const (
+	NEXT_CONNECTION_TYPE_UNKNOWN  = 0
+	NEXT_CONNECTION_TYPE_WIRED    = 1
+	NEXT_CONNECTION_TYPE_WIFI     = 2
+	NEXT_CONNECTION_TYPE_CELLULAR = 3
+	NEXT_CONNECTION_TYPE_MAX      = 3
+)
+
+const (
+	NEXT_PLATFORM_UNKNOWN       = 0
+	NEXT_PLATFORM_WINDOWS       = 1
+	NEXT_PLATFORM_MAC           = 2
+	NEXT_PLATFORM_UNIX          = 3
+	NEXT_PLATFORM_SWITCH        = 4
+	NEXT_PLATFORM_PS4           = 5
+	NEXT_PLATFORM_IOS           = 6
+	NEXT_PLATFORM_XBOX_ONE      = 7
+	NEXT_PLATFORM_XBOX_SERIES_X = 8
+	NEXT_PLATFORM_PS5           = 9
+	NEXT_PLATFORM_MAX           = 9
+)
+
 type NextBeaconPacket struct {
 	Version          uint32
-	CustomerId       uint64
-	DatacenterId     uint64
+	CustomerID       uint64
+	DatacenterID     uint64
 	UserHash         uint64
 	AddressHash      uint64
-	SessionId        uint64
-	PlatformId       int32
+	SessionID        uint64
+	PlatformID       int32
 	ConnectionType   int32
 	Enabled          bool
 	Upgraded         bool
@@ -33,22 +55,22 @@ func (packet *NextBeaconPacket) Serialize(stream encoding.Stream) error {
 	stream.SerializeBool(&packet.Next)
 	stream.SerializeBool(&packet.FallbackToDirect)
 
-	hasDatacenterId := stream.IsWriting() && packet.DatacenterId != 0
-	stream.SerializeBool(&hasDatacenterId)
+	hasDatacenterID := stream.IsWriting() && packet.DatacenterID != 0
+	stream.SerializeBool(&hasDatacenterID)
 
-	stream.SerializeUint64(&packet.CustomerId)
+	stream.SerializeUint64(&packet.CustomerID)
 
-	if hasDatacenterId {
-		stream.SerializeUint64(&packet.DatacenterId)
+	if hasDatacenterID {
+		stream.SerializeUint64(&packet.DatacenterID)
 	}
 
 	if packet.Upgraded {
 		stream.SerializeUint64(&packet.UserHash)
 		stream.SerializeUint64(&packet.AddressHash)
-		stream.SerializeUint64(&packet.SessionId)
+		stream.SerializeUint64(&packet.SessionID)
 	}
 
-	stream.SerializeInteger(&packet.PlatformId, NEXT_PLATFORM_UNKNOWN, NEXT_PLATFORM_MAX)
+	stream.SerializeInteger(&packet.PlatformID, NEXT_PLATFORM_UNKNOWN, NEXT_PLATFORM_MAX)
 
 	stream.SerializeInteger(&packet.ConnectionType, NEXT_CONNECTION_TYPE_UNKNOWN, NEXT_CONNECTION_TYPE_MAX)
 
--- modules/beacon/googlebigquery.go
+++ modules/beacon/googlebigquery.go
@@ -18,22 +18,22 @@ const (
 )
 
 type GoogleBigQueryClient struct {
-	Metrics       *metrics.BillingMetrics
+	Metrics       *metrics.BeaconMetrics
 	Logger        log.Logger
 	TableInserter *bigquery.Inserter
 	BatchSize     int
 
-	buffer      []*NextBeaconPacket
-	bufferMutex sync.RWMutex
+	buffer        []*NextBeaconPacket
+	bufferMutex   sync.RWMutex
 
-	entries chan *NextBeaconPacket
+	entries 	  chan *NextBeaconPacket
 }
 
 // Bill pushes an Entry to the channel
-func (bq *GoogleBigQueryClient) Bill(ctx context.Context, entry *BillingEntry) error {
+func (bq *GoogleBigQueryClient) SendBeaconEntry(ctx context.Context, entry *NextBeaconPacket) error {
 	bq.Metrics.EntriesSubmitted.Add(1)
 	if bq.entries == nil {
-		bq.entries = make(chan *BillingEntry, DefaultBigQueryChannelSize)
+		bq.entries = make(chan *NextBeaconPacket, DefaultBigQueryChannelSize)
 	}
 
 	bq.bufferMutex.RLock()
@@ -57,7 +57,7 @@ func (bq *GoogleBigQueryClient) Bill(ctx context.Context, entry *BillingEntry) e
 // only be called from 1 goroutine to avoid using a mutex around the internal buffer slice
 func (bq *GoogleBigQueryClient) WriteLoop(ctx context.Context) error {
 	if bq.entries == nil {
-		bq.entries = make(chan *BillingEntry, DefaultBigQueryChannelSize)
+		bq.entries = make(chan *NextBeaconPacket, DefaultBigQueryChannelSize)
 	}
 	for entry := range bq.entries {
 		bq.Metrics.EntriesQueued.Set(float64(len(bq.entries)))
@@ -70,7 +70,7 @@ func (bq *GoogleBigQueryClient) WriteLoop(ctx context.Context) error {
 				bq.bufferMutex.Unlock()
 
 				level.Error(bq.Logger).Log("msg", "failed to write to BigQuery", "err", err)
-				bq.Metrics.ErrorMetrics.BillingWriteFailure.Add(float64(bufferLength))
+				bq.Metrics.ErrorMetrics.BeaconWriteFailure.Add(float64(bufferLength))
 				continue
 			}
 
@@ -88,181 +88,21 @@ func (bq *GoogleBigQueryClient) WriteLoop(ctx context.Context) error {
 // Save implements the bigquery.ValueSaver interface for an Entry
 // so it can be used in Put()
 func (entry *NextBeaconPacket) Save() (map[string]bigquery.Value, string, error) {
-
 	e := make(map[string]bigquery.Value)
 
-	e["timestamp"] = int(entry.Timestamp)
-	e["buyerId"] = int(entry.BuyerID)
-	e["sessionId"] = int(entry.SessionID)
-	e["sliceNumber"] = int(entry.SliceNumber)
-	e["directRTT"] = entry.DirectRTT
-	e["directJitter"] = entry.DirectJitter
-	e["directPacketLoss"] = entry.DirectPacketLoss
-	e["userHash"] = int(entry.UserHash)
-
-	if entry.Next {
-		e["next"] = entry.Next
-		e["nextRTT"] = entry.NextRTT
-		e["nextJitter"] = entry.NextJitter
-		e["nextPacketLoss"] = entry.NextPacketLoss
-	}
-
-	nextRelays := make([]bigquery.Value, entry.NumNextRelays)
-	for i := 0; i < int(entry.NumNextRelays); i++ {
-		nextRelays[i] = int(entry.NextRelays[i])
-	}
-	e["nextRelays"] = nextRelays
-
-	e["totalPrice"] = int(entry.TotalPrice)
-
-	if entry.ClientToServerPacketsLost > 0 {
-		e["clientToServerPacketsLost"] = int(entry.ClientToServerPacketsLost)
-	}
-
-	if entry.ServerToClientPacketsLost > 0 {
-		e["serverToClientPacketsLost"] = int(entry.ServerToClientPacketsLost)
-	}
-
-	e["committed"] = entry.Committed
-	e["flagged"] = entry.Flagged
-	e["multipath"] = entry.Multipath
-
-	if entry.Next {
-		e["initial"] = entry.Initial
-		e["nextBytesUp"] = int(entry.NextBytesUp)
-		e["nextBytesDown"] = int(entry.NextBytesDown)
-		e["envelopeBytesUp"] = int(entry.EnvelopeBytesUp)
-		e["envelopeBytesDown"] = int(entry.EnvelopeBytesDown)
-	}
-
+	e["version"] = int(entry.Version)
+	e["customerID"] = int(entry.CustomerID)
 	e["datacenterID"] = int(entry.DatacenterID)
-
-	if entry.Next {
-		e["rttReduction"] = entry.RTTReduction
-		e["packetLossReduction"] = entry.PacketLossReduction
-	}
-
-	nextRelaysPrice := make([]bigquery.Value, entry.NumNextRelays)
-	for i := 0; i < int(entry.NumNextRelays); i++ {
-		nextRelaysPrice[i] = int(entry.NextRelaysPrice[i])
-	}
-	e["nextRelaysPrice"] = nextRelaysPrice
-
-	e["latitude"] = entry.Latitude
-	e["longitude"] = entry.Longitude
-	e["isp"] = entry.ISP
-	e["abTest"] = entry.ABTest
-	e["routeDecision"] = int(entry.RouteDecision)
-
+	e["userHash"] = int(entry.UserHash)
+	e["addressHash"] = int(entry.AddressHash)
+	e["sessionID"] = int(entry.SessionID)
+	e["platformID"] = int(entry.PlatformID)
 	e["connectionType"] = int(entry.ConnectionType)
-	e["platformType"] = int(entry.PlatformType)
-	e["sdkVersion"] = entry.SDKVersion
 
-	if entry.PacketLoss > 0.0 {
-		e["packetLoss"] = entry.PacketLoss
-	}
-
-	if entry.PredictedNextRTT > 0.0 {
-		e["predictedNextRTT"] = entry.PredictedNextRTT
-	}
-
-	e["multipathVetoed"] = entry.MultipathVetoed
-
-	if entry.UseDebug && entry.Debug != "" {
-		e["debug"] = entry.Debug
-	}
-
+	e["enabled"] = entry.Enabled
+	e["upgraded"] = entry.Upgraded
+	e["next"] = entry.Next
 	e["fallbackToDirect"] = entry.FallbackToDirect
-
-	if entry.ClientFlags != 0 {
-		e["clientFlags"] = int(entry.ClientFlags)
-	}
-
-	if entry.UserFlags != 0 {
-		e["userFlags"] = int(entry.UserFlags)
-	}
-
-	if entry.NearRelayRTT != 0 {
-		e["nearRelayRTT"] = entry.NearRelayRTT
-	}
-
-	if entry.PacketsOutOfOrderClientToServer != 0 {
-		e["packetsOutOfOrderClientToServer"] = int(entry.PacketsOutOfOrderClientToServer)
-	}
-
-	if entry.PacketsOutOfOrderServerToClient != 0 {
-		e["packetsOutOfOrderServerToClient"] = int(entry.PacketsOutOfOrderServerToClient)
-	}
-
-	if entry.JitterClientToServer != 0 {
-		e["jitterClientToServer"] = entry.JitterClientToServer
-	}
-
-	if entry.JitterServerToClient != 0 {
-		e["jitterServerToClient"] = entry.JitterServerToClient
-	}
-
-	if entry.UseDebug {
-		if entry.NumNearRelays != 0 {
-			e["numNearRelays"] = int(entry.NumNearRelays)
-
-			nearRelayIDs := make([]bigquery.Value, entry.NumNearRelays)
-			for i := 0; i < int(entry.NumNearRelays); i++ {
-				nearRelayIDs[i] = int(entry.NearRelayIDs[i])
-			}
-			e["nearRelayIDs"] = nearRelayIDs
-
-			nearRelayRTTs := make([]bigquery.Value, entry.NumNearRelays)
-			for i := 0; i < int(entry.NumNearRelays); i++ {
-				nearRelayRTTs[i] = entry.NearRelayRTTs[i]
-			}
-			e["nearRelayRTTs"] = nearRelayRTTs
-
-			nearRelayJitters := make([]bigquery.Value, entry.NumNearRelays)
-			for i := 0; i < int(entry.NumNearRelays); i++ {
-				nearRelayJitters[i] = entry.NearRelayJitters[i]
-			}
-			e["nearRelayJitters"] = nearRelayJitters
-
-			nearRelayPacketLosses := make([]bigquery.Value, entry.NumNearRelays)
-			for i := 0; i < int(entry.NumNearRelays); i++ {
-				nearRelayPacketLosses[i] = entry.NearRelayPacketLosses[i]
-			}
-			e["nearRelayPacketLosses"] = nearRelayPacketLosses
-		}
-	}
-
-	e["relayWentAway"] = entry.RelayWentAway
-	e["routeLost"] = entry.RouteLost
-
-	if entry.NumTags > 0 {
-		tags := make([]bigquery.Value, entry.NumTags)
-		for i := 0; i < int(entry.NumTags); i++ {
-			tags[i] = int(entry.Tags[i])
-		}
-		e["tags"] = tags
-	}
-
-	e["mispredicted"] = entry.Mispredicted
-	e["vetoed"] = entry.Vetoed
-
-	e["latencyWorse"] = entry.LatencyWorse
-	e["noRoute"] = entry.NoRoute
-	e["nextLatencyTooHigh"] = entry.NextLatencyTooHigh
-	e["routeChanged"] = entry.RouteChanged
-	e["commitVeto"] = entry.CommitVeto
-
-	if entry.RouteDiversity > 0 {
-		e["routeDiversity"] = entry.RouteDiversity
-	}
-
-	if entry.LackOfDiversity {
-		e["lackOfDiversity"] = entry.LackOfDiversity
-	}
-
-	if entry.Pro {
-		e["pro"] = entry.Pro
-	}
 
 	return e, "", nil
 }
