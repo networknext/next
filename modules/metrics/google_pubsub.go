0ad5653c1dedef3587915b3ba3abb119d9963591
nbopardi <nikhilb@gmail.com>
2021-03-18 15:45:28 -0700

    update google pubsub metrics

--- modules/metrics/google_pubsub.go
+++ modules/metrics/google_pubsub.go
@@ -2,81 +2,193 @@ package metrics
 
 import "context"
 
+// GooglePublisherMetrics defines the metrics for publishing to Google Pub/Sub.
 type GooglePublisherMetrics struct {
-    EntriesQueuedToPublish Counter
-    EntriesPublished Counter
-    ErrorMetrics GooglePublisherErrorMetrics
+	EntriesQueuedToPublish Counter
+	EntriesPublished       Counter
+	ErrorMetrics           GooglePublisherErrorMetrics
 }
 
-// EmptyBeaconMetrics is used for testing when we want to pass in metrics but don't care about their value.
+// EmptyGooglePublisherMetrics is used for testing when we want to pass in metrics but don't care about their value.
 var EmptyGooglePublisherMetrics GooglePublisherMetrics = GooglePublisherMetrics{
-    EntriesQueuedToPublish: &EmptyCounter{},
-    EntriesPublished:          &EmptyCounter{},
-    ErrorMetrics:              EmptyGooglePublisherErrorMetrics,
+	EntriesQueuedToPublish: &EmptyCounter{},
+	EntriesPublished:       &EmptyCounter{},
+	ErrorMetrics:           EmptyGooglePublisherErrorMetrics,
 }
 
+// GooglePublisherErrorMetrics contains the error metrics for the Google Pub/Sub publisher.
 type GooglePublisherErrorMetrics struct {
-    PublishFailure Counter
+	PublishFailure Counter
 }
 
+// EmptyGooglePublisherErrorMetrics is used for testing when we want to pass in metrics but don't care about their value.
 var EmptyGooglePublisherErrorMetrics = GooglePublisherErrorMetrics{
-    PublishFailure: &EmptyCounter{},
+	PublishFailure: &EmptyCounter{},
 }
 
+// GoogleSubscriberMetrics defines the metrics for subscribing to Google Pub/Sub.
 type GoogleSubscriberMetrics struct {
-    EntriesReceived Counter
-    EntriesQueuedToWrite Counter
-    EntriesSubmitted Counter
-    ErrorMetrics GoogleSubscriberErrorMetrics
+	EntriesReceived      Counter
+	EntriesQueuedToWrite Counter
+	EntriesSubmitted     Counter
+	ErrorMetrics         GoogleSubscriberErrorMetrics
 }
 
+// EmptyGoogleSubscriberMetrics is used for testing when we want to pass in metrics but don't care about their value.
 var EmptyGoogleSubscriberMetrics GoogleSubscriberMetrics = GoogleSubscriberMetrics{
-    EntriesReceived: &EmptyCounter{},
-    EntriesQueuedToWrite: &EmptyCounter{},
-    EntriesSubmitted: &EmptyCounter{},
-    ErrorMetrics: EmptyGoogleSubscriberErrorMetrics,
+	EntriesReceived:      &EmptyCounter{},
+	EntriesQueuedToWrite: &EmptyCounter{},
+	EntriesSubmitted:     &EmptyCounter{},
+	ErrorMetrics:         EmptyGoogleSubscriberErrorMetrics,
 }
 
+// GoogleSubscriberErrorMetrics contains the error metrics for the Google Pub/Sub subscriber.
 type GoogleSubscriberErrorMetrics struct {
-    BatchedReadFailure Counter
-    ReadFailure Counter
-    EntriesWithNaN Counter
-    InvalidEntries Counter
-    BigQueryWriteFailure Counter
+	BatchedReadFailure   Counter
+	ReadFailure          Counter
+	EntriesWithNaN       Counter
+	InvalidEntries       Counter
+	BigQueryWriteFailure Counter
 }
 
+// EmptyGoogleSubscriberErrorMetrics is used for testing when we want to pass in metrics but don't care about their value.
 var EmptyGoogleSubscriberErrorMetrics GoogleSubscriberErrorMetrics = GoogleSubscriberErrorMetrics{
-    BatchedReadFailure: &EmptyCounter{},
-    ReadFailure: &EmptyCounter{},
-    EntriesWithNaN: &EmptyCounter{},
-    InvalidEntries: &EmptyCounter{},
-    BigQueryWriteFailure: &EmptyCounter{},
+	BatchedReadFailure:   &EmptyCounter{},
+	ReadFailure:          &EmptyCounter{},
+	EntriesWithNaN:       &EmptyCounter{},
+	InvalidEntries:       &EmptyCounter{},
+	BigQueryWriteFailure: &EmptyCounter{},
 }
 
 func NewGooglePublisherMetrics(ctx context.Context, metricsHandler Handler, serviceName string, handlerName string, handlerID string) (*GooglePublisherMetrics, error) {
-    publisherMetrics := GooglePublisherMetrics{}
+	publisherMetrics := GooglePublisherMetrics{}
 
-    publisherMetrics.EntriesQueuedToPublish, err = handler.NewCounter(ctx, &Descriptor{
-        DisplayName: handlerName + " Entries Queued To Publish",
-        ServiceName: serviceName,
-        ID:          handlerID + ".entries_submitted",
-        Unit:        "errors",
-        Description: "The number of " + handlerID + " entries that have been queued for publishing through Google PubSub.",
-    })
-    if err != nil {
-        return nil, err
-    }
+	publisherMetrics.EntriesQueuedToPublish, err = handler.NewCounter(ctx, &Descriptor{
+		DisplayName: handlerName + " Entries Queued To Publish",
+		ServiceName: serviceName,
+		ID:          handlerID + ".entries_submitted",
+		Unit:        "entries",
+		Description: "The number of " + handlerID + " entries that have been queued for publishing through Google Pub/Sub.",
+	})
+	if err != nil {
+		return nil, err
+	}
 
-    publisherMetrics.EntriesPublished, err = handler.NewCounter(ctx, &Descriptor{
-        DisplayName: handlerName + " Entries Published",
-        ServiceName: serviceName,
-        ID:          handlerID + ".entries.written",
-        Unit:        "errors",
-        Description: "The number of " + handlerID + " entries that have been published through Google PubSub.",
-    })
-    if err != nil {
-        return nil, err
-    }
+	publisherMetrics.EntriesPublished, err = handler.NewCounter(ctx, &Descriptor{
+		DisplayName: handlerName + " Entries Published",
+		ServiceName: serviceName,
+		ID:          handlerID + ".entries.written",
+		Unit:        "entries",
+		Description: "The number of " + handlerID + " entries that have been published through Google Pub/Sub.",
+	})
+	if err != nil {
+		return nil, err
+	}
 
-    publisherMetrics.ErrorMetrics.
+	publisherMetrics.ErrorMetrics.PublishFailure, err = handler.NewCounter(ctx, &Descriptor{
+		DisplayName: handlerName + " Entries Publish Failure",
+		ServiceName: serviceName,
+		ID:          handlerID + ".error.publish_failure",
+		Unit:        "errors",
+		Description: "The number of " + handlerID + " entries that failed to be published through Google Pub/Sub.",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &publisherMetrics, nil
+}
+
+func NewGoogleSubscriberMetircs(ctx context.Context, metricsHandler Handler, serviceName string, handlerName string, handlerID string) (*GoogleSubscriberMetrics, error) {
+	subscriberMetrics := GoogleSubscriberMetrics{}
+
+	subscriberMetrics.EntriesReceived, err = handler.NewCounter(ctx, &Descriptor{
+		DisplayName: handlerName + " Entries Received",
+		ServiceName: serviceName,
+		ID:          handlerID + ".entries",
+		Unit:        "entries",
+		Description: "The total number of " + handlerID + " entries received through Google Pub/Sub",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	subscriberMetrics.EntriesQueuedToWrite, err = handler.NewCounter(ctx, &Descriptor{
+		DisplayName: handlerName + " Entries Queued To Write",
+		ServiceName: serviceName,
+		ID:          handlerID + ".entries.queued",
+		Unit:        "entries",
+		Description: "The total number of " + handlerID + " entries queued to be written to BigQuery",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	subscriberMetrics.EntriesSubmitted, err = handler.NewCounter(ctx, &Descriptor{
+		DisplayName: handlerName + " Entries Submitted",
+		ServiceName: serviceName,
+		ID:          handlerID + ".entries.written",
+		Unit:        "entries",
+		Description: "The total number of " + handlerID + " entries written to BigQuery",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	subscriberMetrics.ErrorMetrics.BatchedReadFailure, err = handler.NewCounter(ctx, &Descriptor{
+		DisplayName: handlerName + " Batched Read Failure",
+		ServiceName: serviceName,
+		ID:          handlerID + ".error.batched_read_failure",
+		Unit:        "errors",
+		Description: "The total number of " + handlerID + " entries that could not be batch read from Google Pub/Sub",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	subscriberMetrics.ErrorMetrics.BatchedReadFailure, err = handler.NewCounter(ctx, &Descriptor{
+		DisplayName: handlerName + " Read Failure",
+		ServiceName: serviceName,
+		ID:          handlerID + ".error.read_failure",
+		Unit:        "errors",
+		Description: "The total number of " + handlerID + " entries that could not be read",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	subscriberMetrics.ErrorMetrics.EntriesWithNaN, err = handler.NewCounter(ctx, &Descriptor{
+		DisplayName: handlerName + " Entries With NaN or Inf",
+		ServiceName: serviceName,
+		ID:          handlerID + ".error.entries_with_nan_or_inf",
+		Unit:        "errors",
+		Description: "The total number of " + handlerID + " entries that have NaN or Inf values",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	subscriberMetrics.ErrorMetrics.InvalidEntries, err = handler.NewCounter(ctx, &Descriptor{
+		DisplayName: handlerName + " Invalid Entries",
+		ServiceName: serviceName,
+		ID:          handlerID + ".error.invalid_entries",
+		Unit:        "errors",
+		Description: "The total number of " + handlerID + " entries that are invalid",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	subscriberMetrics.ErrorMetrics.BigQueryWriteFailure, err = handler.NewCounter(ctx, &Descriptor{
+		DisplayName: handlerName + " BigQuery Write Failures",
+		ServiceName: serviceName,
+		ID:          handlerID + ".error.write_failure",
+		Unit:        "errors",
+		Description: "The total number of " + handlerID + " entries that failed to write to BigQuery",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &subscriberMetrics, nil
 }
