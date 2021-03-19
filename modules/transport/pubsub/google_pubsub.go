07e62a2065781c6f741fb5c1cf884359035570ef
nbopardi <nikhilb@gmail.com>
2021-03-18 17:21:12 -0700

    created google pubsub publisher

--- modules/transport/pubsub/google_pubsub.go
+++ modules/transport/pubsub/google_pubsub.go
@@ -1,26 +1,26 @@
 package pubsub
 
 import (
-    "context"
+	"context"
 
-    "cloud.google.com/go/bigquery"
-    "cloud.google.com/go/pubsub"
+	"cloud.google.com/go/bigquery"
+	"cloud.google.com/go/pubsub"
 )
 
 // Entry defines the methods for Google PubSub structs to implement
 type Entry interface {
-    validate() (bool) // private function within write entry
-    checkNaNOrInf() (bool, []string) // private function within write entry
-    Save() (map[string]bigquery.Value, string, error)
-    WriteEntry() ([]byte, error)
-    ReadEntry(data []byte) (bool)
+	validate() bool                  // private function within write entry
+	checkNaNOrInf() (bool, []string) // private function within write entry
+	Save() (map[string]bigquery.Value, string, error)
+	WriteEntry() ([]byte, error)
+	ReadEntry(data []byte) bool
 }
 
 type GooglePubSubPublisher interface {
-    Publish(ctx context.Context, entry Entry) (error)
+	Publish(ctx context.Context, entry *Entry) error
 }
 
 type GooglePubSubSubscriber interface {
-    ReceiveAndSubmit(ctx context.Context) (error)
-    UnbatchMessages(m *pubsub.Message) ([][]byte, error)
+	ReceiveAndSubmit(ctx context.Context) error
+	UnbatchMessages(m *pubsub.Message) ([][]byte, error)
 }
