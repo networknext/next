048b91c876b8a8b95facc09f883421b9c1341f7d
nbopardi <nikhilb@gmail.com>
2021-03-25 09:16:53 -0700

    clean shutdown for billing to flush remaining entries to bigquery

--- modules/billing/googlebigquery.go
+++ modules/billing/googlebigquery.go
@@ -72,30 +72,64 @@ func (bq *GoogleBigQueryClient) Bill(ctx context.Context, entry *BillingEntry) e
 // WriteLoop ranges over the incoming channel of Entry types and fills an internal buffer.
 // Once the buffer fills to the BatchSize it will write all entries to BigQuery. This should
 // only be called from 1 goroutine to avoid using a mutex around the internal buffer slice
-func (bq *GoogleBigQueryClient) WriteLoop(ctx context.Context) error {
+func (bq *GoogleBigQueryClient) WriteLoop(ctx context.Context, wg *sync.WaitGroup) error {
+	defer wg.Done()
+
 	if bq.entries == nil {
 		bq.entries = make(chan *BillingEntry, DefaultBigQueryChannelSize)
 	}
-	for entry := range bq.entries {
-		bq.Metrics.EntriesQueued.Set(float64(len(bq.entries)))
-		bq.bufferMutex.Lock()
-		bq.buffer = append(bq.buffer, entry)
-		bufferLength := len(bq.buffer)
+	for {
+		select {
+		case entry := <-bq.entries:
+			bq.Metrics.EntriesQueued.Set(float64(len(bq.entries)))
+			bq.bufferMutex.Lock()
+			bq.buffer = append(bq.buffer, entry)
+			bufferLength := len(bq.buffer)
 
-		if bufferLength >= bq.BatchSize {
-			if err := bq.TableInserter.Put(ctx, bq.buffer); err != nil {
+			if bufferLength >= bq.BatchSize {
+				if err := bq.TableInserter.Put(context.Background(), bq.buffer); err != nil {
+					bq.bufferMutex.Unlock()
+					level.Error(bq.Logger).Log("msg", "failed to write to BigQuery", "err", err)
+					bq.Metrics.ErrorMetrics.BillingWriteFailure.Add(float64(bufferLength))
+					continue
+				}
+
+				bq.buffer = bq.buffer[:0]
+				level.Info(bq.Logger).Log("msg", "flushed entries to BigQuery", "size", bq.BatchSize, "total", bufferLength)
+				bq.Metrics.EntriesFlushed.Add(float64(bufferLength))
+			}
+
+			bq.bufferMutex.Unlock()
+		case <-ctx.Done():
+			// Close entries channel to ensure messages get nacked
+			close(bq.entries)
+
+			var bufferLength int
+
+			// Received shutdown signal, write remainder of entries to BigQuery
+			bq.bufferMutex.Lock()
+			for entry := range bq.entries {
+				// Add the remaining entries to the buffer
+				bq.buffer = append(bq.buffer, entry)
+				bufferLength = len(bq.buffer)
+				bq.Metrics.EntriesQueued.Set(float64(bufferLength))
+			}
+
+			// Emptied out the entries channel, flush to BigQuery
+			if err := bq.TableInserter.Put(context.Background(), bq.buffer); err != nil {
 				bq.bufferMutex.Unlock()
 				level.Error(bq.Logger).Log("msg", "failed to write to BigQuery", "err", err)
 				bq.Metrics.ErrorMetrics.BillingWriteFailure.Add(float64(bufferLength))
-				continue
+				return err
 			}
-
 			bq.buffer = bq.buffer[:0]
-			level.Info(bq.Logger).Log("msg", "flushed entries to BigQuery", "size", bq.BatchSize, "total", bufferLength)
-			bq.Metrics.EntriesFlushed.Add(float64(bufferLength))
-		}
+			bq.bufferMutex.Unlock()
 
-		bq.bufferMutex.Unlock()
+			level.Info(bq.Logger).Log("msg", "flushed entries to BigQuery", "size", bufferLength, "total", bufferLength)
+			bq.Metrics.EntriesFlushed.Add(float64(bufferLength))
+
+			return nil
+		}
 	}
 	return nil
 }
