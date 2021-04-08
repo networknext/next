75ae221af124db4da850d1ee63a0e26498333dba
Glenn Fiedler <glenn@networknext.com>
2021-04-08 16:02:20 -0400

    relay version

--- modules/transport/packet_relay.go
+++ modules/transport/packet_relay.go
@@ -3,7 +3,6 @@ package transport
 import (
 	"errors"
 	"fmt"
-	"math"
 	"net"
 
 	"github.com/networknext/backend/modules/crypto"
@@ -12,8 +11,9 @@ import (
 )
 
 const (
-	VersionNumberUpdateRequest  = 2
+	VersionNumberUpdateRequest  = 3
 	VersionNumberUpdateResponse = 0
+	MaxVersionStringLength = 32
 )
 
 type RelayUpdateRequest struct {
@@ -21,12 +21,12 @@ type RelayUpdateRequest struct {
 	RelayVersion string
 	Address      net.UDPAddr
 	Token        []byte
-
 	PingStats    []routing.RelayStatsPing
-	TrafficStats routing.TrafficStats
-
+	SessionCount uint64
 	ShuttingDown bool
 
+	// todo: remove this stuff. belongs in another packet
+	TrafficStats routing.TrafficStats
 	CPUUsage float64
 	MemUsage float64
 }
@@ -40,6 +40,8 @@ func (r *RelayUpdateRequest) UnmarshalBinary(buff []byte) error {
 	switch r.Version {
 	case 2:
 		return r.unmarshalBinaryV2(buff, index)
+	case 3:
+		return r.unmarshalBinaryV3(buff, index)
 	default:
 		return fmt.Errorf("invalid packet, unknown version: %d", r.Version)
 	}
@@ -96,127 +98,59 @@ func (r *RelayUpdateRequest) unmarshalBinaryV2(buff []byte, index int) error {
 	return nil
 }
 
-// MarshalBinary ...
-func (r RelayUpdateRequest) MarshalBinary() ([]byte, error) {
-	switch r.Version {
-	case 2:
-		return r.marshalBinaryV2()
-	default:
-		return nil, fmt.Errorf("invalid update request version: %d", r.Version)
+func (r *RelayUpdateRequest) unmarshalBinaryV3(buff []byte, index int) error {
+	var numRelays uint32
+
+	var addr string
+	if !(encoding.ReadString(buff, &index, &addr, routing.MaxRelayAddressLength) &&
+		encoding.ReadBytes(buff, &index, &r.Token, crypto.KeySize) &&
+		encoding.ReadUint32(buff, &index, &numRelays)) {
+		return errors.New("invalid packet")
 	}
-}
 
-func (r *RelayUpdateRequest) marshalBinaryV2() ([]byte, error) {
-	data := make([]byte, r.sizeV2())
+	if udp, err := net.ResolveUDPAddr("udp", addr); udp != nil && err == nil {
+		r.Address = *udp
+	} else {
+		return fmt.Errorf("could not convert address '%s' with reason: %v", addr, err)
+	}
 
-	index := 0
-	encoding.WriteUint32(data, &index, r.Version)
-	encoding.WriteString(data, &index, r.Address.String(), math.MaxInt32)
-	encoding.WriteBytes(data, &index, r.Token, crypto.KeySize)
-	encoding.WriteUint32(data, &index, uint32(len(r.PingStats)))
-
-	for i := 0; i < len(r.PingStats); i++ {
+	r.PingStats = make([]routing.RelayStatsPing, numRelays)
+	for i := 0; i < int(numRelays); i++ {
 		stats := &r.PingStats[i]
 
-		encoding.WriteUint64(data, &index, stats.RelayID)
-		encoding.WriteUint32(data, &index, math.Float32bits(stats.RTT))
-		encoding.WriteUint32(data, &index, math.Float32bits(stats.Jitter))
-		encoding.WriteUint32(data, &index, math.Float32bits(stats.PacketLoss))
-	}
-
-	r.TrafficStats.WriteTo(data, &index, 2)
-	encoding.WriteBool(data, &index, r.ShuttingDown)
-	encoding.WriteFloat64(data, &index, r.CPUUsage)
-	encoding.WriteFloat64(data, &index, r.MemUsage)
-
-	return data[:index], nil
-}
-
-func (r *RelayUpdateRequest) sizeV2() uint {
-	return uint(4 + // version
-		4 + // address length
-		len(r.Address.String()) + // address
-		crypto.KeySize + // public key
-		4 + // number of ping stats
-		20*len(r.PingStats) + // ping stats list
-		8 + // session count
-		8 + // envelope up
-		8 + // envelope down
-		8 + // outbound ping tx
-		8 + // route request rx
-		8 + // route request tx
-		8 + // route response rx
-		8 + // route response tx
-		8 + // client to server rx
-		8 + // client to server tx
-		8 + // server to client rx
-		8 + // server to client tx
-		8 + // inbound ping rx
-		8 + // inbound ping tx
-		8 + // pong rx
-		8 + // session ping rx
-		8 + // session ping tx
-		8 + // session pong rx
-		8 + // session pong tx
-		8 + // continue request rx
-		8 + // continue request tx
-		8 + // continue response rx
-		8 + // continue response tx
-		8 + // near ping rx
-		8 + // near ping tx
-		8 + // unknown Rx
-		1 + // shutdown flag
-		8 + // cpu usage
-		8) // memory usage
-}
-
-type RelayUpdateResponse struct {
-	Timestamp    int64
-	RelaysToPing []routing.RelayPingData
-}
-
-func (r *RelayUpdateResponse) UnmarshalBinary(buff []byte) error {
-	index := 0
-	var version uint32
-	if !encoding.ReadUint32(buff, &index, &version) {
-		return errors.New("failed to unmarshal relay update response version")
-	}
-
-	var timestamp uint64
-	if !encoding.ReadUint64(buff, &index, &timestamp) {
-		return errors.New("failed to unmarshal relay update response timestamp")
-	}
-	r.Timestamp = int64(timestamp)
-
-	var numRelaysToPing uint32
-	if !encoding.ReadUint32(buff, &index, &numRelaysToPing) {
-		return errors.New("failed to unmarshal relay update response number of relays to ping")
-	}
-
-	for i := 0; uint32(i) < numRelaysToPing; i++ {
-		var id uint64
-		if !encoding.ReadUint64(buff, &index, &id) {
-			return errors.New("failed to unmarshal relay update response relay id")
+		// todo: these could be much more efficient as byte values [0,255]
+		if !(encoding.ReadUint64(buff, &index, &stats.RelayID) &&
+			encoding.ReadFloat32(buff, &index, &stats.RTT) &&
+			encoding.ReadFloat32(buff, &index, &stats.Jitter) &&
+			encoding.ReadFloat32(buff, &index, &stats.PacketLoss)) {
+			return errors.New("invalid packet, could not read a ping stat")
 		}
+	}
 
-		var addr string
-		if !encoding.ReadString(buff, &index, &addr, routing.MaxRelayAddressLength) {
-			return errors.New("failed to unmarshal relay update response relay address")
-		}
+	if !encoding.ReadUint64(buff, &index, &r.SessionCount) {
+		return errors.New("invalid packet, could not read session count")
+	}
 
-		r.RelaysToPing = append(r.RelaysToPing, routing.RelayPingData{
-			ID:      id,
-			Address: addr,
-		})
+	if !encoding.ReadBool(buff, &index, &r.ShuttingDown) {
+		return errors.New("invalid packet, could not read shutdown flag")
+	}
+
+	if !encoding.ReadString(buff, &index, &r.RelayVersion, MaxVersionStringLength) {
+		return errors.New("invalid relay version string")
 	}
 
 	return nil
 }
 
+type RelayUpdateResponse struct {
+	Timestamp     int64
+	RelaysToPing  []routing.RelayPingData
+	TargetVersion string
+}
+
 func (r RelayUpdateResponse) MarshalBinary() ([]byte, error) {
 	index := 0
-	responseData := make([]byte, r.size())
-
+	responseData := make([]byte, 10*1024)
 	encoding.WriteUint32(responseData, &index, VersionNumberUpdateResponse)
 	encoding.WriteUint64(responseData, &index, uint64(r.Timestamp))
 	encoding.WriteUint32(responseData, &index, uint32(len(r.RelaysToPing)))
@@ -224,10 +158,6 @@ func (r RelayUpdateResponse) MarshalBinary() ([]byte, error) {
 		encoding.WriteUint64(responseData, &index, r.RelaysToPing[i].ID)
 		encoding.WriteString(responseData, &index, r.RelaysToPing[i].Address, routing.MaxRelayAddressLength)
 	}
-
+	encoding.WriteString(responseData, &index, r.TargetVersion, MaxVersionStringLength)
 	return responseData[:index], nil
-}
-
-func (r *RelayUpdateResponse) size() int {
-	return 4 + 8 + 4 + len(r.RelaysToPing)*(8+routing.MaxRelayAddressLength)
 }
