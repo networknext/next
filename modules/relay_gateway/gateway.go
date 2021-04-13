f94f91bfaca23cfe8d437cc786bd82e686394612
nbopardi <nikhilb@gmail.com>
2021-04-13 12:48:06 -0700

    use pubsub-style batching, gateway test works with batch and unbatch

--- modules/relay_gateway/gateway.go
+++ modules/relay_gateway/gateway.go
@@ -8,8 +8,8 @@ import (
 	"sync"
 	"time"
 
+	"github.com/networknext/backend/modules/encoding"
 	"github.com/networknext/backend/modules/metrics"
-	"github.com/networknext/backend/modules/transport"
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
@@ -29,7 +29,8 @@ type GatewayConfig struct {
 }
 
 type GatewayHTTPClient struct {
-	cfg            *GatewayConfig
+	Cfg *GatewayConfig
+
 	updateChan     chan []byte
 	gatewayMetrics *metrics.RelayGatewayMetrics
 	client         *http.Client
@@ -41,7 +42,7 @@ func NewGatewayHTTPClient(cfg *GatewayConfig, updateChan chan []byte, gatewayMet
 	client := &http.Client{Timeout: cfg.HTTPTimeout}
 
 	return &GatewayHTTPClient{
-		cfg:            cfg,
+		Cfg:            cfg,
 		updateChan:     updateChan,
 		gatewayMetrics: gatewayMetrics,
 		client:         client,
@@ -54,15 +55,16 @@ func (httpClient *GatewayHTTPClient) Start(ctx context.Context) error {
 	var wg sync.WaitGroup
 
 	// Create worker goroutines to pull updates from the update channel
-	for i := 0; i < httpClient.cfg.NumGoroutines; i++ {
+	for i := 0; i < httpClient.Cfg.NumGoroutines; i++ {
 		wg.Add(1)
 
 		// Handle update requests
 		go func() {
 			defer wg.Done()
-			// Create a buffer to hold the requests that will go to the relay backends
-			var updateBuffer *transport.RelayUpdateRequestList
-			var updateBufferMutex sync.RWMutex
+			// Create buffers to store updates for batch-sending
+			var updateBufferMessageCount int
+			var updateBufferMutex sync.Mutex
+			updateBuffer := make([]byte, 0)
 
 			for {
 				select {
@@ -70,51 +72,43 @@ func (httpClient *GatewayHTTPClient) Start(ctx context.Context) error {
 					// TODO: flush all messages to relay backends
 					return
 				case update := <-httpClient.updateChan:
-					// Add the update to the buffer and get the buffer's length
+					// Create a byte slice with an offset
+					var offset int
+					data := make([]byte, 4+len(update))
+					encoding.WriteUint32(data, &offset, uint32(len(update)))
+					encoding.WriteBytes(data, &offset, update, len(update))
+
+					// Chain the update to previous updates linked via offset
 					updateBufferMutex.Lock()
-					updateBuffer.Requests = append(updateBuffer.Requests, update)
-					bufferLength := len(updateBuffer.Requests)
+					updateBuffer = append(updateBuffer, data...)
+					updateBufferMessageCount++
 					updateBufferMutex.Unlock()
 
 					// Increment the updates received metric
 					httpClient.gatewayMetrics.UpdatesReceived.Add(1)
 					// Set the number of updates queued for batch-sending
-					httpClient.gatewayMetrics.UpdatesQueued.Set(float64(bufferLength))
+					httpClient.gatewayMetrics.UpdatesQueued.Set(float64(updateBufferMessageCount))
 
 					// Check if we have reached the batch size
-					if bufferLength >= httpClient.cfg.BatchSize {
+					if updateBufferMessageCount >= httpClient.Cfg.BatchSize {
 						// Copy the buffer so we can clear it without affecting the worker goroutines
 						updateBufferMutex.Lock()
+
 						bufferCopy := updateBuffer
-						updateBuffer.Requests = updateBuffer.Requests[:0]
+						updateBuffer = make([]byte, 0)
+						numUpdatesFlushed := updateBufferMessageCount
+						updateBufferMessageCount = 0
+
 						updateBufferMutex.Unlock()
 
 						// Send the buffer to all relay backends
-						for _, address := range httpClient.cfg.RelayBackendAddresses {
-							go func() {
-								// Marshal the buffer copy
-								body, err := bufferCopy.MarshalBinary()
-								if err != nil {
-									httpClient.gatewayMetrics.ErrorMetrics.MarshalBinaryFailure.Add(1)
-									_ = level.Error(httpClient.logger).Log("msg", "unable to marshal buffer copy", "err", err)
-									return
-								}
-
-								// Post to relay backend
-								buffer := bytes.NewBuffer(body)
-								resp, err := httpClient.client.Post(fmt.Sprintf("http://%s/relay_update", address), "application/octet-stream", buffer)
-								if err != nil || resp.StatusCode != http.StatusOK {
-									httpClient.gatewayMetrics.ErrorMetrics.BackendSendFailure.Add(1)
-									_ = level.Error(httpClient.logger).Log("msg", fmt.Sprintf("unable to send update to relay backend %s, response %d", address, resp.StatusCode), "err", err)
-									return
-								}
-								resp.Body.Close()
-							}()
+						for _, address := range httpClient.Cfg.RelayBackendAddresses {
+							go httpClient.PostRelayUpdate(bufferCopy, address)
 						}
 
 						// Set the number of relay update requests sent to the relay backends (not necessarily successful)
-						httpClient.gatewayMetrics.UpdatesFlushed.Add(float64(bufferLength))
-						level.Info(httpClient.logger).Log("msg", fmt.Sprintf("Sent %d relay updates to the relay backends", bufferLength))
+						httpClient.gatewayMetrics.UpdatesFlushed.Add(float64(numUpdatesFlushed))
+						level.Info(httpClient.logger).Log("msg", fmt.Sprintf("Sent %d relay updates to the relay backends", numUpdatesFlushed))
 					}
 				}
 			}
@@ -128,4 +122,21 @@ func (httpClient *GatewayHTTPClient) Start(ctx context.Context) error {
 		wg.Wait()
 		return ctx.Err()
 	}
+}
+
+func (httpClient *GatewayHTTPClient) PostRelayUpdate(bufferCopy []byte, address string) {
+	// Post to relay backend
+	buffer := bytes.NewBuffer(bufferCopy)
+
+	resp, err := httpClient.client.Post(fmt.Sprintf("http://%s/relay_update", address), "application/octet-stream", buffer)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		httpClient.gatewayMetrics.ErrorMetrics.BackendSendFailure.Add(1)
+		level.Error(httpClient.logger).Log("msg", fmt.Sprintf("unable to send update to relay backend %s, response was non-200", address), "err", err)
+		return
+	} else if resp.StatusCode != http.StatusOK {
+		httpClient.gatewayMetrics.ErrorMetrics.BackendSendFailure.Add(1)
+		level.Error(httpClient.logger).Log("msg", fmt.Sprintf("unable to send update to relay backend %s, response was %d", address, resp.StatusCode), "err", err)
+		return
+	}
+	resp.Body.Close()
 }
