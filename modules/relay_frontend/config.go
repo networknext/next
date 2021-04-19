76e0a14bb65b822ab8f9339ea053a3603a025bbc
nbopardi <nikhilb@gmail.com>
2021-04-19 13:54:40 -0700

    relay frontend works

--- modules/relay_frontend/config.go
+++ modules/relay_frontend/config.go
@@ -1,33 +1,50 @@
 package relay_frontend
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/networknext/backend/modules/envvar"
 )
 
-type Config struct {
-	ENV                string
-	MasterTimeVariance time.Duration
-	MatrixStoreAddress string
-	MSReadTimeout      time.Duration
-	MSWriteTimeout     time.Duration
-	MSMatrixTimeout    time.Duration
-	ValveMatrix        bool
+type RelayFrontendConfig struct {
+	Env                    string
+	MasterTimeVariance     time.Duration
+	MatrixStoreAddress     string
+	MSMaxIdleConnections   int
+	MSMaxActiveConnections int
+	MSReadTimeout          time.Duration
+	MSWriteTimeout         time.Duration
+	MSMatrixExpireTimeout  time.Duration
 }
 
-func GetConfig() (*Config, error) {
-	cfg := new(Config)
+func GetConfig() (*RelayFrontendConfig, error) {
+	cfg := new(RelayFrontendConfig)
 	var err error
 
-	cfg.ENV = envvar.Get("ENV", "local")
+	cfg.Env = envvar.Get("ENV", "local")
 
 	cfg.MasterTimeVariance, err = envvar.GetDuration("MASTER_TIME_VARIANCE", 5000*time.Millisecond)
 	if err != nil {
 		return nil, err
 	}
 
-	cfg.MatrixStoreAddress = envvar.Get("MATRIX_STORE_ADDRESS", "127.0.0.1:6379")
+	cfg.MatrixStoreAddress = envvar.Get("MATRIX_STORE_ADDRESS", "")
+	if cfg.MatrixStoreAddress == "" {
+		return nil, fmt.Errorf("MATRIX_STORE_ADDRESS not set")
+	}
+
+	maxIdleConnections, err := envvar.GetInt("MATRIX_STORE_MAX_IDLE_CONNS", 5)
+	if err != nil {
+		return nil, err
+	}
+	cfg.MSMaxIdleConnections = maxIdleConnections
+
+	maxActiveConnections, err := envvar.GetInt("MATRIX_STORE_MAX_ACTIVE_CONNS", 5)
+	if err != nil {
+		return nil, err
+	}
+	cfg.MSMaxActiveConnections = maxActiveConnections
 
 	cfg.MSReadTimeout, err = envvar.GetDuration("MATRIX_STORE_READ_TIMEOUT", 250*time.Millisecond)
 	if err != nil {
@@ -39,12 +56,7 @@ func GetConfig() (*Config, error) {
 		return nil, err
 	}
 
-	cfg.MSMatrixTimeout, err = envvar.GetDuration("MATRIX_STORE_MATRIX_TIMEOUT", 5*time.Second)
-	if err != nil {
-		return nil, err
-	}
-
-	cfg.ValveMatrix, err = envvar.GetBool("FEATURE_VALVE_MARTRIX", false)
+	cfg.MSMatrixExpireTimeout, err = envvar.GetDuration("MATRIX_STORE_EXPIRE_TIMEOUT", 5*time.Second)
 	if err != nil {
 		return nil, err
 	}
