a01a0a3386e4f98525fc549df48b97846c8dbb9f
nbopardi <nikhilb@gmail.com>
2021-05-05 06:43:47 -0700

    need to fix marshal binary v3

--- modules/transport/packet_relay.go
+++ modules/transport/packet_relay.go
@@ -158,6 +158,8 @@ func (r RelayUpdateRequest) MarshalBinary() ([]byte, error) {
 		return r.marshalBinaryV1()
 	case 2:
 		return r.marshalBinaryV2()
+	case 3:
+		return r.marshalBinaryV3()
 	default:
 		return nil, fmt.Errorf("invalid update request version: %d", r.Version)
 	}
@@ -222,6 +224,33 @@ func (r *RelayUpdateRequest) marshalBinaryV2() ([]byte, error) {
 
 	index := 0
 	encoding.WriteUint32(data, &index, r.Version)
+	encoding.WriteString(data, &index, r.Address.String(), math.MaxInt32)
+	encoding.WriteBytes(data, &index, r.Token, crypto.KeySize)
+	encoding.WriteUint32(data, &index, uint32(len(r.PingStats)))
+
+	for i := 0; i < len(r.PingStats); i++ {
+		stats := &r.PingStats[i]
+
+		encoding.WriteUint64(data, &index, stats.RelayID)
+		encoding.WriteUint32(data, &index, math.Float32bits(stats.RTT))
+		encoding.WriteUint32(data, &index, math.Float32bits(stats.Jitter))
+		encoding.WriteUint32(data, &index, math.Float32bits(stats.PacketLoss))
+	}
+
+	r.TrafficStats.WriteTo(data, &index, 2)
+	encoding.WriteBool(data, &index, r.ShuttingDown)
+	encoding.WriteFloat64(data, &index, r.CPUUsage)
+	encoding.WriteFloat64(data, &index, r.MemUsage)
+
+	return data[:index], nil
+}
+
+func (r *RelayUpdateRequest) marshalBinaryV3() ([]byte, error) {
+	data := make([]byte, r.sizeV3())
+
+	index := 0
+	encoding.WriteUint32(data, &index, r.Version)
+	encoding.WriteString(data, &index, r.RelayVersion, uint32(len(r.RelayVersion)))
 	encoding.WriteString(data, &index, r.Address.String(), math.MaxInt32)
 	encoding.WriteBytes(data, &index, r.Token, crypto.KeySize)
 	encoding.WriteUint32(data, &index, uint32(len(r.PingStats)))
@@ -338,6 +367,48 @@ func (r *RelayUpdateRequest) sizeV2() uint {
 		8 + // near ping tx
 		8 + // unknown Rx
 		1 + // shutdown flag
+		8 + // cpu usage
+		8) // memory usage
+}
+
+// Changes from v2:
+// Added relay version string
+func (r *RelayUpdateRequest) sizeV3() uint {
+	return uint(4 + // version
+		4 + // length of relay version
+		len(r.RelayVersion) + // relay version string
+		4 + // address length
+		len(r.Address.String()) + // address
+		crypto.KeySize + // public key
+		4 + // number of ping stats
+		20*len(r.PingStats) + // ping stats list
+		8 + // session count
+		1 + // shutdown flag
+		8 + // envelope up
+		8 + // envelope down
+		8 + // outbound ping tx
+		8 + // route request rx
+		8 + // route request tx
+		8 + // route response rx
+		8 + // route response tx
+		8 + // client to server rx
+		8 + // client to server tx
+		8 + // server to client rx
+		8 + // server to client tx
+		8 + // inbound ping rx
+		8 + // inbound ping tx
+		8 + // pong rx
+		8 + // session ping rx
+		8 + // session ping tx
+		8 + // session pong rx
+		8 + // session pong tx
+		8 + // continue request rx
+		8 + // continue request tx
+		8 + // continue response rx
+		8 + // continue response tx
+		8 + // near ping rx
+		8 + // near ping tx
+		8 + // unknown Rx
 		8 + // cpu usage
 		8) // memory usage
 }
