c481fd0981a7f650c0e0259d864336ddcdcd4465
nbopardi <nikhilb@gmail.com>
2021-05-22 09:20:22 -0700

    not >, but >=

--- modules/core/core.go
+++ modules/core/core.go
@@ -1680,7 +1680,7 @@ func MakeRouteDecision_TakeNetworkNext(routeMatrix []RouteEntry, routeShader *Ro
 	// should we try to reduce packet loss?
 
 	reducePacketLoss := false
-	if routeShader.ReducePacketLoss && directPacketLoss > routeShader.AcceptablePacketLoss && internal.ReducePacketLossMinSliceNumber > sliceNumber {
+	if routeShader.ReducePacketLoss && directPacketLoss > routeShader.AcceptablePacketLoss && internal.ReducePacketLossMinSliceNumber >= sliceNumber {
 		if debug != nil {
 			*debug += "try to reduce packet loss\n"
 		}
