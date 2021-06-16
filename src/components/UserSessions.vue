8c2fcc145868971ce5baf689f70f6ba780832f01
Andrew Baumbach <andrew@networknext.com>
2021-06-16 16:18:49 -0400

    Everything looks good locally now

--- src/components/UserSessions.vue
+++ src/components/UserSessions.vue
@@ -1,82 +1,97 @@
 <template>
-  <div class="table-responsive table-no-top-line" v-if="showSessions">
-    <table class="table table-sm">
-      <thead>
-        <tr>
-          <th>
-            <span>
-              Date
-            </span>
-          </th>
-          <th>
-            <span>
-              Session ID
-            </span>
-          </th>
-          <th>
-            <span>
-              Platform
-            </span>
-          </th>
-          <th>
-            <span>
-              Connection Type
-            </span>
-          </th>
-          <th>
-            <span>
-              ISP
-            </span>
-          </th>
-          <th>
-            <span>
-              Datacenter
-            </span>
-          </th>
-          <th>
-            <span>
-              Server Address
-            </span>
-          </th>
-        </tr>
-      </thead>
-      <tbody v-if="sessions.length > 0">
-        <tr id="data-row" v-for="(session, index) in sessions" v-bind:key="index">
-          <td>
-            {{ convertUTCDateToLocalDate(new Date(session.time_stamp)) }}
-          </td>
-          <td>
-              <router-link v-bind:to="`/session-tool/${session.meta.id}`" class="text-dark fixed-width">{{ session.meta.id }}</router-link>
-          </td>
-          <td>
-            {{ session.meta.platform }}
-          </td>
-          <td>
-            {{ session.meta.connection === "wifi" ? "Wi-Fi" : session.meta.connection.charAt(0).toUpperCase() + session.meta.connection.slice(1) }}
-          </td>
-          <td>
-            {{ session.meta.location.isp || "Unknown"}}
-          </td>
-          <td>
-            {{ session.meta.datacenter_alias !== "" ? session.meta.datacenter_alias : session.meta.datacenter_name }}
-          </td>
-          <td>
-            {{ session.meta.server_addr }}
-          </td>
-        </tr>
-      </tbody>
-      <tbody v-if="sessions.length === 0">
-        <tr>
-          <td colspan="7" class="text-muted">
-              There are no sessions belonging to this user.
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <div class="float-right">
-      <button id="more-sessions-button" class="btn btn-primary" v-if="currentPage < MAX_PAGES" @click="fetchMoreSessions()">
-        More Sessions
-      </button>
+  <div>
+    <div
+      class="spinner-border"
+      role="status"
+      id="sessions-spinner"
+      v-show="!showSessions"
+    >
+      <span class="sr-only">Loading...</span>
+    </div>
+    <div class="table-responsive table-no-top-line" v-if="showSessions">
+      <table class="table table-sm">
+        <thead>
+          <tr>
+            <th>
+              <span>
+                Date
+              </span>
+            </th>
+            <th>
+              <span>
+                Session ID
+              </span>
+            </th>
+            <th>
+              <span>
+                Platform
+              </span>
+            </th>
+            <th>
+              <span>
+                Connection Type
+              </span>
+            </th>
+            <th>
+              <span>
+                ISP
+              </span>
+            </th>
+            <th>
+              <span>
+                Datacenter
+              </span>
+            </th>
+            <th>
+              <span>
+                Server Address
+              </span>
+            </th>
+          </tr>
+        </thead>
+        <tbody v-if="sessions.length > 0">
+          <tr id="data-row" v-for="(session, index) in sessions" v-bind:key="index">
+            <td>
+              {{ convertUTCDateToLocalDate(new Date(session.time_stamp)) }}
+            </td>
+            <td>
+                <router-link v-bind:to="`/session-tool/${session.meta.id}`" class="text-dark fixed-width">{{ session.meta.id }}</router-link>
+            </td>
+            <td>
+              {{ session.meta.platform }}
+            </td>
+            <td>
+              {{ session.meta.connection === "wifi" ? "Wi-Fi" : session.meta.connection.charAt(0).toUpperCase() + session.meta.connection.slice(1) }}
+            </td>
+            <td>
+              {{ session.meta.location.isp || "Unknown"}}
+            </td>
+            <td>
+              {{ session.meta.datacenter_alias !== "" ? session.meta.datacenter_alias : session.meta.datacenter_name }}
+            </td>
+            <td>
+              {{ session.meta.server_addr }}
+            </td>
+          </tr>
+        </tbody>
+        <tbody v-if="sessions.length === 0">
+          <tr>
+            <td colspan="7" class="text-muted">
+                There are no sessions belonging to this user.
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="float-left" style="padding-bottom: 20px;">
+        <button id="more-sessions-button" class="btn btn-primary" @click="reloadSessions()">
+          Refresh Sessions
+        </button>
+      </div>
+      <div class="float-right" style="padding-bottom: 20px;">
+        <button id="more-sessions-button" class="btn btn-primary" v-if="this.currentPage < MAX_PAGES" @click="fetchMoreSessions()">
+          More Sessions
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -84,8 +99,6 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import { NavigationGuardNext, Route } from 'vue-router'
-
-const MAX_PAGES = 10
 
 /**
  * This component displays all of the information related to the user
@@ -95,29 +108,26 @@ const MAX_PAGES = 10
 @Component
 export default class UserSessions extends Vue {
   private sessions: Array<any>
-  private timeStamps: Array<any>
   private sessionLoop: any
   private showSessions: boolean
   private searchID: string
   private currentPage: number
-  private savedPage: number
+  private MAX_PAGES = 10
 
   constructor () {
     super()
     this.searchID = ''
     this.sessions = []
-    this.timeStamps = []
     this.showSessions = false
     this.sessionLoop = null
     this.currentPage = 0
-    this.savedPage = 0
   }
 
   private mounted () {
     this.currentPage = 0
     this.searchID = this.$route.params.pathMatch || ''
     if (this.searchID !== '') {
-      this.restartLoop()
+      this.fetchUserSessions()
     }
   }
 
@@ -130,9 +140,6 @@ export default class UserSessions extends Vue {
     this.currentPage = 0
     if (this.searchID !== '') {
       this.fetchUserSessions()
-      this.sessionLoop = setInterval(() => {
-        this.fetchUserSessions()
-      }, 10000)
     }
     next()
   }
@@ -142,15 +149,14 @@ export default class UserSessions extends Vue {
   }
 
   private fetchMoreSessions () {
-    const nextPage = this.currentPage + 1
+    this.fetchUserSessions()
+  }
 
-    if (nextPage >= MAX_PAGES) {
-      // This shouldn't happen
-      return
-    }
-    this.currentPage = nextPage
-    this.savedPage = this.currentPage
-    this.restartLoop()
+  private reloadSessions () {
+    this.currentPage = 0
+    this.showSessions = false
+    this.sessions = []
+    this.fetchUserSessions()
   }
 
   private fetchUserSessions () {
@@ -158,16 +164,12 @@ export default class UserSessions extends Vue {
       return
     }
 
-    this.$apiService.fetchUserSessions({ user_id: this.searchID, page: this.savedPage })
+    this.$apiService.fetchUserSessions({ user_id: this.searchID, page: this.currentPage })
       .then((response: any) => {
-        this.sessions = response.sessions || []
-        this.showSessions = true
+        this.sessions = this.sessions.concat(response.sessions)
         this.currentPage = response.page
       })
       .catch((error: Error) => {
-        if (this.sessionLoop) {
-          clearInterval(this.sessionLoop)
-        }
         if (this.sessions.length === 0) {
           console.log(`Something went wrong fetching sessions details for: ${this.searchID}`)
           console.log(error)
@@ -178,16 +180,6 @@ export default class UserSessions extends Vue {
           this.showSessions = true
         }
       })
-  }
-
-  private restartLoop () {
-    if (this.sessionLoop) {
-      clearInterval(this.sessionLoop)
-    }
-    this.fetchUserSessions()
-    this.sessionLoop = setInterval(() => {
-      this.fetchUserSessions()
-    }, 10000)
   }
 
   private convertUTCDateToLocalDate (date: Date) {
