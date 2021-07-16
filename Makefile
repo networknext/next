5bc5b044cc58b59112fd6f9531cdda63fcd39d3f
Andrew Baumbach <andrew@networknext.com>
2021-07-16 15:56:36 -0400

    Adding commands for deploying frontend code to the portal site VMs

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ ARTIFACT_BUCKET = gs://development_artifacts
 ARTIFACT_BUCKET_STAGING = gs://staging_artifacts
 ARTIFACT_BUCKET_PROD = gs://prod_artifacts
 
+PORTAL_DEV_MIG = portal-site-mig
+
 .PHONY: build-portal-artifacts-local
 build-portal-artifacts-local:
 	./deploy/build-artifacts.sh -e local -b $(ARTIFACT_BUCKET_PROD) -s portal
@@ -17,3 +19,7 @@ build-portal-artifacts-staging:
 .PHONY: build-portal-artifacts-prod
 build-portal-artifacts-prod:
 	./deploy/build-artifacts.sh -e prod -b $(ARTIFACT_BUCKET_PROD) -s portal
+
+.PHONY: deploy-portal-dev
+deploy-portal-dev: build-portal-artifacts-dev
+	./deploy/deploy-portal.sh -b $(ARTIFACT_BUCKET) -e dev -m $(PORTAL_DEV_MIG)
---
+++ deploy/deploy-portal.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mig=
+bucket=
+env=
+
+print_usage() {
+    printf "Usage: deploy-portal.sh\n\n"
+    # printf "b [string]\tBucket name on GCP Storage\n"
+
+    printf "Example:\n\n"
+    printf "> deploy-portal.sh\n"
+}
+
+while getopts 'm:b:e:h' flag; do
+  case "${flag}" in
+    m) mig="${OPTARG}" ;;
+    b) bucket="${OPTARG}" ;;
+    e) env="${OPTARG}" ;;
+    h) print_usage
+       exit 1 ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+# ssh into all the VMs under the specified MIG
+
+VMs=$(gcloud compute --project network-next-v3-dev instance-groups managed list-instances ${mig} --zone us-central1-a --format "value(instance)")
+COMMAND="cd /portal && sudo gsutil cp ${bucket}/portal-dist.${env}.tar.gz artifact.tar.gz && tar -xvf artifact.tar.gz"
+
+for i in "${VMs[@]}"; do
+  printf "Deploying Frontend code to ${i}... \n"
+  gcloud compute --project network-next-v3-dev ssh ${i} -- ${COMMAND}
+  printf "done\n"
+done
