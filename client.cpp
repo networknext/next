5926f3195879a3d9adccb61a60d4d148ba0eef69
Glenn Fiedler <glenn@networknext.com>
2021-09-15 18:36:54 -0400

    add files

---
+++ client.cpp
@@ -0,0 +1,101 @@
+#include <enet.h>
+#include <next.h>
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <next.h>
+
+const int MaxChannels = 2;
+const int MaxIncomingBandwidth = 0;
+const int MaxOutgoingBandwidth = 0;
+
+static volatile int quit = 0;
+
+void interrupt_handler( int signal )
+{
+    (void) signal; quit = 1;
+}
+
+int main( int argc, char ** argv ) 
+{
+    if ( next_init( NULL, NULL ) != NEXT_OK )
+    {
+        printf( "error: could not initialize network next\n" );
+        return 1;
+    }
+
+    if ( enet_initialize() != 0 )
+    {
+        printf( "error: failed to initialize enet\n" );
+        return 1;
+    }
+
+    ENetHost * client = enet_host_create( NULL, 1, MaxChannels, MaxIncomingBandwidth, MaxOutgoingBandwidth ); 
+
+    if ( client == NULL )
+    {
+        printf( "error: failed to create enet client\n" );
+        return 1;
+    }
+
+    ENetAddress address;
+    enet_address_set_host( &address, "localhost" );
+    address.port = 1234;
+
+    ENetPeer * peer = enet_host_connect( client, &address, MaxChannels, 0 );    
+  
+    if ( peer == NULL )
+    {
+        printf( "error: could not create client peer\n" );
+        return 1;
+    }
+
+    printf( "client connecting to server %x:%d\n", address.host, address.port );
+
+    ENetEvent event;
+       
+    if ( enet_host_service( client, &event, 5000 ) > 0 && event.type == ENET_EVENT_TYPE_CONNECT )
+    {
+        printf( "client connected to server\n" );
+    }
+    else
+    {
+        printf( "error: client could not connect to server\n" );
+        return 1;
+    }
+
+    while ( true )
+    {
+        while ( enet_host_service( client, &event, 0 ) > 0 )
+        {
+            switch ( event.type )
+            {
+                case ENET_EVENT_TYPE_RECEIVE:
+                    printf( "packet of length %u was received from server on channel %u\n", int(event.packet->dataLength), event.channelID );
+                    enet_packet_destroy( event.packet );
+                    break;
+           
+                default:
+                    break;
+            }
+        }
+
+        printf( "tick\n" );
+
+        ENetPacket * packet = enet_packet_create( "hello", strlen ("hello") + 1, 0 );
+        enet_peer_send( peer, 0, packet );
+        enet_host_flush( client );
+
+        next_sleep( 1.0f );
+    }
+
+    enet_peer_reset( peer );
+
+    enet_host_destroy( client );
+
+    enet_deinitialize();
+
+    next_term();
+
+    return 0;
+}
---
+++ premake5.lua
@@ -0,0 +1,87 @@
+solution "enet"
+	configurations { "Debug", "Release" }
+	platforms { "x64" }
+
+project "enet"
+	kind "StaticLib"
+	language "C"
+	files { "enet/*.c" }
+	includedirs { "enet/", "." }
+	configuration "Debug"
+		targetsuffix "d"
+		defines({ "DEBUG" })
+		symbols "On"		
+	configuration "Release"
+		defines({ "NDEBUG" })
+		optimize "On"
+	configuration { "Debug", "x64" }
+		targetsuffix "64d"
+	configuration { "Release", "x64" }
+		targetsuffix "64"
+
+project "next"
+	kind "StaticLib"
+	links { "sodium" }
+	files {
+		"next/next.h",
+		"next/next.cpp",
+		"next/next_*.h",
+		"next/next_*.cpp",
+	}
+	includedirs { "next", "sodium" }
+	filter "system:windows"
+		linkoptions { "/ignore:4221" }
+		disablewarnings { "4324" }
+	filter "system:macosx"
+		linkoptions { "-framework SystemConfiguration -framework CoreFoundation" }
+
+project "sodium"
+	kind "StaticLib"
+	includedirs { "sodium" }
+	files {
+		"sodium/**.c",
+		"sodium/**.h",
+	}
+  	filter { "system:not windows", "platforms:*x64 or *avx or *avx2" }
+		files {
+			"sodium/**.S"
+		}
+	filter "platforms:*x86"
+		architecture "x86"
+		defines { "NEXT_X86=1" }
+	filter "platforms:*x64"
+		architecture "x86_64"
+		defines { "NEXT_X64=1" }
+	filter "platforms:*avx"
+		architecture "x86_64"
+		vectorextensions "AVX"
+		defines { "NEXT_X64=1", "NEXT_AVX=1" }
+	filter "platforms:*avx2"
+		architecture "x86_64"
+		vectorextensions "AVX2"
+		defines { "NEXT_X64=1", "NEXT_AVX=1", "NEXT_AVX2=1" }
+	filter "system:windows"
+		disablewarnings { "4221", "4244", "4715", "4197", "4146", "4324", "4456", "4100", "4459", "4245" }
+		linkoptions { "/ignore:4221" }
+	configuration { "gmake" }
+  		buildoptions { "-Wno-unused-parameter", "-Wno-unused-function", "-Wno-unknown-pragmas", "-Wno-unused-variable", "-Wno-type-limits" }
+
+project "client"
+	kind "ConsoleApp"
+	links { "enet", "next", "sodium" }
+	files { "client.cpp" }
+	includedirs { "enet", "next" }
+	filter "system:not windows"
+		links { "pthread" }
+	filter "system:macosx"
+		linkoptions { "-framework SystemConfiguration -framework CoreFoundation" }
+
+project "server"
+	kind "ConsoleApp"
+	links { "enet", "next", "sodium" }
+	files { "server.cpp" }
+	includedirs { "enet", "next" }
+	filter "system:not windows"
+		links { "pthread" }
+	filter "system:macosx"
+		linkoptions { "-framework SystemConfiguration -framework CoreFoundation" }
