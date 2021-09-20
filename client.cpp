724818fc351ea7a9c7ffb460633358958771762f
Glenn Fiedler <glenn@networknext.com>
2021-09-20 14:10:56 -0400

    client setup with signal and cleanup as well

--- client.cpp
+++ client.cpp
@@ -18,6 +18,8 @@ void interrupt_handler( int signal )
 
 int main( int argc, char ** argv ) 
 {
+    signal( SIGINT, interrupt_handler ); signal( SIGTERM, interrupt_handler );
+
     if ( next_init( NULL, NULL ) != NEXT_OK )
     {
         next_printf( NEXT_LOG_LEVEL_ERROR, "could not initialize network next" );
@@ -90,6 +92,11 @@ int main( int argc, char ** argv )
         next_sleep( 1.0f );
     }
 
+    printf( "\n" );
+    fflush( stdout );
+
+    next_printf( NEXT_LOG_LEVEL_INFO, "shutting down..." );
+
     enet_peer_reset( peer );
 
     enet_host_destroy( client );
@@ -97,6 +104,8 @@ int main( int argc, char ** argv )
     enet_deinitialize();
 
     next_term();
+
+    next_printf( NEXT_LOG_LEVEL_INFO, "done" );
 
     return 0;
 }
