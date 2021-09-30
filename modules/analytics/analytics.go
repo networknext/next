7dcf431298f4c7b04a8641b126d50c9a78b08c0b
nbopardi <nikhilb@gmail.com>
2021-09-30 10:22:23 -0700

    more interfaces

--- modules/analytics/analytics.go
+++ modules/analytics/analytics.go
@@ -4,6 +4,16 @@ import (
 	"context"
 )
 
+// PingStatsWriter is an interface that handles publishing ping stats entries to Google Pub/Sub
+type PingStatsPublisher interface {
+	Publish(ctx context.Context, entries []PingStatsEntry) error
+}
+
+// RelayStatsWriter is an interface that handles publishing relay stats entries to Google Pub/Sub
+type RelayStatsPublisher interface {
+	Publish(ctx context.Context, entries []RelayStatsEntry) error
+}
+
 // PingStatsWriter is an interface that handles writing ping stats entries to BigQuery
 type PingStatsWriter interface {
 	Write(ctx context.Context, entries []*PingStatsEntry) error
