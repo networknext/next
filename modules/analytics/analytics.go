17d5213d2449e13bf0d2733c713df32e3e26fc77
nbopardi <nikhilb@gmail.com>
2021-09-30 09:45:33 -0700

    move interfaces to their own file

---
+++ modules/analytics/analytics.go
@@ -0,0 +1,15 @@
+package analytics
+
+import (
+	"context"
+)
+
+// PingStatsWriter is an interface that handles writing ping stats entries to BigQuery
+type PingStatsWriter interface {
+	Write(ctx context.Context, entries []*PingStatsEntry) error
+}
+
+// RelayStatsWriter is an interface that handles writing relay stats entries to BigQuery
+type RelayStatsWriter interface {
+	Write(ctx context.Context, entries []*RelayStatsEntry) error
+}
