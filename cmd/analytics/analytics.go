9ee6b6f059e42504132b2ff53ed0b3070996829c
nbopardi <nikhilb@gmail.com>
2021-10-04 09:03:42 -0700

    add colons

--- cmd/analytics/analytics.go
+++ cmd/analytics/analytics.go
@@ -203,15 +203,15 @@ func mainReturnWithCode() int {
 
 		// Metrics
 		Goroutines                 int     `json:"goroutines"`
-		MemoryAllocated            float64 `json"mb_allocated"`
-		PingStatsEntriesReceived   int     `json"ping_stats_entries_received"`
-		PingStatsEntriesSubmitted  int     `json"ping_stats_entries_submitted"`
-		PingStatsEntriesQueued     int     `json"ping_stats_entries_queued"`
-		PingStatsEntriesFlushed    int     `json"ping_stats_entries_flushed"`
-		RelayStatsEntriesReceived  int     `json"relay_stats_entries_received"`
-		RelayStatsEntriesSubmitted int     `json"relay_stats_entries_submitted"`
-		RelayStatsEntriesQueued    int     `json"relay_stats_entries_queued"`
-		RelayStatsEntriesFlushed   int     `json"relay_stats_entries_flushed"`
+		MemoryAllocated            float64 `json:"mb_allocated"`
+		PingStatsEntriesReceived   int     `json:"ping_stats_entries_received"`
+		PingStatsEntriesSubmitted  int     `json:"ping_stats_entries_submitted"`
+		PingStatsEntriesQueued     int     `json:"ping_stats_entries_queued"`
+		PingStatsEntriesFlushed    int     `json:"ping_stats_entries_flushed"`
+		RelayStatsEntriesReceived  int     `json:"relay_stats_entries_received"`
+		RelayStatsEntriesSubmitted int     `json:"relay_stats_entries_submitted"`
+		RelayStatsEntriesQueued    int     `json:"relay_stats_entries_queued"`
+		RelayStatsEntriesFlushed   int     `json:"relay_stats_entries_flushed"`
 	}
 
 	statusData := &AnalyticsStatus{}
