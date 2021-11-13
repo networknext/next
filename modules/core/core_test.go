8912085e0f25f457f0ff32bee974f7b4fe6b52b6
Glenn Fiedler <glenn@networknext.com>
2021-11-13 10:45:53 -0500

    advanced packet filter

--- modules/core/core_test.go
+++ modules/core/core_test.go
@@ -7517,30 +7517,6 @@ func TestPittleAndChonkle(t *testing.T) {
     	GenerateChonkle(output[1:], magic[:], fromAddress[:], fromPort, toAddress[:], toPort, packetLength)
     	GeneratePittle(output[packetLength-2:], fromAddress[:], fromPort, toAddress[:], toPort, packetLength)
     	assert.Equal(t, true, BasicPacketFilter(output[:], packetLength))
+    	assert.Equal(t, true, AdvancedPacketFilter(output[:], magic[:], fromAddress[:], fromPort, toAddress[:], toPort, packetLength))
 	}
 }
-
-/*
-void test_pittle_and_chonkle()
-{
-    uint8_t output[1500];
-    memset( output, 0, sizeof(output) );
-    output[0] = 1;
-    for ( int i = 0; i < 10000; ++i )
-    {
-        uint8_t magic[8];
-        uint8_t from_address[4];
-        uint8_t to_address[4];
-        next_random_bytes( magic, 8 );
-        next_random_bytes( from_address, 4 );
-        next_random_bytes( to_address, 4 );
-        uint16_t from_port = uint16_t( i + 1000000 );
-        uint16_t to_port = uint16_t( i + 5000 );
-        int packet_length = 18 + ( i % ( sizeof(output) - 18 ) );
-        next_generate_chonkle( output + 1, magic, from_address, 4, from_port, to_address, 4, to_port, packet_length );
-        next_generate_pittle( output + packet_length - 2, from_address, 4, from_port, to_address, 4, to_port, packet_length );
-        next_check( next_basic_packet_filter( output, sizeof(output) ) );
-        next_check( next_advanced_packet_filter( output, magic, from_address, 4, from_port, to_address, 4, to_port, packet_length ) );
-    }
-}
-*/
