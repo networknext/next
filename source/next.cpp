f13a6afbc307b55ab61843b6e980e3bc253442d7
Glenn Fiedler <glenn@networknext.com>
2021-12-06 12:26:53 -0500

    updated code to write route request packet

--- source/next.cpp
+++ source/next.cpp
@@ -5068,10 +5068,6 @@ void next_relay_manager_send_pings( next_relay_manager_t * manager, next_platfor
     
     next_assert( socket );
 
-    // todo: we need ping token here
-    uint8_t ping_token[256];
-    memset( ping_token, 0, sizeof(ping_token) );
-
     uint8_t packet_data[NEXT_MAX_PACKET_BYTES];
 
     double current_time = next_time();
@@ -5086,6 +5082,10 @@ void next_relay_manager_send_pings( next_relay_manager_t * manager, next_platfor
         if ( manager->relay_last_ping_time[i] + ping_time <= current_time )
         {
             uint64_t ping_sequence = next_ping_history_ping_sent( &manager->relay_ping_history[i], next_time() );
+
+            // todo: we need the ping token here
+            uint8_t ping_token[1024];
+            memset( ping_token, 0, sizeof(ping_token) );
 
             // todo: need to fill out with real data
             uint8_t magic[8];
@@ -5934,13 +5934,35 @@ void next_route_manager_begin_next_route( next_route_manager_t * route_manager, 
     route_manager->route_data.pending_route_kbps_up = route_token.kbps_up;
     route_manager->route_data.pending_route_kbps_down = route_token.kbps_down;
     
-    // todo: this really should be write_route_request_packet
-    route_manager->route_data.pending_route_request_packet_bytes = 1 + ( num_tokens - 1 ) * NEXT_ENCRYPTED_ROUTE_TOKEN_BYTES;
-    route_manager->route_data.pending_route_request_packet_data[0] = NEXT_ROUTE_REQUEST_PACKET;
     memcpy( route_manager->route_data.pending_route_request_packet_data + 1, tokens + NEXT_ENCRYPTED_ROUTE_TOKEN_BYTES, ( size_t(num_tokens) - 1 ) * NEXT_ENCRYPTED_ROUTE_TOKEN_BYTES );
     memcpy( route_manager->route_data.pending_route_private_key, route_token.private_key, NEXT_CRYPTO_BOX_SECRETKEYBYTES );
-    
+
+    uint8_t * token_data = tokens + NEXT_ENCRYPTED_ROUTE_TOKEN_BYTES;
+    int token_bytes = ( num_tokens - 1 ) * NEXT_ENCRYPTED_ROUTE_TOKEN_BYTES;
+
+    // todo: we need real data here
+    uint8_t magic[8];
+    uint8_t from_address[4];
+    uint8_t to_address[4];
+    next_random_bytes( magic, 8 );
+    next_random_bytes( from_address, 4 );
+    next_random_bytes( to_address, 4 );
+    uint16_t from_port = uint16_t( 1000 );
+    uint16_t to_port = uint16_t( 5000 );
+
+    route_manager->route_data.pending_route_request_packet_bytes = next_write_route_request_packet( route_manager->route_data.pending_route_request_packet_data, token_data, token_bytes, magic, from_address, 4, from_port, to_address, 4, to_port );
+
+    next_assert( route_manager->route_data.pending_route_request_packet_bytes > 0 );
     next_assert( route_manager->route_data.pending_route_request_packet_bytes <= NEXT_MAX_PACKET_BYTES );
+
+    const uint8_t * packet_data = route_manager->route_data.pending_route_request_packet_data;
+    int packet_bytes = route_manager->route_data.pending_route_request_packet_bytes;
+
+    next_assert( next_basic_packet_filter( packet_data, packet_bytes ) );
+    next_assert( next_advanced_packet_filter( packet_data, magic, from_address, 4, from_port, to_address, 4, to_port, packet_bytes ) );
+
+    (void) packet_data;
+    (void) packet_bytes;
 }
 
 void next_route_manager_continue_next_route( next_route_manager_t * route_manager, bool committed, int num_tokens, uint8_t * tokens, const uint8_t * public_key, const uint8_t * private_key )
