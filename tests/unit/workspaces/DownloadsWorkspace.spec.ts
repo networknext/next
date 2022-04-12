63d92c5628402183e1e85632c8a99fbaabcc7f62
Andrew Baumbach <andrew@networknext.com>
2022-04-12 16:36:33 -0400

    fix tests

--- tests/unit/workspaces/DownloadsWorkspace.spec.ts
+++ tests/unit/workspaces/DownloadsWorkspace.spec.ts
@@ -10,7 +10,7 @@ import VueTour from 'vue-tour'
 import { FlagPlugin } from '@/plugins/flags'
 import { JSONRPCPlugin } from '@/plugins/jsonrpc'
 import VueGtag from 'vue-gtag'
-import { ENET_DOWNLOAD_EVENT, ENET_DOWNLOAD_URL, SDK_DOCUMENTATION_URL, SDK_DOWNLOAD_URL, UE4_PLUGIN_DOWNLOAD_URL, WHITE_PAPER_DOWNLOAD_EVENT, WHITE_PAPER_DOWNLOAD_URL } from '@/components/types/Constants'
+import { ENET_DOWNLOAD_EVENT, ENET_DOWNLOAD_URL, SDK_DOCUMENTATION_URL, SDK_DOWNLOAD_URL, UE4_PLUGIN_DOWNLOAD_URL, UE4_PLUGIN_SOURCE_URL, UNITY_PLUGIN_DOWNLOAD_URL, UNITY_PLUGIN_SOURCE_URL, WHITE_PAPER_DOWNLOAD_EVENT, WHITE_PAPER_DOWNLOAD_URL } from '@/components/types/Constants'
 
 describe('DownloadsWorkspace.vue', () => {
   const localVue = createLocalVue()
@@ -62,6 +62,9 @@ describe('DownloadsWorkspace.vue', () => {
   let windowSpy: jest.SpyInstance
   let spyDownloadSDK: jest.SpyInstance
   let spyDownloadUE4: jest.SpyInstance
+  let spyViewUE4: jest.SpyInstance
+  let spyDownloadUnity: jest.SpyInstance
+  let spyViewUnity: jest.SpyInstance
   let spyDownloadDocs: jest.SpyInstance
   let spyWhitePaperDownload: jest.SpyInstance
   let spyENetDownload: jest.SpyInstance
@@ -76,13 +79,22 @@ describe('DownloadsWorkspace.vue', () => {
     spyDownloadSDK = jest.spyOn(apiService, 'sendSDKDownloadSlackNotification').mockImplementation(() => {
       return Promise.resolve()
     })
-    spyDownloadUE4 = jest.spyOn(apiService, 'sendUE4DownloadNotifications').mockImplementation(() => {
+    spyDownloadUE4 = jest.spyOn(apiService, 'sendUE4DownloadNotification').mockImplementation(() => {
+      return Promise.resolve()
+    })
+    spyViewUE4 = jest.spyOn(apiService, 'sendUE4SourceViewNotification').mockImplementation(() => {
+      return Promise.resolve()
+    })
+    spyDownloadUnity = jest.spyOn(apiService, 'sendUnityDownloadNotification').mockImplementation(() => {
+      return Promise.resolve()
+    })
+    spyViewUnity = jest.spyOn(apiService, 'sendUnitySourceViewNotification').mockImplementation(() => {
       return Promise.resolve()
     })
     spyDownloadDocs = jest.spyOn(apiService, 'sendDocsViewSlackNotification').mockImplementation(() => {
       return Promise.resolve()
     })
-    spyWhitePaperDownload = jest.spyOn(apiService, 'send2022WhitePaperDownloadNotifications').mockImplementation(() => {
+    spyWhitePaperDownload = jest.spyOn(apiService, 'send2022WhitePaperDownloadNotification').mockImplementation(() => {
       return Promise.resolve()
     })
     spyENetDownload = jest.spyOn(apiService, 'sendENetDownloadNotification').mockImplementation(() => {
@@ -94,6 +106,9 @@ describe('DownloadsWorkspace.vue', () => {
     windowSpy.mockRestore()
     spyDownloadSDK.mockRestore()
     spyDownloadUE4.mockRestore()
+    spyViewUE4.mockRestore()
+    spyDownloadUnity.mockRestore()
+    spyViewUnity.mockRestore()
     spyDownloadDocs.mockRestore()
     spyWhitePaperDownload.mockRestore()
     spyENetDownload.mockRestore()
@@ -116,11 +131,14 @@ describe('DownloadsWorkspace.vue', () => {
 
     const buttons = wrapper.findAll('.btn')
 
-    expect(buttons.length).toBe(4)
+    expect(buttons.length).toBe(7)
     expect(buttons.at(0).text()).toBe('SDK v4.20')
-    expect(buttons.at(1).text()).toBe('UE4 Plugin')
-    expect(buttons.at(2).text()).toBe('ENet Support')
-    expect(buttons.at(3).text()).toBe('Documentation')
+    expect(buttons.at(1).text()).toBe('ENet Support')
+    expect(buttons.at(2).text()).toBe('Documentation')
+    expect(buttons.at(3).text()).toBe('Plugin Download')
+    expect(buttons.at(4).text()).toBe('Github Source')
+    expect(buttons.at(5).text()).toBe('Plugin Download')
+    expect(buttons.at(6).text()).toBe('Github Source')
     // expect(buttons.at(4).text()).toBe('Download')
   })
 
@@ -129,7 +147,7 @@ describe('DownloadsWorkspace.vue', () => {
     const wrapper = shallowMount(DownloadsWorkspace, { localVue, store })
     const buttons = wrapper.findAll('.btn')
 
-    expect(buttons.length).toBe(4)
+    expect(buttons.length).toBe(7)
 
     expectedURL = SDK_DOWNLOAD_URL
 
@@ -137,23 +155,41 @@ describe('DownloadsWorkspace.vue', () => {
     expect(windowSpy).toBeCalled()
     expect(spyDownloadSDK).toBeCalled()
 
-    expectedURL = UE4_PLUGIN_DOWNLOAD_URL
-
-    buttons.at(1).trigger('click')
-    expect(windowSpy).toBeCalled()
-    expect(spyDownloadUE4).toBeCalled()
-
     expectedURL = ENET_DOWNLOAD_URL
 
-    buttons.at(2).trigger('click')
+    buttons.at(1).trigger('click')
     expect(windowSpy).toBeCalled()
     expect(spyENetDownload).toBeCalled()
 
     expectedURL = SDK_DOCUMENTATION_URL
 
-    buttons.at(3).trigger('click')
+    buttons.at(2).trigger('click')
     expect(windowSpy).toBeCalled()
     expect(spyDownloadDocs).toBeCalled()
+
+    expectedURL = UE4_PLUGIN_DOWNLOAD_URL
+
+    buttons.at(3).trigger('click')
+    expect(windowSpy).toBeCalled()
+    expect(spyDownloadUE4).toBeCalled()
+
+    expectedURL = UE4_PLUGIN_SOURCE_URL
+
+    buttons.at(4).trigger('click')
+    expect(windowSpy).toBeCalled()
+    expect(spyViewUE4).toBeCalled()
+
+    expectedURL = UNITY_PLUGIN_DOWNLOAD_URL
+
+    buttons.at(5).trigger('click')
+    expect(windowSpy).toBeCalled()
+    expect(spyDownloadUnity).toBeCalled()
+
+    expectedURL = UNITY_PLUGIN_SOURCE_URL
+
+    buttons.at(6).trigger('click')
+    expect(windowSpy).toBeCalled()
+    expect(spyViewUnity).toBeCalled()
 
     /*
 
