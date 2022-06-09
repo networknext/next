18f6917e7372788f4587ef2fec3377cc89774bc2
Glenn Fiedler <glenn@networknext.com>
2022-06-09 15:51:11 -0400

    don't copy libnext.so in bootstrap.sh. this is pointless

--- deploy/bootstrap.sh
+++ deploy/bootstrap.sh
@@ -35,9 +35,6 @@ gsutil cp "$bucket/libpgm-5.2.so.0" '/usr/local/lib'
 # Copy libzmq from GCP Storage
 gsutil cp "$bucket/libzmq.so" '/usr/local/lib'
 
-# Copy libnext from GCP Storage
-gsutil cp "$bucket/libnext.so" '/usr/local/lib'
-
 # Refresh the known libs on the system
 ldconfig
 
