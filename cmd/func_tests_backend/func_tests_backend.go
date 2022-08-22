445a917ea647f80e27bf372a0e2d8fd73fa2aa51
Glenn Fiedler <glenn@networknext.com>
2022-08-22 17:09:41 -0400

    build more stuff in the func tests backend build stage

--- cmd/func_tests_backend/func_tests_backend.go
+++ cmd/func_tests_backend/func_tests_backend.go
@@ -9,6 +9,55 @@ import (
 	"fmt"
 )
 
+func run(command string, args ...string) bool {
+
+	cmd := exec.Command(command, args...)
+
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+    cmd.Stdout = &stdout
+    cmd.Stderr = &stderr
+
+    err := cmd.Run()
+
+    fmt.Printf("%s", stdout.String())
+    fmt.Printf("%s", stderr.String())
+
+    if err != nil {
+    	fmt.Printf("error: run failed: %v\n", err)
+    	return false
+    }
+
+    return true
+}
+
+/*
+func visual_studio_build(compiler string, config string, platform string, solution string) {
+	devenv := ""
+	if compiler == "vs2017" {
+		devenv = "/mnt/c/build/vs2017.sh"
+	} else if compiler == "vs2019" {
+		devenv = "/mnt/c/build/vs2019.sh"
+	} else if compiler == "vs2022" {
+		devenv = "/mnt/c/build/vs2022.sh"
+	} else {
+		fmt.Printf("error: unknown compiler %s\n", compiler)
+		os.Exit(1)
+	}
+	if platform != "" {
+		if !run("bash", devenv, "/Build", fmt.Sprintf("%s|%s", config, platform), solution) {
+			fmt.Printf("error: failed to build solution\n")
+			os.Exit(1)
+		}
+	} else {
+		if !run("bash", devenv, "/Build", config, solution) {
+			fmt.Printf("error: failed to build solution\n")
+			os.Exit(1)
+		}
+	}
+}
+*/
+
 func main() {
 	fmt.Printf("hello world\n")
 }
