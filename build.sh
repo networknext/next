1930ee7b3193484145cae4d13752c0ae231e6ac8
Glenn Fiedler <glenn@networknext.com>
2022-08-22 18:45:09 -0400

    tidy up build command to work on mac and linux

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
+#!/usr/bin/env bash
 
-mkdir dist
+mkdir -p dist
+
+LDFLAGS="-lsodium -lcurl -lpthread -lm"
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+  LDFLAGS="${LDFLAGS} -framework CoreFoundation -framework SystemConfiguration"
+fi
 
 parallel ::: \
-'cd ./dist && g++ -I../sdk4/include -shared -o sdk4.so ../sdk4/source/*.cpp -lsodium -lcurl -lpthread -lm -framework CoreFoundation -framework SystemConfiguration' \
-'cd ./dist && g++ -I../sdk5/include -shared -o sdk5.so ../sdk5/source/*.cpp -lsodium -lcurl -lpthread -lm -framework CoreFoundation -framework SystemConfiguration' \
-'cd ./dist && g++ -o reference_relay ../reference/relay/*.cpp -lsodium -lcurl -lpthread -lm -framework CoreFoundation -framework SystemConfiguration' \
-'go build -o ./dist/func_tests_backend ./cmd/func_tests_backend/*.go' \
-'go build -o ./dist/magic_backend ./cmd/magic_backend/*.go' \
-'go build -o ./dist/magic_frontend ./cmd/magic_frontend/*.go' \
-'go build -o ./dist/relay_gateway ./cmd/relay_gateway/*.go' \
-'go build -o ./dist/relay_backend ./cmd/relay_backend/*.go' \
-'go build -o ./dist/relay_frontend ./cmd/relay_frontend/*.go' \
+"cd ./dist && g++ -I../sdk4/include -shared -o sdk4.so ../sdk4/source/*.cpp ${LDFLAGS}" \
+"cd ./dist && g++ -I../sdk5/include -shared -o sdk5.so ../sdk5/source/*.cpp ${LDFLAGS}" \
+"cd ./dist && g++ -o reference_relay ../reference/relay/*.cpp ${LDFLAGS}" \
+"go build -o ./dist/func_tests_backend ./cmd/func_tests_backend/*.go" \
+"go build -o ./dist/magic_backend ./cmd/magic_backend/*.go" \
+"go build -o ./dist/magic_frontend ./cmd/magic_frontend/*.go" \
+"go build -o ./dist/relay_gateway ./cmd/relay_gateway/*.go" \
+"go build -o ./dist/relay_backend ./cmd/relay_backend/*.go" \
+"go build -o ./dist/relay_frontend ./cmd/relay_frontend/*.go" \
