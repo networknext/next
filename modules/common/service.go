d70663188fb0d02d4696de629d9de6270d64ede8
Glenn Fiedler <glenn@networknext.com>
2022-08-31 09:25:25 -0400

    moved database load and watch functionality into service. it's optional. only turns on once 'LoadDatabsae' is called

--- modules/common/service.go
+++ modules/common/service.go
@@ -7,11 +7,14 @@ import (
 	"os/signal"
 	"syscall"
 	"context"
+	"sort"
+	"time"
 
 	"github.com/networknext/backend/modules/backend"
 	"github.com/networknext/backend/modules/core"
 	"github.com/networknext/backend/modules/envvar"
 	"github.com/networknext/backend/modules/transport"
+	"github.com/networknext/backend/modules/routing"
 
 	"github.com/gorilla/mux"
 )
@@ -56,6 +59,21 @@ func CreateService(serviceName string) *Service {
 	return &service
 }
 
+func (service *Service) LoadDatabase() {
+
+	databasePath := envvar.Get("DATABASE_PATH", "database.bin")
+	overlayPath := envvar.Get("OVERLAY_PATH", "overlay.bin")
+
+	database, overlay := loadDatabase(databasePath, overlayPath)
+
+	// todo: store database etc.
+
+	_ = database
+	_ = overlay
+
+	watchDatabase(service.Context, databasePath, overlayPath)
+}
+
 func (service *Service) StartWebServer() {
 	port := envvar.Get("HTTP_PORT", "80")
 	fmt.Printf("starting http server on port %s\n", port)
@@ -75,4 +93,123 @@ func (service *Service) WaitForShutdown() {
 	core.Debug("received shutdown signal")
 	// todo: probably need to wait for some stuff...
 	core.Debug("successfully shutdown")
+}
+
+// -----------------------------------------------------------------------
+
+func loadDatabase(databasePath string, overlayPath string) (*routing.DatabaseBinWrapper, *routing.OverlayBinWrapper) {
+
+	// load the database (required)
+
+	databaseFile, err := os.Open(databasePath)
+	if err != nil {
+		core.Error("could not load database: %v", err)
+		return nil, nil
+	}
+	defer databaseFile.Close()
+
+	database := routing.CreateEmptyDatabaseBinWrapper()
+	err = backend.DecodeBinWrapper(databaseFile, database); 
+	if err != nil || database.IsEmpty() {
+		core.Error("error: could not read database: %v", err)
+		return nil, nil
+	}
+
+	fmt.Printf("loaded database: '%s'\n", databasePath)
+
+	// load the overlay if it exists
+
+	overlayFile, err := os.Open(overlayPath)
+	if err != nil {
+		return database, nil
+	}
+	defer overlayFile.Close()
+	
+	overlay := routing.CreateEmptyOverlayBinWrapper()
+	err = backend.DecodeOverlayWrapper(overlayFile, overlay)
+	if err != nil || overlay.IsEmpty() {
+		return database, nil
+	}
+
+	// IMPORTANT: discard the overlay if it's older than the database
+	if database.CreationTime > overlay.CreationTime {
+		return database, nil
+	}
+
+	fmt.Printf("loaded overlay: '%s'\n", overlayPath)
+
+	return database, overlay
+}
+
+func generateSecondaryValues(database *routing.DatabaseBinWrapper) (map[uint64]routing.Relay, []routing.Relay) {
+
+	relayHash := make(map[uint64]routing.Relay)
+	relayArray := database.Relays
+
+	sort.SliceStable(relayArray, func(i, j int) bool {
+		return relayArray[i].Name < relayArray[j].Name
+	})
+
+	for i := range relayArray {
+		fmt.Printf("loaded relay %s\n", relayArray[i].Name)
+		relayHash[relayArray[i].ID] = relayArray[i]
+	}
+
+	return relayHash, relayArray
+}
+
+func applyOverlay(database *routing.DatabaseBinWrapper, overlay *routing.OverlayBinWrapper) {
+	if overlay != nil {
+		for _, buyer := range overlay.BuyerMap {
+			_, ok := database.BuyerMap[buyer.ID]
+			if !ok {
+				database.BuyerMap[buyer.ID] = buyer
+			}
+		}
+	}
+}
+
+func fileExists(filename string) bool {
+ 	_, err := os.Stat(filename)
+    if err == nil {
+        return true
+    }
+    return false
+}
+
+func watchDatabase(ctx context.Context, databasePath string, overlayPath string) {
+
+	syncInterval := envvar.GetDuration("DATABASE_SYNC_INTERVAL", time.Second)//time.Minute)
+
+	go func() {
+
+		ticker := time.NewTicker(syncInterval)
+
+		// todo: wg done defer
+
+		for {
+			select {
+
+			case <-ctx.Done():
+				return
+
+			case <-ticker.C:
+
+				newDatabase, newOverlay := loadDatabase(databasePath, overlayPath)
+
+				if newDatabase == nil {
+					continue
+				}
+
+				relayHashNew, relayArrayNew := generateSecondaryValues(newDatabase)
+
+				applyOverlay(newDatabase, newOverlay)
+
+				_ = relayHashNew
+				_ = relayArrayNew
+
+				// todo: pointer swaps under mutex (don't need to hold on to the overlay)
+			}
+		}
+	}()
 }
