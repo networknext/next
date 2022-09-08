de99e4ec5f6a8e3e46b641497dce43da958daf97
Glenn Fiedler <glenn@networknext.com>
2022-09-08 12:41:38 -0400

    relax long relay update. since we are now talking to redis pubsub, we'll get some long mutex pauses as push back when a batch is flushed. normal

--- cmd/relay_gateway/relay_gateway.go
+++ cmd/relay_gateway/relay_gateway.go
@@ -117,7 +117,7 @@ func RelayUpdateHandler(getRelayData func() *common.RelayData, getMagicValues fu
 
 		defer func() {
 			duration := time.Since(startTime)
-			if duration.Milliseconds() > 100 {
+			if duration.Milliseconds() > 1000 {
 				core.Error("long relay update: %s", duration.String())
 			}
 		}()
