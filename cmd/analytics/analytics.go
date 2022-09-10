7197e5a23d60b250a6ca92549ca1f8042af0d5df
Glenn Fiedler <glenn@networknext.com>
2022-09-09 23:13:36 -0400

    setup new service with cost matrix and route matrix loops

--- cmd/analytics/analytics.go
+++ cmd/analytics/analytics.go
@@ -3,26 +3,36 @@ package main
 import (
 	"context"
 	"time"
-
+	"io/ioutil"
+	"net/http"
+	
 	"github.com/networknext/backend/modules/core"
 	"github.com/networknext/backend/modules/common"
 	"github.com/networknext/backend/modules/envvar"
 )
 
+var costMatrixURI string
 var routeMatrixURI string
+var costMatrixInterval time.Duration
 var routeMatrixInterval time.Duration
 
 func main() {
 
 	service := common.CreateService("analytics")
 
-	routeMatrixURI = envvar.GetString("ROUTE_MATRIX_URI", "http://127.0.0.1:30001")
+	costMatrixURI = envvar.GetString("COST_MATRIX_URI", "http://127.0.0.1:30001/cost_matrix")
+	routeMatrixURI = envvar.GetString("ROUTE_MATRIX_URI", "http://127.0.0.1:30001/route_matrix")
+	costMatrixInterval = envvar.GetDuration("ROUTE_MATRIX_INTERVAL", 10 * time.Second)
 	routeMatrixInterval = envvar.GetDuration("ROUTE_MATRIX_INTERVAL", 10 * time.Second)
 
+	core.Log("cost matrix uri: %s", costMatrixURI)
 	core.Log("route matrix uri: %s", routeMatrixURI)
+	core.Log("cost matrix interval: %s", costMatrixInterval)
 	core.Log("route matrix interval: %s", routeMatrixInterval)
 
-	ProcessRelayStats(service.Context)
+	ProcessCostMatrix(service.Context)
+
+	ProcessRouteMatrix(service.Context)
 
 	ProcessBilling(service.Context)
 
@@ -33,18 +43,84 @@ func main() {
 	service.WaitForShutdown()
 }
 
-func ProcessRelayStats(ctx context.Context) {
+func ProcessCostMatrix(ctx context.Context) {
+
+	httpClient := &http.Client{
+		Timeout: routeMatrixInterval,
+	}
 
 	ticker := time.NewTicker(routeMatrixInterval)
 
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case <-ticker.C:
-			core.Debug("get route matrix")
+	go func() {	
+		for {
+			select {
+
+			case <-ctx.Done():
+				return
+
+			case <-ticker.C:
+
+				core.Debug("get cost matrix")
+
+				response, err := httpClient.Get(routeMatrixURI)
+				if err != nil {
+					core.Error("failed to http get cost matrix: %v", err)
+					continue
+				}
+
+				buffer, err := ioutil.ReadAll(response.Body)
+				if err != nil {
+					core.Error("failed to read cost matrix: %v", err)
+					continue
+				}
+
+				response.Body.Close()
+
+				// todo: read in cost matrix
+				_ = buffer
+			}
 		}
+	}()
+}
+
+func ProcessRouteMatrix(ctx context.Context) {
+
+	httpClient := &http.Client{
+		Timeout: routeMatrixInterval,
 	}
+
+	ticker := time.NewTicker(routeMatrixInterval)
+
+	go func() {	
+		for {
+			select {
+
+			case <-ctx.Done():
+				return
+
+			case <-ticker.C:
+
+				core.Debug("get route matrix")
+
+				response, err := httpClient.Get(routeMatrixURI)
+				if err != nil {
+					core.Error("failed to http get route matrix: %v", err)
+					continue
+				}
+
+				buffer, err := ioutil.ReadAll(response.Body)
+				if err != nil {
+					core.Error("failed to read route matrix: %v", err)
+					continue
+				}
+
+				response.Body.Close()
+
+				// todo: read in route matrix
+				_ = buffer
+			}
+		}
+	}()
 }
 
 func ProcessBilling(ctx context.Context) {
