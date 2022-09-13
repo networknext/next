98674daacc4d3c28288a06003a63e5ae4298f659
Glenn Fiedler <glenn@networknext.com>
2022-09-13 17:37:46 -0400

    nope

--- cmd/func_tests_backend/func_tests_backend.go
+++ cmd/func_tests_backend/func_tests_backend.go
@@ -344,7 +344,7 @@ func test_google_pubsub() {
 
 	waitGroup.Add(NumProducers)
 
-	const NumMessagesPerProducer = 10000
+	const NumMessagesPerProducer = 100000
 
 	for i := 0; i < NumProducers; i++ {
 
