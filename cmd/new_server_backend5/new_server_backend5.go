67e918d4e069365ec9e8c1504c15395685b6e888
Glenn Fiedler <glenn@networknext.com>
2022-09-26 17:47:02 -0400

    cleaned up new server backend 5 by putting the route matrix related stuff into service.go

--- cmd/new_server_backend5/new_server_backend5.go
+++ cmd/new_server_backend5/new_server_backend5.go
@@ -1,36 +1,20 @@
 package main
 
-// #cgo pkg-config: libsodium
-// #include <sodium.h>
-import "C"
-
 import (
-	"bytes"
-	"encoding/gob"
-	"io/ioutil"
 	"net"
 	"net/http"
-	"sync"
-	"time"
 
 	"github.com/networknext/backend/modules/common"
 	"github.com/networknext/backend/modules/core"
 	"github.com/networknext/backend/modules/envvar"
 	"github.com/networknext/backend/modules/handlers"
-	"github.com/networknext/backend/modules/routing"
 )
 
 var service *common.Service
 
 var maxPacketSize int
 var serverBackendAddress net.UDPAddr
-var routeMatrixURI string
-var routeMatrixInterval time.Duration
-var privateKey []byte
-
-var routeMatrixMutex sync.RWMutex
-var routeMatrix *common.RouteMatrix
-var database *routing.DatabaseBinWrapper
+var serverBackendPrivateKey []byte
 
 func main() {
 
@@ -38,16 +22,12 @@ func main() {
 
 	maxPacketSize = envvar.GetInt("UDP_MAX_PACKET_SIZE", 4096)
 	serverBackendAddress = *envvar.GetAddress("SERVER_BACKEND_ADDRESS", core.ParseAddress("127.0.0.1:45000"))
-	routeMatrixURI = envvar.GetString("ROUTE_MATRIX_URI", "http://127.0.0.1:30001/route_matrix")
-	routeMatrixInterval = envvar.GetDuration("ROUTE_MATRIX_INTERVAL", time.Second)
-	privateKey = envvar.GetBase64("SERVER_BACKEND_PRIVATE_KEY", []byte{})
+	serverBackendPrivateKey = envvar.GetBase64("SERVER_BACKEND_PRIVATE_KEY", []byte{})
 
 	core.Log("max packet size: %d bytes", maxPacketSize)
 	core.Log("server backend address: %s", serverBackendAddress.String())
-	core.Log("route matrix uri: %s", routeMatrixURI)
-	core.Log("route matrix interval: %s", routeMatrixInterval.String())
 
-	updateRouteMatrix()
+	service.UpdateRouteMatrix()
 
 	service.OverrideHealthHandler(healthHandler)
 
@@ -62,9 +42,9 @@ func main() {
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 
-	routeMatrixMutex.RLock()
+	routeMatrix, _ := service.RouteMatrixAndDatabase()
+
 	not_ready := routeMatrix == nil
-	routeMatrixMutex.RUnlock()
 
 	if not_ready {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -77,89 +57,13 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 func packetHandler(conn *net.UDPConn, from *net.UDPAddr, packetData []byte) {
 
 	handler := handlers.SDK5_Handler{}
-
-	routeMatrixMutex.Lock()
-	handler.Database = database
-	handler.RouteMatrix = routeMatrix
-	routeMatrixMutex.Unlock()
-
+	handler.RouteMatrix, handler.Database = service.RouteMatrixAndDatabase()
 	handler.MaxPacketSize = maxPacketSize
 	handler.ServerBackendAddress = serverBackendAddress
-	handler.PrivateKey = privateKey
+	handler.PrivateKey = serverBackendPrivateKey
 	handler.GetMagicValues = func() ([]byte, []byte, []byte) {
 		return service.GetMagicValues()
 	}
 
 	handlers.SDK5_PacketHandler(&handler, conn, from, packetData)
-}
-
-func updateRouteMatrix() {
-
-	httpClient := &http.Client{
-		Timeout: routeMatrixInterval,
-	}
-
-	ticker := time.NewTicker(routeMatrixInterval)
-
-	go func() {
-		for {
-			select {
-
-			case <-service.Context.Done():
-				return
-
-			case <-ticker.C:
-
-				routeMatrixMutex.Lock()
-				currentRouteMatrix := routeMatrix
-				routeMatrixMutex.Unlock()
-
-				if currentRouteMatrix != nil && time.Now().Unix()-int64(currentRouteMatrix.CreatedAt) > 30 {
-					core.Error("route matrix is stale")
-					routeMatrixMutex.Lock()
-					routeMatrix = nil
-					routeMatrixMutex.Unlock()
-				}
-
-				response, err := httpClient.Get(routeMatrixURI)
-				if err != nil {
-					core.Error("failed to http get route matrix: %v", err)
-					continue
-				}
-
-				buffer, err := ioutil.ReadAll(response.Body)
-				if err != nil {
-					core.Error("failed to read route matrix: %v", err)
-					continue
-				}
-
-				response.Body.Close()
-
-				newRouteMatrix := common.RouteMatrix{}
-
-				err = newRouteMatrix.Read(buffer)
-				if err != nil {
-					core.Error("failed to read route matrix: %v", err)
-					continue
-				}
-
-				var newDatabase routing.DatabaseBinWrapper
-
-				databaseBuffer := bytes.NewBuffer(newRouteMatrix.BinFileData)
-				decoder := gob.NewDecoder(databaseBuffer)
-				err = decoder.Decode(&newDatabase)
-				if err != nil {
-					core.Error("failed to read database: %v", err)
-					continue
-				}
-
-				routeMatrixMutex.Lock()
-				routeMatrix = &newRouteMatrix
-				database = &newDatabase
-				routeMatrixMutex.Unlock()
-
-				core.Debug("updated route matrix: %d relays", len(routeMatrix.RelayIds))
-			}
-		}
-	}()
 }
