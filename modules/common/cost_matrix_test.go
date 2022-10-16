6bc9758f7a292c9215e9627859570abd9bbc1d81
Glenn Fiedler <glenn@networknext.com>
2022-10-16 14:36:33 -0400

    sketch out dummy unit tests for cost and route matrix

--- modules/common/cost_matrix_test.go
+++ modules/common/cost_matrix_test.go
@@ -3,12 +3,39 @@ package common_test
 import (
 	"testing"
 
-	_ "github.com/networknext/backend/modules/common"
+	"github.com/networknext/backend/modules/common"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func GenerateRandomCostMatrix() common.CostMatrix {
+
+	return common.CostMatrix{
+		// todo
+	}
+}
+
+func CostMatrixReadWriteTest(writeMessage *common.CostMatrix, readMessage *common.CostMatrix, t *testing.T) {
+
+	const BufferSize = 10 * 1024
+
+	buffer := make([]byte, BufferSize)
+
+	buffer = writeMessage.Write(buffer[:])
+
+	err := readMessage.Read(buffer)
+	assert.Nil(t, err)
+
+	assert.Equal(t, writeMessage, readMessage)
+}
+
 func TestCostMatrixReadWrite(t *testing.T) {
+
 	t.Parallel()
-	assert.True(t, true)
+
+	writeMessage := GenerateRandomCostMatrix()
+
+	readMessage := common.CostMatrix{}
+
+	CostMatrixReadWriteTest(&writeMessage, &readMessage, t)
 }
