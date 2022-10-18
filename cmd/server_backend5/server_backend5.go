b369a019fc504bafbd72f2c6b65eb69598d1db64
Andrew Baumbach <andrew@networknext.com>
2022-10-18 14:11:59 -0400

    Fix server backend 5 in dev

--- cmd/server_backend5/server_backend5.go
+++ cmd/server_backend5/server_backend5.go
@@ -21,11 +21,9 @@ func main() {
 	service = common.CreateService("new_server_backend5")
 
 	maxPacketSize = envvar.GetInt("UDP_MAX_PACKET_SIZE", 4096)
-	serverBackendAddress = *envvar.GetAddress("SERVER_BACKEND_ADDRESS", core.ParseAddress("127.0.0.1:45000"))
 	serverBackendPrivateKey = envvar.GetBase64("SERVER_BACKEND_PRIVATE_KEY", []byte{})
 
 	core.Log("max packet size: %d bytes", maxPacketSize)
-	core.Log("server backend address: %s", serverBackendAddress.String())
 
 	service.UpdateRouteMatrix()
 
