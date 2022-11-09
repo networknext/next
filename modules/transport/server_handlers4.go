87c126cfddcc95b5d229bbb8bc6ecae503c07a4e
Andrew Baumbach <andrew@networknext.com>
2022-11-09 10:03:37 -0500

    stick to portal backend only

--- modules/transport/server_handlers4.go
+++ modules/transport/server_handlers4.go
@@ -1891,7 +1891,7 @@ func BuildPortalData(state *SessionHandlerState) *SessionPortalData {
 		},
 		Slice: SessionSlice{
 			Version:   SessionSliceVersion,
-			Timestamp: time.Now().UTC(),
+			Timestamp: time.Now(),
 			Next: routing.Stats{
 				RTT:        float64(state.Packet.NextRTT),
 				Jitter:     float64(state.Packet.NextJitter),
