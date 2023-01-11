1618b097c0ceecdf79462f642d67903f95c7dd30
Glenn Fiedler <glenn@networknext.com>
2023-01-11 16:19:37 -0500

    put logs in on health check for relay backend so we can see what's up

--- cmd/relay_backend/relay_backend.go
+++ cmd/relay_backend/relay_backend.go
@@ -157,8 +157,11 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	hasRouteMatrix := len(routeMatrixData) > 0
 	routeMatrixMutex.RUnlock()
 	if ready && hasRouteMatrix {
+		core.Debug("health check OK")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(http.StatusText(http.StatusOK)))
+	} else {
+		core.Debug("health check NOT OK")
 	}
 }
 
