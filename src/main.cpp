c7aad3ee25582481f1ac256dac3bb082737857f0
Glenn Fiedler <glenn@networknext.com>
2023-01-16 16:21:46 -0500

    update copyright

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /*
  * Network Next Relay.
- * Copyright © 2017 - 2020 Network Next, Inc. All rights reserved.
+ * Copyright © 2017 - 2023 Network Next, Inc. All rights reserved.
  */
 
 #include "includes.h"
