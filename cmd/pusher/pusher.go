ae07b46a4b46221c77372741b9360f419df5294c
Andrew Baumbach <andrew@networknext.com>
2023-01-19 11:40:30 -0500

    Update validation process for bin and ip2location files

--- cmd/pusher/pusher.go
+++ cmd/pusher/pusher.go
@@ -33,7 +33,7 @@ func main() {
 		envvar.GetDuration("LOCATION_FILE_REFRESH_INTERVAL", 5*time.Minute),
 		envvar.GetList("LOCATION_FILE_DESTINATION_MIGS", []string{}),
 		envvar.GetString("LOCATION_FILE_BUCKET_PATH", "gs://network-next-local-upload"),
-		validateLocationFiles,
+		service.ValidateIP2Location,
 		ispSyncFile,
 		citySyncFile,
 	)
@@ -51,7 +51,7 @@ func main() {
 		envvar.GetDuration("BIN_FILE_REFRESH_INTERVAL", time.Minute),
 		envvar.GetList("BIN_FILE_DESTINATION_MIGS", []string{}),
 		"",
-		validateBinFiles,
+		service.ValidateBinFiles,
 		databaseSyncFile,
 		overlaySyncFile,
 	)
@@ -69,98 +69,66 @@ func validateLocationFiles(locationFiles []string) bool {
 
 	// todo: much simpler validation
 	/*
-	ipStr := "192.0.2.1"
-	testIP := net.ParseIP(ipStr)
-	if testIP == nil {
-		return false
-	}
+		ipStr := "192.0.2.1"
+		testIP := net.ParseIP(ipStr)
+		if testIP == nil {
+			return false
+		}
 
-	ispFile := locationFiles[0]
+		ispFile := locationFiles[0]
 
-	ispBytes, err := ioutil.ReadFile(ispFile)
-	if err != nil {
-		core.Error("failed to read location file: %v", err)
-		return false
-	}
+		ispBytes, err := ioutil.ReadFile(ispFile)
+		if err != nil {
+			core.Error("failed to read location file: %v", err)
+			return false
+		}
 
-	ispReader, err := geoip2.FromBytes(ispBytes)
-	if err != nil {
-		core.Error("failed to create geo reader: %v", err)
-		return false
-	}
-	defer ispReader.Close()
+		ispReader, err := geoip2.FromBytes(ispBytes)
+		if err != nil {
+			core.Error("failed to create geo reader: %v", err)
+			return false
+		}
+		defer ispReader.Close()
 
-	cityFile := locationFiles[1]
+		cityFile := locationFiles[1]
 
-	cityBytes, err := ioutil.ReadFile(cityFile)
-	if err != nil {
-		core.Error("failed to read location file: %v", err)
-		return false
-	}
+		cityBytes, err := ioutil.ReadFile(cityFile)
+		if err != nil {
+			core.Error("failed to read location file: %v", err)
+			return false
+		}
 
-	cityReader, err := geoip2.FromBytes(cityBytes)
-	if err != nil {
-		core.Error("failed to create geo reader: %v", err)
-		return false
-	}
+		cityReader, err := geoip2.FromBytes(cityBytes)
+		if err != nil {
+			core.Error("failed to create geo reader: %v", err)
+			return false
+		}
 
-	location := routing.Location{}
+		location := routing.Location{}
 
-	city, err := cityReader.City(testIP)
-	if err != nil {
-		core.Error("failed to look up city: %v", err)
-		return false
-	}
-	defer cityReader.Close()
+		city, err := cityReader.City(testIP)
+		if err != nil {
+			core.Error("failed to look up city: %v", err)
+			return false
+		}
+		defer cityReader.Close()
 
-	location.Latitude = float32(city.Location.Latitude)
-	location.Longitude = float32(city.Location.Longitude)
+		location.Latitude = float32(city.Location.Latitude)
+		location.Longitude = float32(city.Location.Longitude)
 
-	isp, err := ispReader.ISP(testIP)
-	if err != nil {
-		core.Error("failed to look up ISP: %v", err)
-		return false
-	}
+		isp, err := ispReader.ISP(testIP)
+		if err != nil {
+			core.Error("failed to look up ISP: %v", err)
+			return false
+		}
 
-	location.ISP = isp.ISP
-	location.ASN = int(isp.AutonomousSystemNumber)
+		location.ISP = isp.ISP
+		location.ASN = int(isp.AutonomousSystemNumber)
 
-	if location == routing.LocationNullIsland {
-		core.Error("location returned as null island")
-		return false
-	}
-	*/
-
-	return true
-}
-
-func validateBinFiles(binFiles []string) bool {
-
-	databaseFile := binFiles[0]
-	overlayFile := binFiles[1]
-
-	// todo
-	_ = databaseFile
-	_ = overlayFile
-
-	/*
-	databaseWrapper := routing.DatabaseBinWrapper{}
-	overlayWrapper := routing.OverlayBinWrapper{}
-
-	if err := databaseWrapper.ReadDatabaseBinFile(databaseFile); err != nil {
-		core.Error("failed to read database file: %v", err)
-		return false
-	}
-
-	if databaseWrapper.IsEmpty() {
-		core.Error("database file can not be empty")
-		return false
-	}
-
-	if err := overlayWrapper.ReadOverlayBinFile(overlayFile); err != nil {
-		core.Error("failed to read overlay file: %v", err)
-		return false
-	}
+		if location == routing.LocationNullIsland {
+			core.Error("location returned as null island")
+			return false
+		}
 	*/
 
 	return true
