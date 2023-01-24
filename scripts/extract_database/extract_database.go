bf2a8f4624f15e4e63d70f82d5ae52f26126e7bb
Glenn Fiedler <glenn@networknext.com>
2023-01-24 13:40:25 -0500

    extracted all relay fields

--- scripts/extract_database/extract_database.go
+++ scripts/extract_database/extract_database.go
@@ -22,7 +22,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	rows, err := pgsql.Query("SELECT id, display_name FROM relays")
+	rows, err := pgsql.Query("SELECT id, display_name, datacenter, public_ip, public_port, internal_ip, internal_port, ssh_ip, ssh_port, ssh_user, public_key_base64, private_key_base64, mrc, port_speed, max_sessions FROM relays")
 	if err != nil {
         fmt.Printf("error: could not extract relays: %v\n", err)
         os.Exit(1)
@@ -35,12 +35,28 @@ func main() {
 	fmt.Printf("\nrelays:\n")
 
 	for rows.Next() {
+
         var id uint64
         var name string
-        if err := rows.Scan(&id, &name); err != nil {
+        var datacenter uint64
+        var public_ip string
+        var public_port int
+        var internal_ip string
+        var internal_port int
+        var ssh_ip string
+        var ssh_port int
+        var ssh_user string
+        var public_key_base64 string
+        var private_key_base64 string
+        var mrc int
+        var port_speed int
+        var max_sessions int
+
+        if err := rows.Scan(&id, &name, &datacenter, &public_ip, &public_port, &internal_ip, &internal_port, &ssh_ip, &ssh_port, &ssh_user, &public_key_base64, &private_key_base64, &mrc, &port_speed, &max_sessions); err != nil {
             fmt.Printf("error: failed to scan relay row: %v\n", err)
             os.Exit(1)
         }
-        fmt.Printf("%d: %s\n", id, name)
+
+        fmt.Printf("%d: %s, %d, %s, %d, %s, %d, %s, %d, %s, %s, %s, %d, %d, %d\n", id, name, datacenter, public_ip, public_port, internal_ip, internal_port, ssh_ip, ssh_port, ssh_user, public_key_base64, private_key_base64, mrc, port_speed, max_sessions)
     }
 }
