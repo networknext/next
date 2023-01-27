ab3a57ae512e5557051a52343c2324c36becba1c
Glenn Fiedler <glenn@networknext.com>
2023-01-26 22:19:26 -0500

    trash night

--- modules-old/storage/multipath_veto_handler.go
+++
@@ -1,7 +0,0 @@
-package storage
-
-type MultipathVetoHandler interface {
-	GetMapCopy(companyCode string) map[uint64]bool
-	MultipathVetoUser(companyCode string, userHash uint64) error
-	Sync() error
-}
