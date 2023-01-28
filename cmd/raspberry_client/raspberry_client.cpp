b3222b34e968a261c7d5889b993e5d04b9aa8173
Glenn Fiedler <glenn@networknext.com>
2023-01-28 14:03:21 -0500

    fallback to direct alt

--- cmd/raspberry_client/raspberry_client.cpp
+++ cmd/raspberry_client/raspberry_client.cpp
@@ -167,7 +167,11 @@ void client_thread_function( void * data )
 
         while ( !quit )
         {
+            // todo
+            /*
             if ( next_client_fallback_to_direct( client ) )
+            */
+            if ( next_client_stats(client)->fallback_to_direct )
             {
                 printf( "client has fallen back to direct. reconnecting...\n");
                 break;
