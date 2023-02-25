50cdd24ca9fd7786d23106e143aa51da0f9dce8b
Glenn Fiedler <glenn@networknext.com>
2023-02-25 18:18:59 -0500

    try again

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -24,7 +24,7 @@ func main() {
 
 	service := common.CreateService("api")
 
-	pgsqlConfig := envvar.GetString("PGSQL_CONFIG", "host=127.0.0.1 port=5432 username=postgres dbname=postgres sslmode=disable")
+	pgsqlConfig := envvar.GetString("PGSQL_CONFIG", "host=127.0.0.1 port=5432 user=postgres dbname=postgres sslmode=disable")
 	redisHostname := envvar.GetString("REDIS_HOSTNAME", "127.0.0.1:6379")
 	redisPoolActive := envvar.GetInt("REDIS_POOL_ACTIVE", 1000)
 	redisPoolIdle := envvar.GetInt("REDIS_POOL_IDLE", 10000)
--- tools/extract_database/extract_database.go
+++ tools/extract_database/extract_database.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 
-	database, err := db.ExtractDatabase("host=127.0.0.1 port=5432 username=postgres dbname=postgres sslmode=disable")
+	database, err := db.ExtractDatabase("host=127.0.0.1 port=5432 user=postgres dbname=postgres sslmode=disable")
 
 	if err != nil {
 		fmt.Printf("error: failed to extract database: %v\n", err)
