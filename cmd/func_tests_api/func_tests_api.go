2b9fb286a6e54dcd00ba37ae5d19092ff71c81e5
Glenn Fiedler <glenn@networknext.com>
2023-02-25 19:35:49 -0500

    very simple load test framework for api started

--- cmd/func_tests_api/func_tests_api.go
+++ cmd/func_tests_api/func_tests_api.go
@@ -6,180 +6,46 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"os/exec"
 	"runtime"
 	"reflect"
 	"time"
 )
 
-/*
-/*
-	// run the magic backend and make sure it runs and does things it's expected to do
+func api() (*exec.Cmd, *bytes.Buffer) {
 
-	cmd := exec.Command("./magic_backend")
-
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	cmd.Env = make([]string, 0)
-	cmd.Env = append(cmd.Env, "ENV=local")
-	cmd.Env = append(cmd.Env, "DEBUG_LOGS=1")
-	cmd.Env = append(cmd.Env, "HTTP_PORT=40000")
-	cmd.Env = append(cmd.Env, "MAGIC_UPDATE_SECONDS=5")
-
-	err := cmd.Start()
-	if err != nil {
-		fmt.Printf("\nerror: failed to run magic backend!\n\n")
-		fmt.Printf("%s", stdout.String())
-		fmt.Printf("%s", stderr.String())
-		os.Exit(1)
+	cmd := exec.Command("./api")
+	if cmd == nil {
+		panic("could not create api!\n")
+		return nil, nil
 	}
 
-	time.Sleep(20 * time.Second)
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env, "HTTP_PORT=50000")
 
-	check_output("magic_backend", cmd, stdout, stderr)
-	check_output("starting http server on port 40000", cmd, stdout, stderr)
+	var output bytes.Buffer
+	cmd.Stdout = &output
+	cmd.Stderr = &output
+	cmd.Start()
 
-	// test the vm health check
-
-	response, err := http.Get("http://127.0.0.1:40000/vm_health")
-	if err != nil || response.StatusCode != 200 {
-		fmt.Printf("error: vm health check failed\n")
-		cmd.Process.Signal(syscall.SIGTERM)
-		os.Exit(1)
-	}
-
-	// test the lb health check
-
-	response, err = http.Get("http://127.0.0.1:40000/lb_health")
-	if err != nil || response.StatusCode != 200 {
-		fmt.Printf("error: lb health check failed\n")
-		cmd.Process.Signal(syscall.SIGTERM)
-		os.Exit(1)
-	}
-
-	// test the version endpoint
-
-	_, err = http.Get("http://127.0.0.1:40000/version")
-	if err != nil || response.StatusCode != 200 {
-		fmt.Printf("error: version endpoint failed\n")
-		cmd.Process.Signal(syscall.SIGTERM)
-		os.Exit(1)
-	}
-
-	// test the magic values endpoint
-
-	response, err = http.Get("http://127.0.0.1:40000/magic")
-	if err != nil || response.StatusCode != 200 {
-		fmt.Printf("error: magic endpoint failed\n")
-		cmd.Process.Signal(syscall.SIGTERM)
-		os.Exit(1)
-	}
-
-	magicData, error := ioutil.ReadAll(response.Body)
-	if error != nil {
-		fmt.Printf("error: failed to read magic response data\n")
-		cmd.Process.Signal(syscall.SIGTERM)
-		os.Exit(1)
-	}
-
-	if len(magicData) != 32 {
-		fmt.Printf("error: magic data should be 32 bytes long\n")
-		cmd.Process.Signal(syscall.SIGTERM)
-		os.Exit(1)
-	}
-
-	time.Sleep(time.Second)
-
-	check_output("served magic values", cmd, stdout, stderr)
-
-	// test that the magic values shuffle from upcoming -> current -> previous over time
-
-	magicCounter := binary.LittleEndian.Uint64(magicData[0:8])
-
-	var upcomingMagic [8]byte
-	var currentMagic [8]byte
-	var previousMagic [8]byte
-
-	copy(upcomingMagic[:], magicData[8:16])
-	copy(currentMagic[:], magicData[16:24])
-	copy(previousMagic[:], magicData[24:32])
-
-	magicUpdates := 0
-
-	for i := 0; i < 30; i++ {
-
-		response, err = http.Get("http://127.0.0.1:40000/magic")
-		if err != nil || response.StatusCode != 200 {
-			fmt.Printf("error: magic endpoint failed\n")
-			cmd.Process.Signal(syscall.SIGTERM)
-			os.Exit(1)
-		}
-
-		magicData, error := ioutil.ReadAll(response.Body)
-		if error != nil {
-			fmt.Printf("error: failed to read magic response data\n")
-			cmd.Process.Signal(syscall.SIGTERM)
-			os.Exit(1)
-		}
-
-		if len(magicData) != 32 {
-			fmt.Printf("error: magic data should be 32 bytes long\n")
-			cmd.Process.Signal(syscall.SIGTERM)
-			os.Exit(1)
-		}
-
-		newMagicCounter := binary.LittleEndian.Uint64(magicData[0:8])
-		if newMagicCounter < magicCounter {
-			fmt.Printf("error: magic counter must not decrease\n")
-			cmd.Process.Signal(syscall.SIGTERM)
-			os.Exit(1)
-		}
-
-		if newMagicCounter != magicCounter {
-
-			magicCounter = newMagicCounter
-			magicUpdates++
-
-			if bytes.Compare(magicData[16:24], upcomingMagic[:]) != 0 {
-				fmt.Printf("error: did not see upcoming magic shuffle to current magic\n")
-				cmd.Process.Signal(syscall.SIGTERM)
-				os.Exit(1)
-			}
-
-			if bytes.Compare(magicData[24:32], currentMagic[:]) != 0 {
-				fmt.Printf("error: did not see current magic shuffle to previous magic\n")
-				cmd.Process.Signal(syscall.SIGTERM)
-				os.Exit(1)
-			}
-
-			copy(upcomingMagic[:], magicData[8:16])
-			copy(currentMagic[:], magicData[16:24])
-			copy(previousMagic[:], magicData[24:32])
-
-		}
-
-		time.Sleep(time.Second)
-
-	}
-
-	// we should see 5,6 or 7 magic updates (30 seconds with updates once every 5 seconds...)
-
-	if magicUpdates != 5 && magicUpdates != 6 && magicUpdates != 7 {
-		fmt.Printf("error: did not see magic values update every ~5 seconds (%d magic updates)", magicUpdates)
-		cmd.Process.Signal(syscall.SIGTERM)
-		os.Exit(1)
-	}
-*/
+	return cmd, &output
+}
 
 func test_relays() {
 
 	fmt.Printf("test_relays\n")
 
-	// ...
+	api_cmd, api_stdout := api()
+
+	time.Sleep(time.Second * 30)
+	
+	api_cmd.Process.Signal(os.Interrupt)
+	api_cmd.Wait()
+
+	fmt.Printf("stdout: %s\n", api_stdout)
 }
 
 type test_function func()
