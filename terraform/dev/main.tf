4fb66376dd8533aa017fd5f4ba5c57b9b4068638
Glenn Fiedler <glenn@networknext.com>
2023-03-03 22:02:33 -0500

    relay gateway is hacked up in terraform

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -8,16 +8,7 @@ variable "zone" { type = string }
 variable "service_account" { type = string }
 variable "dev_artifacts_bucket" { type = string }
 variable "machine_type" { type = string }
-
-# ----------------------------------------------------------------------------------------
-
-resource "random_string" "run_id" {
-  length  = 8
-  lower   = true
-  upper   = false
-  numeric = false
-  special = false
-}
+variable "git_hash" { type = string }
 
 # ----------------------------------------------------------------------------------------
 
@@ -86,6 +77,14 @@ resource "google_compute_firewall" "development" {
 
 # ----------------------------------------------------------------------------------------
 
+resource "google_compute_address" "magic-backend" {
+  name         = "magic-backend"
+  subnetwork   = google_compute_subnetwork.development.id
+  address_type = "INTERNAL"
+  region       = "us-central1"
+  purpose      = "SHARED_LOADBALANCER_VIP"
+}
+
 resource "google_compute_subnetwork" "magic-backend" {
   name          = "magic-backend"
   project       = var.project
@@ -102,6 +101,7 @@ resource "google_compute_forwarding_rule" "magic-backend" {
   region                = var.region
   depends_on            = [google_compute_subnetwork.magic-backend]
   ip_protocol           = "TCP"
+  ip_address            = google_compute_address.magic-backend.id
   load_balancing_scheme = "INTERNAL_MANAGED"
   port_range            = "80"
   target                = google_compute_region_target_http_proxy.magic-backend.id
@@ -140,7 +140,7 @@ resource "google_compute_region_backend_service" "magic-backend" {
 }
 
 resource "google_compute_instance_template" "magic-backend" {
-  name         = "magic-backend-${random_string.run_id.result}"
+  name         = "magic-backend-${var.git_hash}"
   project      = var.project
   machine_type = var.machine_type
   tags         = ["http-server"]
@@ -265,6 +265,240 @@ output "redis_host" {
 
 # ----------------------------------------------------------------------------------------
 
+resource "google_compute_global_address" "relay-gateway" {
+  name = "relay-gateway"
+}
+
+resource "google_compute_subnetwork" "relay-gateway" {
+  name          = "relay-gateway"
+  ip_cidr_range = "10.0.3.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.development.id
+}
+
+resource "google_compute_global_forwarding_rule" "relay-gateway" {
+  name                  = "relay-gateway"
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL"
+  port_range            = "80"
+  target                = google_compute_target_http_proxy.relay-gateway.id
+  ip_address            = google_compute_global_address.relay-gateway.id
+}
+
+resource "google_compute_target_http_proxy" "relay-gateway" {
+  name     = "relay-gateway"
+  url_map  = google_compute_url_map.relay-gateway.id
+}
+
+resource "google_compute_url_map" "relay-gateway" {
+  name            = "relay-gateway"
+  default_service = google_compute_backend_service.relay-gateway.id
+}
+
+resource "google_compute_backend_service" "relay-gateway" {
+  name                    = "relay-gateway"
+  protocol                = "HTTP"
+  port_name               = "http"
+  load_balancing_scheme   = "EXTERNAL"
+  timeout_sec             = 10
+  health_checks           = [google_compute_health_check.relay-gateway-lb.id]
+  backend {
+    group           = google_compute_instance_group_manager.relay-gateway.instance_group
+    balancing_mode  = "UTILIZATION"
+    capacity_scaler = 1.0
+  }
+}
+
+resource "google_compute_instance_template" "relay-gateway" {
+  name         = "relay-gateway-${var.git_hash}"
+  machine_type = var.machine_type
+  tags         = ["allow-health-check", "http-server"]
+
+  network_interface {
+    network    = google_compute_network.development.id
+    subnetwork = google_compute_subnetwork.development.id
+  }
+
+  disk {
+    source_image = "ubuntu-os-cloud/ubuntu-minimal-2204-lts"
+    auto_delete  = true
+    boot         = true
+  }
+
+  metadata = {
+    startup-script = <<-EOF1
+      #!/bin/bash
+      gsutil cp ${var.dev_artifacts_bucket}/bootstrap.sh bootstrap.sh
+      chmod +x bootstrap.sh
+      sudo ./bootstrap.sh -b ${var.dev_artifacts_bucket} -a relay_gateway.tar.gz
+      cat <<EOF > /app/app.env
+      ENV=dev
+      DEBUG_LOGS=1
+      GOOGLE_PROJECT_ID=${var.project}
+      REDIS_HOSTNAME="${google_redis_instance.redis.host}:6379"
+      MAGIC_URL="http://${google_compute_address.magic-backend.address}/magic"
+      DATABASE_URL="${var.dev_artifacts_bucket}/database.bin"
+      DATABASE_PATH="/app/database.bin"
+      RELAY_BACKEND_PUBLIC_KEY=SS55dEl9nTSnVVDrqwPeqRv/YcYOZZLXCWTpNBIyX0Y=
+      RELAY_BACKEND_PRIVATE_KEY=ls5XiwAZRCfyuZAbQ1b9T1bh2VZY8vQ7hp8SdSTSR7M=
+      EOF
+      sudo gsutil cp ${var.dev_artifacts_bucket}/database.bin /app/database.bin
+      sudo systemctl start app.service
+    EOF1
+  }
+
+  service_account {
+    email  = var.service_account
+    scopes = ["cloud-platform"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_compute_health_check" "relay-gateway-lb" {
+  name     = "relay-gateway-lb"
+  check_interval_sec  = 5
+  timeout_sec         = 5
+  healthy_threshold   = 2
+  unhealthy_threshold = 10
+  http_health_check {
+    request_path = "/lb_health"
+    port         = "80"
+  }
+}
+
+resource "google_compute_health_check" "relay-gateway-vm" {
+  name                = "relay-gateway-vm"
+  check_interval_sec  = 5
+  timeout_sec         = 5
+  healthy_threshold   = 2
+  unhealthy_threshold = 10
+  http_health_check {
+    request_path = "/vm_health"
+    port         = "80"
+  }
+}
+
+resource "google_compute_instance_group_manager" "relay-gateway" {
+  name     = "relay-gateway"
+  zone     = "us-central1-a"
+  named_port {
+    name = "http"
+    port = 80
+  }
+  version {
+    instance_template = google_compute_instance_template.relay-gateway.id
+    name              = "primary"
+  }
+  base_instance_name = "relay-gateway"
+  target_size        = 2
+  auto_healing_policies {
+    health_check      = google_compute_health_check.relay-gateway-vm.id
+    initial_delay_sec = 120
+  }
+  update_policy {
+    type                           = "PROACTIVE"
+    minimal_action                 = "REPLACE"
+    most_disruptive_allowed_action = "REPLACE"
+    max_surge_fixed                = 10
+    max_unavailable_fixed          = 0
+    replacement_method             = "SUBSTITUTE"
+  }
+}
+
+resource "google_compute_firewall" "relay-gateway" {
+  name          = "relay-gateway"
+  direction     = "INGRESS"
+  network       = google_compute_network.development.id
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
+  allow {
+    protocol = "tcp"
+  }
+  target_tags = ["allow-health-check"]
+}
+
+output "relay-gateway-address" {
+  description = "The IP address of the relay frontend load balancer"
+  value = google_compute_global_address.relay-gateway.address
+}
+
+# ----------------------------------------------------------------------------------------
+
+/*
+resource "google_sql_database_instance" "postgres" {
+  name = "postgres"
+  database_version = "POSTGRES_14"
+  region = "${var.region}"
+  settings {
+    tier = "db-f1-micro"
+  }
+  deletion_protection = false
+}
+
+resource "google_sql_database" "database" {
+  name      = "database"
+  instance  = "${google_sql_database_instance.postgres.name}"
+}
+
+resource "google_sql_user" "users" {
+  name     = "developer"
+  password = "developer"
+  instance = "${google_sql_database_instance.postgres.name}"
+}
+
+output "postgres_host" {
+  description = "The IP address of the postgres instance"
+  value = "${google_sql_database_instance.postgres.ip_address.0.ip_address}"
+}
+*/
+
+# ----------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 module "gce-lb-http" {
   source  = "GoogleCloudPlatform/lb-http/google"
@@ -382,37 +616,6 @@ module "gce-lb-http" {
 
 
 
-
-// ---------------------------------------------------------------------------------------
-
-/*
-resource "google_sql_database_instance" "postgres" {
-  name = "postgres"
-  database_version = "POSTGRES_14"
-  region = "${var.region}"
-  settings {
-    tier = "db-f1-micro"
-    ip_configuration {
-      private_network = google_compute_network.development.id
-    }
-  }
-}
-
-resource "google_sql_database" "database" {
-  name      = "database"
-  instance  = "${google_sql_database_instance.postgres.name}"
-}
-
-resource "google_sql_user" "users" {
-  name     = "developer"
-  password = "developer"
-  instance = "${google_sql_database_instance.postgres.name}"
-}
-
-output "postgres_host" {
-  description = "The IP address of the postgres instance"
-  value = "${google_sql_database_instance.postgres.ip_address.0.ip_address}"
-}
 
 # ----------------------------------------------------------------------------------------
 
