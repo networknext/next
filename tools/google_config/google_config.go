7e579648b3539052a44ac242ef0e1c9be7717f97
Glenn Fiedler <glenn@networknext.com>
2023-03-15 19:12:53 -0400

    OK i have the logic down for google zones -> datacenter name

--- tools/google_config/google_config.go
+++ tools/google_config/google_config.go
@@ -8,6 +8,7 @@ import (
 	"regexp"
 	"strings"
 	"encoding/gob"
+	"sort"
 
 	// "encoding/json"
 	// 
@@ -18,74 +19,12 @@ import (
 
 // ===========================================================================================================================================
 
-/*
-// This definition drives the set of amazon datacenters, eg. "amazon.[country/city].[number]"
+// This definition drives the set of google datacenters, eg. "google.[country/city].[number]"
 
 var datacenterMap = map[string]*Datacenter{
 
-	// regions (AZID)
+	"us-east1":  {"southcarolina", 33.8361, -81.1637},
 
-	"afs1":  {"johannesburg", -33.9249, 18.4241},
-	"ape1":  {"hongkong", 22.3193, 114.1694},
-	"apne1": {"tokyo", 35.6762, 139.6503},
-	"apne2": {"seoul", 37.5665, 126.9780},
-	"apne3": {"osaka", 34.6937, 135.5023},
-	"aps1":  {"mumbai", 19.0760, 72.8777},
-	"aps2":  {"hyderabad", 17.3850, 78.4867},
-	"apse1": {"singapore", 1.3521, 103.8198},
-	"apse2": {"sydney", -33.8688, 151.2093},
-	"apse3": {"jakarta", -6.2088, 106.8456},
-	"apse4": {"melbourne", -37.8136, 144.9631},
-	"cac1":  {"montreal", 45.5019, -73.5674},
-	"euc1":  {"frankfurt", 50.1109, 8.6821},
-	"euc2":  {"zurich", 47.3769, 8.5417},
-	"eun1":  {"stockholm", 59.3293, 18.0686},
-	"eus1":  {"milan", 45.4642, 9.1900},
-	"eus2":  {"spain", 41.5976, -0.9057},
-	"euw1":  {"ireland", 53.7798, -7.3055},
-	"euw2":  {"london", 51.5072, -0.1276},
-	"euw3":  {"paris", 48.8566, 2.3522},
-	"mec1":  {"uae", 23.4241, 53.8478},
-	"mes1":  {"bahrain", 26.0667, 50.5577},
-	"sae1":  {"saopaulo", -23.5558, -46.6396},
-	"use1":  {"virginia", 39.0438, -77.4874},
-	"use2":  {"ohio", 40.4173, -82.9071},
-	"usw1":  {"sanjose", 37.3387, -121.8853},
-	"usw2":  {"oregon", 45.8399, -119.7006},
-
-	// local zones (AZID)
-
-	"los1": {"nigeria", 6.5244, 3.3792},
-	"tpe1": {"taipei", 25.0330, 121.5654},
-	"ccu1": {"kolkata", 22.5726, 88.3639},
-	"del1": {"delhi", 28.7041, 77.1025},
-	"bkk1": {"bangkok", 13.7563, 100.5018},
-	"per1": {"perth", -31.9523, 115.8613},
-	"ham1": {"hamburg", 53.5488, 9.9872},
-	"waw1": {"warsaw", 52.2297, 21.0122},
-	"cph1": {"copenhagen", 55.6761, 12.5683},
-	"hel1": {"finland", 60.1699, 24.9384},
-	"mct1": {"oman", 23.5880, 58.3829},
-  "atl1": {"atlanta", 33.7488, -84.3877},
-  "bos1": {"boston", 42.3601, -71.0589},
-  "bue1": {"buenosaires", -34.6037, -58.3816}, 
-  "chi1": {"chicago", 41.8781, -87.6298},
-  "dfw1": {"dallas", 32.7767, -96.7970},
-  "iah1": {"houston", 29.7604, -95.3698},
-  "lim1": {"lima", -12.0464, -77.0428},
-  "mci1": {"kansas", 39.0997, -94.5786},
-  "mia1": {"miami", 25.7617, -80.1918},
-  "msp1": {"minneapolis", 44.9778, -93.2650},
-  "nyc1": {"newyork", 40.7128, -74.0060},
-  "phl1": {"philadelphia", 39.9526, -75.1652},
-  "qro1": {"mexico", 23.6345, -102.5528},
-  "scl1": {"santiago", -33.4489, -70.6693},
-  "den1": {"denver", 39.7392, -104.9903},
-  "las1": {"lasvegas", 36.1716, -115.1391},
-  "lax1": {"losangeles", 34.0522, -118.2437},
-  "pdx1": {"portland", 45.5152, -122.6784},
-	"phx1": {"phoenix", 33.4484, -112.0740},
-	"sea1": {"seattle", 47.6062, -122.3321},
 }
 
 type Datacenter struct {
@@ -93,7 +32,6 @@ type Datacenter struct {
 	latitude  float32
 	longitude float32
 }
-*/
 
 // ===========================================================================================================================================
 
@@ -110,8 +48,11 @@ func bash(command string) string {
 }
 
 type Zone struct {
-	Zone   string
-	Region string
+	Zone            string
+	Region          string
+	DatacenterName  string
+	Latitude        float32
+	Longitude       float32
 }
 
 func main() {
@@ -194,10 +135,10 @@ func main() {
 		lines := strings.Split(output, "\n")
 
 		for i := 1; i < len(lines); i++ {
-			re := regexp.MustCompile(`^([a-zA-Z0-9-]+)\w+([a-zA-Z0-9-]+)\w+`)
+			re := regexp.MustCompile(`^([a-zA-Z0-9-]+)\w+`)
 			match := re.FindStringSubmatch(lines[i])
-			if len(match) >= 2 {
-				zones = append(zones, &Zone{match[0], match[1]})
+			if len(match) >= 1 {
+				zones = append(zones, &Zone{match[0], "", "", 0, 0})
 			}
 		}
 
@@ -212,10 +153,62 @@ func main() {
 
 		fmt.Printf("\nZones:\n\n")
 		for i := range zones {
-			fmt.Printf("  %s | %s\n", zones[i].Zone, zones[i].Region)
+			fmt.Printf("  %s\n", zones[i].Zone)
 		}
 	}
 
+	// unique the zones (not sure why I need to do this...) then sort by zone name
+
+	zoneMap := make(map[string]*Zone)
+	
+	for i := range zones {
+		zoneMap[zones[i].Zone] = zones[i]
+	}
+
+	index := 0
+	zones = make([]*Zone, len(zoneMap))
+	for _,v := range zoneMap {
+		zones[index] = v
+		index++
+	}
+
+	sort.SliceStable(zones, func(i, j int) bool { return zones[i].Zone < zones[j].Zone })
+
+	// print out the known datacenters
+
+	fmt.Printf("\nKnown datacenters:\n\n")
+
+	unknown := make([]*Zone, 0)
+
+	datacenterToRegion := make(map[string]string)
+
+	for i := range zones {
+		values := strings.Split(zones[i].Zone, "-")
+		a := values[0]
+		b := values[1]
+		c := values[2]
+		region := fmt.Sprintf("%s-%s", a, b)
+		datacenter := datacenterMap[region]
+		number := c[0] - 'a' + 1
+		if datacenter != nil {
+			zones[i].DatacenterName = fmt.Sprintf("google.%s.%d", datacenter.name, number)
+			zones[i].Latitude = datacenter.latitude
+			zones[i].Longitude = datacenter.longitude
+			fmt.Printf("  %s\n", zones[i].DatacenterName)
+			datacenterToRegion[zones[i].DatacenterName] = region
+		} else {
+			unknown = append(unknown, zones[i])
+		}
+	}
+
+	// print out the unknown datacenters
+
+	if len(unknown) > 0 {
+		fmt.Printf("\nUnknown datacenters:\n\n")
+		// for i := range unknown {
+			// fmt.Printf("  %s\n", unknown[i].Zone)
+		// }
+	}
 
 
 
@@ -232,117 +225,6 @@ func main() {
 
 
 /*
-	// load zones cache, if possible
-
-	zones := make([]Zone, 0)
-
-	loadedZonesCache := false
-
-	{
-	 	file, err := os.Open("cache/amazon_zones.bin")
-	 	if err == nil {
-			gob.NewDecoder(file).Decode(&zones)
-		 	if err == nil {
-		 		loadedZonesCache = true
-		 	}
-		 	file.Close()
-	 	}
-	}
-
-	// otherwise, iterate across each region and get zones then save to cache
-
-	if !loadedZonesCache {
-
-		for i := range regionsResponse.Regions {
-
-			fmt.Printf("\n%s zones:\n\n", regionsResponse.Regions[i].RegionName)
-
-			output := bash(fmt.Sprintf("aws ec2 describe-availability-zones --region=%s --all-availability-zones", regionsResponse.Regions[i].RegionName))
-
-			availabilityZonesResponse := AvailabilityZonesResponse{}
-			if err := json.Unmarshal([]byte(output), &availabilityZonesResponse); err != nil {
-				panic(err)
-			}
-
-			for j := range availabilityZonesResponse.AvailabilityZones {
-				zoneType := availabilityZonesResponse.AvailabilityZones[j].ZoneType
-				if zoneType != "availability-zone" && zoneType != "local-zone" {
-					continue
-				}
-				local := zoneType == "local-zone"
-				zone := availabilityZonesResponse.AvailabilityZones[j].ZoneName
-				azid := availabilityZonesResponse.AvailabilityZones[j].ZoneId
-				region := availabilityZonesResponse.AvailabilityZones[j].RegionName
-				if local {
-					fmt.Printf("  %s [%s] (local)\n", zone, azid)
-				} else {
-					fmt.Printf("  %s [%s]\n", zone, azid)
-				}
-				zones = append(zones, Zone{zone, azid, region, local, "", 0, 0})
-			}
-
-		}
-
-		// sort by azid then cache results
-
-		sort.SliceStable(zones, func(i, j int) bool { return zones[i].AZID < zones[j].AZID })
-
-		{
-			file, err := os.Create("cache/amazon_zones.bin")
-		 	if err != nil {
-		 		panic(err)
-		 	}
-			gob.NewEncoder(file).Encode(zones)
-		 	file.Close()
-		}
-	}
-
-	// print all zones
-
-	fmt.Printf("\nAll zones:\n\n")
-
-	for i := range zones {
-		values := strings.Split(zones[i].AZID, "-")
-		a := values[len(values)-2]
-		b := values[len(values)-1]
-		fmt.Printf("  %s|%s\n", a, b)
-	}
-
-	// print out the known datacenters
-
-	fmt.Printf("\nKnown datacenters:\n\n")
-
-	unknown := make([]*Zone, 0)
-
-	datacenterToRegion := make(map[string]string)
-	datacenterIsLocal := make(map[string]bool)
-
-	for i := range zones {
-		values := strings.Split(zones[i].AZID, "-")
-		a := values[len(values)-2]
-		b := values[len(values)-1]
-		datacenter := datacenterMap[a]
-		number, _ := strconv.Atoi(b[2:])
-		if datacenter != nil {
-			zones[i].DatacenterName = fmt.Sprintf("amazon.%s.%d", datacenter.name, number)
-			zones[i].Latitude = datacenter.latitude
-			zones[i].Longitude = datacenter.longitude
-			fmt.Printf("  %s\n", zones[i].DatacenterName)
-			datacenterToRegion[zones[i].DatacenterName] = zones[i].Region
-			datacenterIsLocal[zones[i].DatacenterName] = zones[i].Local
-		} else {
-			unknown = append(unknown, &zones[i])
-		}
-	}
-
-	// print out the unknown datacenters
-
-	if len(unknown) > 0 {
-		fmt.Printf("\nUnknown datacenters:\n\n")
-		for i := range unknown {
-			fmt.Printf("  %s -> %s\n", unknown[i].Zone, unknown[i].AZID)
-		}
-	}
 
 	// generate amazon.txt
 
