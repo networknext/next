50c08be01a3266b11db519196c2cc54cd4a5611a
Glenn Fiedler <glenn@networknext.com>
2023-06-25 19:29:06 -0400

    add continue response wrong size

--- cmd/func_test_relay/func_test_relay.go
+++ cmd/func_test_relay/func_test_relay.go
@@ -2960,6 +2960,71 @@ func test_continue_request_packet_forward_to_next_hop_internal_address() {
 	}
 }
 
+func test_continue_response_packet_wrong_size() {
+
+	fmt.Printf("test_continue_response_packet_wrong_size\n")
+
+	backend_cmd, _ := backend("ZERO_MAGIC")
+
+	time.Sleep(time.Second)
+
+	config := RelayConfig{}
+	config.num_threads = 4
+	config.print_counters = true
+
+	relay_cmd, relay_stdout := relay("relay", 2000, config)
+
+	time.Sleep(5 * time.Second)
+
+	lc := net.ListenConfig{}
+
+	lp, err := lc.ListenPacket(context.Background(), "udp", "127.0.0.1:0")
+	if err != nil {
+		panic("could not bind socket")
+	}
+
+	conn := lp.(*net.UDPConn)
+
+	clientPort := conn.LocalAddr().(*net.UDPAddr).Port
+
+	clientAddress := core.ParseAddress(fmt.Sprintf("127.0.0.1:%d", clientPort))
+
+	serverAddress := core.ParseAddress("127.0.0.1:2000")
+
+ 	for i := 0; i < 10; i++ {
+		for j := 0; j < 1000; j++ {
+			packet := make([]byte, common.RandomInt(18,1500))
+			common.RandomBytes(packet[:])
+			packet[0] = 16 // CONTINUE_RESPONSE_PACKET
+			var magic [constants.MagicBytes]byte
+			var fromAddressBuffer [32]byte
+			var toAddressBuffer [32]byte
+			fromAddress, fromPort := core.GetAddressData(&clientAddress, fromAddressBuffer[:])
+			toAddress, toPort := core.GetAddressData(&serverAddress, toAddressBuffer[:])
+			packetLength := len(packet)
+			core.GenerateChonkle(packet[1:], magic[:], fromAddress[:], fromPort, toAddress[:], toPort, packetLength)
+			core.GeneratePittle(packet[packetLength-2:], fromAddress[:], fromPort, toAddress[:], toPort, packetLength)
+			conn.WriteToUDP(packet, &serverAddress)
+		}
+		time.Sleep(time.Second)
+	}
+
+	conn.Close()
+
+	backend_cmd.Process.Signal(os.Interrupt)
+	relay_cmd.Process.Signal(os.Interrupt)
+
+	backend_cmd.Wait()
+	relay_cmd.Wait()
+
+	if !strings.Contains(relay_stdout.String(), "Relay initialized") {
+		panic("could not initialize relay")
+	}
+
+	checkCounter("RELAY_COUNTER_CONTINUE_RESPONSE_PACKET_RECEIVED", relay_stdout.String())
+	checkCounter("RELAY_COUNTER_CONTINUE_RESPONSE_PACKET_WRONG_SIZE", relay_stdout.String())
+}
+
 // fmt.Printf("=======================================\n%s=============================================\n", relay_stdout)
 
 type test_function func()
@@ -3015,10 +3080,11 @@ func main() {
 		test_continue_request_packet_wrong_size,
 		test_continue_request_packet_could_not_read_token,
 		test_continue_request_packet_token_expired,
-		
 		test_continue_request_packet_could_not_find_session,
 		test_continue_request_packet_forward_to_next_hop_public_address,
 		test_continue_request_packet_forward_to_next_hop_internal_address,
+
+		test_continue_response_packet_wrong_size,
 	}
 
 	var tests []test_function
