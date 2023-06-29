950dd01697f0d7e12be8bcf352f81546153c3681
Glenn Fiedler <glenn@networknext.com>
2023-06-29 11:45:22 -0400

    another fix

--- cmd/func_backend/func_backend.go
+++ cmd/func_backend/func_backend.go
@@ -362,6 +362,8 @@ func RelayUpdateHandler(writer http.ResponseWriter, request *http.Request) {
 	copy(responsePacket.ExpectedRelayBackendPublicKey[:], TestRelayBackendPublicKey)
 
 	token := core.RouteToken{}
+	token.NextAddress = net.UDPAddr{IP: net.IPv4(0,0,0,0), Port: 10000}
+	token.PrevAddress = net.UDPAddr{IP: net.IPv4(0,0,0,0), Port: 20000}
 	core.WriteEncryptedRouteToken(&token, responsePacket.TestToken[:], TestRelayBackendPrivateKey, TestRelayPublicKey)
 
 	// send the response packet
