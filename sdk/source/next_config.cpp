b20708131cbc09d8e7ae69a76189b8684d055caa
Glenn Fiedler <glenn@networknext.com>
2023-08-04 09:37:38 -0400

    add missing cpp file

---
+++ sdk/source/next_config.cpp
@@ -0,0 +1,39 @@
+/*
+    Network Next Accelerate. Copyright © 2017 - 2023 Network Next, Inc.
+
+    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following 
+    conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions 
+       and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote 
+       products derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+    IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+    OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "next_config.h"
+
+uint8_t next_server_backend_public_key[] =
+{
+     76,  97, 202, 140,  71, 135,  62, 212,
+    160, 181, 151, 195, 202, 224, 207, 113,
+      8,  45,  37,  60, 145,  14, 212, 111,
+     25,  34, 175, 186,  37, 150, 163,  64
+};
+
+uint8_t next_relay_backend_public_key[] =
+{
+    0x49, 0x2e, 0x79, 0x74, 0x49, 0x7d, 0x9d, 0x34,
+    0xa7, 0x55, 0x50, 0xeb, 0xab, 0x03, 0xde, 0xa9,
+    0x1b, 0xff, 0x61, 0xc6, 0x0e, 0x65, 0x92, 0xd7,
+    0x09, 0x64, 0xe9, 0x34, 0x12, 0x32, 0x5f, 0x46
+};
