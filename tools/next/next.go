4113c759ebd2013df03e645add65ab04cfa828cc
Glenn Fiedler <glenn@networknext.com>
2023-08-14 15:54:32 -0400

    run equivalent of setup_relay.sh if the relay hasn't had it done yet, when you go 'next setup [relay]'

--- tools/next/next.go
+++ tools/next/next.go
@@ -1127,6 +1127,31 @@ const (
 
 if [[ -f /etc/next_setup_completed ]]; then echo "already setup" && exit 0; fi
 
+# if the setup_relay.sh has not run yet then do it here
+
+if [[ ! -f /etc/setup_relay_has_run ]]; 
+then
+	echo sshd: ALL > hosts.deny
+	echo sshd: $VPN_ADDRESS > hosts.allow
+	sudo mv hosts.deny /etc/hosts.deny
+	sudo mv hosts.allow /etc/hosts.allow
+	sudo journalctl --vacuum-size 10M
+	sudo NEEDRESTART_SUSPEND=1 apt autoremove -y
+	sudo NEEDRESTART_SUSPEND=1 apt update -y
+	sudo NEEDRESTART_SUSPEND=1 apt upgrade -y
+	sudo NEEDRESTART_SUSPEND=1 apt dist-upgrade -y
+	sudo NEEDRESTART_SUSPEND=1 apt install libcurl3-gnutls build-essential unattended-upgrades -y
+	sudo NEEDRESTART_SUSPEND=1 apt autoremove -y
+	wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.18.tar.gz
+	tar -zxf libsodium-1.0.18.tar.gz
+	cd libsodium-1.0.18
+	./configure
+	make -j
+	sudo make install
+	sudo ldconfig
+	sudo touch /etc/setup_relay_has_run
+fi
+
 # make the relay prompt cool
 
 echo making the relay prompt cool
