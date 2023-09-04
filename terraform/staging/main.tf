85839dd3c9545a1ac701180a47d0991f63a4e915
Glenn Fiedler <glenn@networknext.com>
2023-09-04 19:56:34 -0400

    beef up redis

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -226,56 +226,66 @@ resource "google_redis_instance" "redis_portal" {
   redis_version           = "REDIS_6_X"
   redis_configs           = { "activedefrag" = "yes", "maxmemory-policy" = "allkeys-lru" }
   authorized_network      = google_compute_network.staging.id
-  replica_count           = 2
+  replica_count           = 5
   read_replicas_mode      = "READ_REPLICAS_ENABLED"
 }
 
 resource "google_redis_instance" "redis_map_cruncher" {
   name                    = "redis-map-cruncher"
   tier                    = "STANDARD_HA"
-  memory_size_gb          = 1
+  memory_size_gb          = 5
   region                  = "us-central1"
   redis_version           = "REDIS_6_X"
   redis_configs           = { "activedefrag" = "yes", "maxmemory-policy" = "allkeys-lru" }
   authorized_network      = google_compute_network.staging.id
+  replica_count           = 5
+  read_replicas_mode      = "READ_REPLICAS_ENABLED"
 }
 
 resource "google_redis_instance" "redis_raspberry" {
   name                    = "redis-raspberry"
   tier                    = "STANDARD_HA"
-  memory_size_gb          = 1
+  memory_size_gb          = 5
   region                  = "us-central1"
   redis_version           = "REDIS_6_X"
   redis_configs           = { "activedefrag" = "yes", "maxmemory-policy" = "allkeys-lru" }
   authorized_network      = google_compute_network.staging.id
+  replica_count           = 5
+  read_replicas_mode      = "READ_REPLICAS_ENABLED"
 }
 
 resource "google_redis_instance" "redis_analytics" {
   name                    = "redis-analytics"
   tier                    = "STANDARD_HA"
-  memory_size_gb          = 1
+  memory_size_gb          = 5
   region                  = "us-central1"
   redis_version           = "REDIS_6_X"
   redis_configs           = { "activedefrag" = "yes", "maxmemory-policy" = "allkeys-lru" }
   authorized_network      = google_compute_network.staging.id
+  replica_count           = 5
+  read_replicas_mode      = "READ_REPLICAS_ENABLED"
 }
 
 resource "google_redis_instance" "redis_relay_backend" {
   name                    = "redis-relay-backend"
   tier                    = "STANDARD_HA"
-  memory_size_gb          = 1
+  memory_size_gb          = 5
   region                  = "us-central1"
   redis_version           = "REDIS_6_X"
   authorized_network      = google_compute_network.staging.id
+  replica_count           = 5
+  read_replicas_mode      = "READ_REPLICAS_ENABLED"
 }
 
 resource "google_redis_instance" "redis_server_backend" {
   name                    = "redis-server-backend"
   tier                    = "STANDARD_HA"
-  memory_size_gb          = 1
+  memory_size_gb          = 5
   region                  = "us-central1"
   redis_version           = "REDIS_6_X"
   authorized_network      = google_compute_network.staging.id
+  replica_count           = 5
+  read_replicas_mode      = "READ_REPLICAS_ENABLED"
 }
 
 output "redis_portal_address" {
