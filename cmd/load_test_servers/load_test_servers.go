1e8855d6ec02610c79e561350dad1fa3881b5acf
Glenn Fiedler <glenn@networknext.com>
2023-09-10 18:50:50 -0400

    add load test servers service for staging

---
+++ cmd/load_test_servers/load_test_servers.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/networknext/next/modules/common"
+	"github.com/networknext/next/modules/core"
+	"github.com/networknext/next/modules/envvar"
+)
+
+var magicUpdateSeconds int
+
+func main() {
+
+	service := common.CreateService("load_test_servers")
+
+	numServers := envvar.GetInt("NUM_SERVERS", 10000)
+
+	core.Log("simulating %d servers", numServers)
+
+	go SimulateServers(numServers)
+
+	service.WaitForShutdown()
+}
+
+func SimulateServers(numServers int) {
+	for i := 0; i < numServers; i++ {
+		// ...
+	}
+}
+
+func RunServer() {
+	for {
+		// ...
+	}
+}
