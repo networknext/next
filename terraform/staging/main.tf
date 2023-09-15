b9d94c37a75028af1c1a98636eee153d9f78ccc8
Glenn Fiedler <glenn@networknext.com>
2023-09-15 15:58:08 -0400

    load test relays set to 1 instance

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -1853,7 +1853,7 @@ module "load_test_relays" {
   default_subnetwork = google_compute_subnetwork.staging.id
   service_account    = var.google_service_account
   tags               = ["allow-ssh", "allow-udp-all"]
-  target_size        = 0
+  target_size        = 1
 }
 
 # ----------------------------------------------------------------------------------------
