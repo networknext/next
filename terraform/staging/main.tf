b749f4d743413ed76fd0970c3429c3bad4b6621f
Glenn Fiedler <glenn@networknext.com>
2023-09-15 20:00:19 -0400

    reduce to n2-standard-2 for portal cruncher. it uses very little cpu

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -1696,7 +1696,7 @@ module "portal_cruncher" {
 
   tag                = var.tag
   extra              = var.extra
-  machine_type       = var.google_machine_type
+  machine_type       = "n1-standard-2"
   project            = var.google_project
   region             = var.google_region
   default_network    = google_compute_network.staging.id
