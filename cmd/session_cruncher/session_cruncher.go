902dbc197e118b02fba1e231a2cd0e2a21dca949
Glenn Fiedler <glenn@networknext.com>
2023-09-20 15:50:51 -0400

    sketching out session cruncher

--- cmd/session_cruncher/session_cruncher.go
+++ cmd/session_cruncher/session_cruncher.go
@@ -3,12 +3,21 @@ package main
 import (
 	"time"
 	"net/http"
+	"io/ioutil"
+	"encoding/binary"
 
 	"github.com/networknext/next/modules/common"
 	"github.com/networknext/next/modules/core"
 
 	_ "github.com/wangjia184/sortedset"
 )
+
+type Session struct {
+	sessionId	uint64
+	score       int32
+}
+
+var sessionChannel chan Session
 
 func main() {
 
@@ -18,9 +27,23 @@ func main() {
 	service.Router.HandleFunc("/session_counts", sessionCountsHandler).Methods("GET")
 	service.Router.HandleFunc("/sessions/{begin}/{end}", sessionsHandler).Methods("GET")
 
+	go ProcessThread()
+
+	go SortThread()
+
 	service.StartWebServer()
 
 	service.WaitForShutdown()
+}
+
+func ProcessThread() {
+	for {
+		select {
+		case session := <-sessionChannel:
+			core.Log("session %016x (%d)", session.sessionId, session.score)
+			// todo
+		}
+	}
 }
 
 func SortThread() {
@@ -35,6 +58,25 @@ func SortThread() {
 
 func sessionBatchHandler(w http.ResponseWriter, r *http.Request) {
 	core.Log("session batch handler")
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		core.Error("could not read body")
+		return
+	}
+	defer r.Body.Close()
+	if len(body) % 12 != 0 {
+		core.Error("session batch should be multiple of 12 bytes")
+		return
+	}
+	numSessions := len(body) / 12
+	index := 0
+	for i := 0; i < numSessions; i++ {
+		session := Session{}
+		session.sessionId = binary.LittleEndian.Uint64(body[index:index+8])
+		session.score = int32(binary.LittleEndian.Uint32(body[index+8:index+12]))
+		index += 10
+		sessionChannel <- session
+    }
 }
 
 func sessionCountsHandler(w http.ResponseWriter, r *http.Request) {
