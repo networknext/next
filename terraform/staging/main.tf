275b87088af01ee7e859873af0090106f8098a7c
Glenn Fiedler <glenn@networknext.com>
2023-09-23 16:29:42 -0400

    increase session cruncher to 22 core c3

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -1707,7 +1707,7 @@ module "session_cruncher" {
 
   tag                        = var.tag
   extra                      = var.extra
-  machine_type               = "c3-highcpu-8"
+  machine_type               = "c3-highcpu-22"
   project                    = var.google_project
   region                     = var.google_region
   zones                      = var.google_zones
