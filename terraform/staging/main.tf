a42d0a122510df0c50a0d2133dabc5efd8335c40
Glenn Fiedler <glenn@networknext.com>
2023-09-25 16:27:21 -0400

    server backend needs to target low CPU

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -1846,7 +1846,7 @@ module "server_backend" {
   tags               = ["allow-ssh", "allow-health-checks", "allow-udp-40000"]
   min_size           = 3
   max_size           = 64
-  target_cpu         = 60
+  target_cpu         = 25
 
   depends_on = [google_pubsub_topic.pubsub_topic, google_pubsub_subscription.pubsub_subscription]
 }
