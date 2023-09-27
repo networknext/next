bbd59eb1500b949d00e6e88eef28fd010de6de58
Glenn Fiedler <glenn@networknext.com>
2023-09-27 15:33:48 -0400

    beef up api

--- terraform/dev/backend/main.tf
+++ terraform/dev/backend/main.tf
@@ -1672,7 +1672,7 @@ module "api" {
 
   tag                        = var.tag
   extra                      = var.extra
-  machine_type               = var.google_machine_type
+  machine_type               = "n1-highcpu-2"
   project                    = var.google_project
   region                     = var.google_region
   zones                      = var.google_zones
