79449978cb7702e6f2c657006a5a7e71c9e7cce6
Glenn Fiedler <glenn@networknext.com>
2023-10-06 15:45:29 -0400

    fix startup script for redis stack

--- terraform/modules/redis_stack/main.tf
+++ terraform/modules/redis_stack/main.tf
@@ -49,12 +49,11 @@ resource "google_compute_instance" "service" {
     set -euo pipefail
     export DEBIAN_FRONTEND=noninteractive
     apt-get update
-    apt-get install -y 
     curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
-    sudo chmod 644 /usr/share/keyrings/redis-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
-    sudo apt-get update
-    sudo apt-get install redis-stack-server
+    chmod 644 /usr/share/keyrings/redis-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/redis.list
+    apt-get update -y
+    apt-get install -y redis-stack-server
     EOF
   }
 
