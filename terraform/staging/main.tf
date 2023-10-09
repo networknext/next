da35ebe84ac27689b852ecb7e1bf772cd070b5bd
Glenn Fiedler <glenn@networknext.com>
2023-10-09 09:33:52 -0400

    set redis clusters

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -272,8 +272,8 @@ output "redis_time_series_address" {
 # ----------------------------------------------------------------------------------------
 
 locals {
-  redis_portal_address = "10.0.0.26:6379"
-  redis_server_backend_address = "10.0.0.31:6379"
+  redis_portal_address = "10.0.0.52:6379"
+  redis_server_backend_address = "10.0.0.66:6379"
 }
 
 resource "google_redis_instance" "redis_relay_backend" {
