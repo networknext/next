34ba3aa478b8c0b9402de0742f79ee0a0b473869
Glenn Fiedler <glenn@networknext.com>
2023-10-24 12:07:55 -0400

    no redis clusters in dev

--- terraform/dev/backend/main.tf
+++ terraform/dev/backend/main.tf
@@ -1569,7 +1569,7 @@ module "relay_backend" {
     ENABLE_GOOGLE_PUBSUB=true
     MAX_JITTER=10
     MAX_PACKET_LOSS=0.1
-    REDIS_PORTAL_CLUSTER="${local.redis_portal_address}"
+    REDIS_PORTAL_HOSTNAME="${google_redis_instance.redis_portal.host}:6379"
     EOF
     sudo gsutil cp ${var.google_database_bucket}/dev.bin /app/database.bin
     sudo systemctl start app.service
@@ -1748,7 +1748,6 @@ module "server_backend" {
     UDP_SOCKET_WRITE_BUFFER=104857600
     GOOGLE_PROJECT_ID=${var.google_project}
     MAGIC_URL="http://${module.magic_backend.address}/magic"
-    REDIS_CLUSTER="${local.redis_portal_address}"
     RELAY_BACKEND_PUBLIC_KEY=${var.relay_backend_public_key}
     RELAY_BACKEND_PRIVATE_KEY=${var.relay_backend_private_key}
     SERVER_BACKEND_ADDRESS="##########:40000"
@@ -1760,7 +1759,7 @@ module "server_backend" {
     ENABLE_GOOGLE_PUBSUB=true
     ENABLE_REDIS_TIME_SERIES=true
     REDIS_TIME_SERIES_HOSTNAME="${module.redis_time_series.address}:6379"
-    REDIS_PORTAL_CLUSTER="${local.redis_portal_address}"
+    REDIS_PORTAL_HOSTNAME="${google_redis_instance.redis_portal.host}:6379"
     REDIS_RELAY_BACKEND_HOSTNAME="${google_redis_instance.redis_relay_backend.host}:6379"
     SESSION_CRUNCHER_URL="http://${module.session_cruncher.address}"
     SERVER_CRUNCHER_URL="http://${module.server_cruncher.address}"
