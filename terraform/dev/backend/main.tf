0068483d47454f52a5717725e437b2d2b6cdf89c
Glenn Fiedler <glenn@networknext.com>
2023-10-28 20:35:47 -0400

    give permissions

--- terraform/dev/backend/main.tf
+++ terraform/dev/backend/main.tf
@@ -374,6 +374,21 @@ resource "google_pubsub_subscription" "pubsub_subscription" {
   }
 }
 
+data "google_project" "project" {
+}
+
+resource "google_project_iam_member" "viewer" {
+  project = data.google_project.project.project_id
+  role   = "roles/bigquery.metadataViewer"
+  member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "editor" {
+  project = data.google_project.project.project_id
+  role   = "roles/bigquery.dataEditor"
+  member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+}
+
 # ----------------------------------------------------------------------------------------
 
 locals {
