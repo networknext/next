41cd240c7770813b74deac6ad784b40b2e67ea6e
Glenn Fiedler <glenn@networknext.com>
2023-11-03 13:19:24 -0400

    updated staging so it works with secrets

--- terraform/dev/relays/terraform.tfvars
+++ terraform/dev/relays/terraform.tfvars
@@ -7,8 +7,6 @@ ssh_public_key_file      = "~/.ssh/id_rsa.pub"
 ssh_private_key_file     = "~/.ssh/id_rsa"
 relay_version            = "relay-debug-1.0.28"
 relay_artifacts_bucket   = "network_next_relay_artifacts"
-relay_public_key         = "S0Nr+Hh05vpCpggvUjTwqqC5FX+nuKN02q1K9aOiGQY="
-relay_private_key        = "ei8wBWrWnnJOoI3dyQgajOcwfk1axAoKg0L5Xp9UCZw="
 relay_backend_public_key = "SS55dEl9nTSnVVDrqwPeqRv/YcYOZZLXCWTpNBIyX0Y="
 relay_backend_hostname   = "https://relay-dev.virtualgo.net"
 
--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -22,20 +22,22 @@ variable "cloudflare_zone_id" { type = string }
 variable "cloudflare_domain" { type = string }
 
 variable "relay_backend_public_key" { type = string }
-variable "relay_backend_private_key" { type = string }
+
 variable "server_backend_public_key" { type = string }
-variable "server_backend_private_key" { type = string }
-variable "ping_key" { type = string }
-variable "api_private_key" { type = string }
-variable "buyer_public_key" { type = string }
-variable "buyer_private_key" { type = string }
+
+variable "load_test_buyer_public_key" { type = string }
+variable "load_test_buyer_private_key" { type = string }
 
 variable "ip2location_bucket_name" { type = string }
 
 locals {
-  google_project         = file("../projects/staging-project.txt")
-  google_service_account = file("../projects/staging-runtime-service-account.txt")
-  maxmind_license_key    = file("~/secrets/maxmind.txt")
+  google_project             = file("../projects/staging-project.txt")
+  google_service_account     = file("../projects/staging-runtime-service-account.txt")
+  maxmind_license_key        = file("~/secrets/maxmind.txt")
+  relay_backend_private_key  = file("~/secrets/staging-relay-backend-private-key.txt")
+  server_backend_private_key = file("~/secrets/staging-server-backend-private-key.txt")
+  api_private_key            = file("~/secrets/staging-api-private-key.txt")
+  ping_key                   = file("~/secrets/staging-ping-key.txt")
 }
 
 # ----------------------------------------------------------------------------------------
@@ -559,8 +561,8 @@ module "relay_gateway" {
     DATABASE_URL="${var.google_database_bucket}/staging.bin"
     DATABASE_PATH="/app/database.bin"
     RELAY_BACKEND_PUBLIC_KEY=${var.relay_backend_public_key}
-    RELAY_BACKEND_PRIVATE_KEY=${var.relay_backend_private_key}
-    PING_KEY=${var.ping_key}
+    RELAY_BACKEND_PRIVATE_KEY=${local.relay_backend_private_key}
+    PING_KEY=${local.ping_key}
     RELAY_BACKEND_ADDRESS=""
     EOF
     sudo gsutil cp ${var.google_database_bucket}/staging.bin /app/database.bin
@@ -677,7 +679,7 @@ module "api" {
     DATABASE_URL="${var.google_database_bucket}/staging.bin"
     DATABASE_PATH="/app/database.bin"
     PGSQL_CONFIG="host=${google_sql_database_instance.postgres.ip_address.0.ip_address} port=5432 user=developer password=developer dbname=database sslmode=disable"
-    API_PRIVATE_KEY=${var.api_private_key}
+    API_PRIVATE_KEY=${local.api_private_key}
     ALLOWED_ORIGIN="*"
     EOF
     sudo gsutil cp ${var.google_database_bucket}/staging.bin /app/database.bin
@@ -809,12 +811,12 @@ module "server_backend" {
     MAGIC_URL="http://${module.magic_backend.address}/magic"
     REDIS_CLUSTER="${local.redis_portal_address}"
     RELAY_BACKEND_PUBLIC_KEY=${var.relay_backend_public_key}
-    RELAY_BACKEND_PRIVATE_KEY=${var.relay_backend_private_key}
+    RELAY_BACKEND_PRIVATE_KEY=${local.relay_backend_private_key}
     SERVER_BACKEND_ADDRESS="##########:40000"
     SERVER_BACKEND_PUBLIC_KEY=${var.server_backend_public_key}
-    SERVER_BACKEND_PRIVATE_KEY=${var.server_backend_private_key}
+    SERVER_BACKEND_PRIVATE_KEY=${local.server_backend_private_key}
     ROUTE_MATRIX_URL="http://${module.relay_backend.address}/route_matrix"
-    PING_KEY=${var.ping_key}
+    PING_KEY=${local.ping_key}
     IP2LOCATION_BUCKET_NAME=${var.ip2location_bucket_name}
     ENABLE_GOOGLE_PUBSUB=true
     ENABLE_REDIS_TIME_SERIES=true
@@ -949,7 +951,7 @@ module "load_test_servers" {
     NUM_RELAYS=1000
     NUM_SERVERS=50000
     SERVER_BACKEND_ADDRESS=${module.server_backend.address}:40000
-    NEXT_BUYER_PRIVATE_KEY=leN7D7+9vr3TEZexVmvbYzdH1hbpwBvioc6y1c9Dhwr4ZaTkEWyX2Li5Ph/UFrw8QS8hAD9SQZkuVP6x14tEcqxWppmrvbdn
+    NEXT_BUYER_PRIVATE_KEY=${var.load_test_buyer_private_key}
     EOF
     sudo systemctl start app.service
   EOF1
@@ -990,7 +992,7 @@ module "load_test_sessions" {
     NUM_RELAYS=1000
     NUM_SESSIONS=50000
     SERVER_BACKEND_ADDRESS=${module.server_backend.address}:40000
-    NEXT_BUYER_PRIVATE_KEY=leN7D7+9vr3TEZexVmvbYzdH1hbpwBvioc6y1c9Dhwr4ZaTkEWyX2Li5Ph/UFrw8QS8hAD9SQZkuVP6x14tEcqxWppmrvbdn
+    NEXT_BUYER_PRIVATE_KEY=${var.load_test_buyer_private_key}
     EOF
     sudo systemctl start app.service
   EOF1
