3e575df53134902e92b550cbfa1537daaf7ae966
Glenn Fiedler <glenn@networknext.com>
2024-01-30 12:26:46 -0500

    ps5 client wip

--- sdk/build/ps5/client.cpp
+++ sdk/build/ps5/client.cpp
@@ -1,85 +1,51 @@
+/*
+    Network Next. Copyright © 2017 - 2024 Network Next, Inc.
 
-// Network Next PS5 Testbed
+    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following 
+    conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions 
+       and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote 
+       products derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+    IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+    OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #include "next.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <kernel.h>
 
-const char * buyer_public_key = "M/NxwbhSaPjUHES+kePTWD9TFA0bga1kubG+3vg0rTx/3sQoFgMB1w==";
+size_t sceLibcHeapSize = 100*1024*1024; // give 100MB heap to libc
 
-unsigned int sceLibcHeapExtendedAlloc = 1;
+extern void next_run_tests();
 
-size_t sceLibcHeapSize = SCE_LIBC_HEAP_SIZE_EXTENDED_ALLOC_NO_LIMIT;
-
-static volatile int quit = 0;
-
-void packet_received( next_client_t * client, void * context, const uint8_t * packet_data, int packet_bytes )
+int main()
 {
-    (void) client;
-    (void) context;
-    (void) packet_data;
-    (void) packet_bytes;
+    next_quiet( true );
 
-    // ...
-}
-
-int32_t main( int argc, const char * const argv[] )
-{
-    SceKernelModule next_library = sceKernelLoadStartModule( "/app0/next-ps5-4.0.16.prx", 0, NULL, 0, NULL, NULL );
-    if ( next_library < 0 )
+    if ( next_init( NULL, NULL ) != NEXT_OK )
     {
-        printf( "Failed to load next PRX library\n" );
+        printf( "error: failed to initialize network next\n" );
     }
 
-    next_log_level(NEXT_LOG_LEVEL_NONE);
+    printf( "\nRunning SDK tests:\n\n" );
 
-    next_config_t config;
-    next_default_config( &config );
-    strncpy_s( config.buyer_public_key, buyer_public_key, sizeof(config.buyer_public_key) - 1 );
-
-    next_init( NULL, &config );
-
-    printf( "\nRunning tests...\n\n" );
-
-    next_test();
-
-    printf( "\nAll tests passed successfully!\n\n" );
-
-    next_log_level( NEXT_LOG_LEVEL_INFO );
-
-    printf( "Starting client...\n\n" );
-    
-    next_client_t * client = next_client_create( NULL, "0.0.0.0:0", packet_received, NULL );
-    if ( !client )
-    {
-        printf( "error: failed to create network next client" );
-        exit( 1 );
-    }
-
-    next_client_open_session( client, "173.255.241.176:50000" );
-
-    while ( !quit )
-    {
-        next_client_update( client );
-
-        uint8_t packet_data[32];
-        memset( packet_data, 0, sizeof(packet_data) );
-        next_client_send_packet( client, packet_data, sizeof( packet_data ) );
-
-        next_platform_sleep( 1.0f / 60.0f );
-    }
-
-    printf( "\nShutting down...\n\n" );
-    
-    next_client_destroy( client );
+    next_run_tests();
 
     next_term();
 
-    sceKernelStopUnloadModule( next_library, 0, NULL, 0, NULL, NULL );
-
     printf( "\n" );
+
+    fflush( stdout );
 
     return 0;
 }
