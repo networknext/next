b1be30688dc16fa9de454bae4830f16cae58439a
Glenn Fiedler <glenn@networknext.com>
2024-03-03 12:26:59 -0500

    debug

--- relay/premake5.lua
+++ relay/premake5.lua
@@ -6,7 +6,6 @@ solution "relay"
 	warnings "Extra"
 	floatingpoint "Fast"
 	flags { "FatalWarnings" }
-	defines { "NEXT_DEVELOPMENT" }
 	filter "configurations:Debug"
 		symbols "On"
 		defines { "_DEBUG" }
@@ -16,6 +15,14 @@ solution "relay"
 		editandcontinue "Off"
 
 project "relay"
+	kind "ConsoleApp"
+	links { "sodium", "pthread" ,"curl" }
+	files { "*.cpp", "*.h" }
+	filter "system:macosx"
+		linkoptions { "-framework SystemConfiguration -framework CoreFoundation" }
+
+project "debug"
+	defines { "RELAY_DEBUG" }
 	kind "ConsoleApp"
 	links { "sodium", "pthread" ,"curl" }
 	files { "*.cpp", "*.h" }
