c01c603c63d96ce5fbcdd2927f964599709e3c08
Glenn Fiedler <glenn@networknext.com>
2025-02-13 12:44:50 -0500

    upgrade google cloud terraform provider from 5.x to 6.x. it's more reliable

--- terraform/staging/backend/main.tf
+++ terraform/staging/backend/main.tf
@@ -50,11 +50,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0.0"
+      version = "~> 6.0.0"
     }
     google-beta = {
       source = "hashicorp/google-beta"
-      version = "~> 5.0.0"
+      version = "~> 6.0.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
