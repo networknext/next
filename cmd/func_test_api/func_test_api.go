154328a101211c12cbec7233d136850a2fedabaf
Glenn Fiedler <glenn@networknext.com>
2025-02-22 11:57:28 -0500

    fix

--- cmd/func_test_api/func_test_api.go
+++ cmd/func_test_api/func_test_api.go
@@ -838,7 +838,7 @@ func test_relay() {
 
 	relayId := uint64(0)
 	{
-		relay := admin.RelayData{RelayName: "test", DatacenterId: datacenterId, PublicIP: "127.0.0.1", InternalIP: "0.0.0.0", SSH_IP: "127.0.0.1", PublicKeyBase64: "+ONHHci1bizkWzi4MTt1E5b0p0M5Xe0PhUay5H5KIl4="}
+		relay := admin.RelayData{RelayName: "test", DatacenterId: datacenterId, PublicIP: "127.0.0.1", InternalIP: "0.0.0.0", SSH_IP: "127.0.0.1", PublicKeyBase64: "+ONHHci1bizkWzi4MTt1E5b0p0M5Xe0PhUay5H5KIl4=", PrivateKeyBase64: "+ONHHci1bizkWzi4MTt1E5b0p0M5Xe0PhUay5H5KIl4="}
 
 		var response CreateRelayResponse
 
@@ -935,7 +935,7 @@ func test_relay() {
 
 	// update relay
 	{
-		relay := admin.RelayData{RelayId: relayId, RelayName: "Updated", DatacenterId: datacenterId, PublicIP: "127.0.0.1", InternalIP: "0.0.0.0", SSH_IP: "127.0.0.1", PublicKeyBase64: "+ONHHci1bizkWzi4MTt1E5b0p0M5Xe0PhUay5H5KIl4="}
+		relay := admin.RelayData{RelayId: relayId, RelayName: "Updated", DatacenterId: datacenterId, PublicIP: "127.0.0.1", InternalIP: "0.0.0.0", SSH_IP: "127.0.0.1", PublicKeyBase64: "+ONHHci1bizkWzi4MTt1E5b0p0M5Xe0PhUay5H5KIl4=", PrivateKeyBase64: "+ONHHci1bizkWzi4MTt1E5b0p0M5Xe0PhUay5H5KIl4="}
 
 		response := UpdateRelayResponse{}
 
