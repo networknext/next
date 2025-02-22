b72700544091611f0a0d84f55711e992ce0d9341
Glenn Fiedler <glenn@networknext.com>
2025-02-21 22:42:48 -0500

    modify set of akamai relays

--- terraform/dev/relays/main.tf
+++ terraform/dev/relays/main.tf
@@ -179,14 +179,20 @@ locals {
       image           = "linode/ubuntu22.04"
     },
 
-    "akamai.fremont" = {
-      datacenter_name = "akamai.fremont"
+    "akamai.dallas" = {
+      datacenter_name = "akamai.dallas"
       type            = "g6-dedicated-2"
       image           = "linode/ubuntu22.04"
     }
-    
-    "akamai.dallas" = {
-      datacenter_name = "akamai.dallas"
+
+    "akamai.miami" = {
+      datacenter_name = "akamai.miami"
+      type            = "g6-dedicated-2"
+      image           = "linode/ubuntu22.04"
+    }
+
+    "akamai.saopaulo" = {
+      datacenter_name = "akamai.saopaulo"
       type            = "g6-dedicated-2"
       image           = "linode/ubuntu22.04"
     }
