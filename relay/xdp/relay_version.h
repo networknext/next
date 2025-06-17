4e5fcc0754f0350c82345c97df0f7449fd29e5a8
Glenn Fiedler <glenn@networknext.com>
2025-06-17 17:17:39 -0400

    relay

--- relay/xdp/relay_version.h
+++ relay/xdp/relay_version.h
@@ -5,7 +5,7 @@
 #ifndef RELAY_VERSION_H
 #define RELAY_VERSION_H
 
-#define RELAY_VERSION                                        "1.0.8"
+#define RELAY_VERSION                                        "1.0.9"
 
 #define RELAY_VERSION_LENGTH                                      32
 
