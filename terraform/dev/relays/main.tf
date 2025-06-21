f92c02b4b37a70eca0a956ba7de314926f6850a6
Glenn Fiedler <glenn@networknext.com>
2025-06-20 21:31:16 -0400

    tear down dev

--- terraform/dev/relays/main.tf
+++ terraform/dev/relays/main.tf
@@ -169,25 +169,25 @@ locals {
 
     "akamai.newyork" = {
       datacenter_name = "akamai.newyork"
-      type            = "g6-dedicated-8"
+      type            = "g6-dedicated-4"
       image           = "linode/ubuntu22.04"
     },
 
     "akamai.atlanta" = {
       datacenter_name = "akamai.atlanta"
-      type            = "g6-dedicated-8"
+      type            = "g6-dedicated-4"
       image           = "linode/ubuntu22.04"
     },
 
     "akamai.fremont" = {
       datacenter_name = "akamai.fremont"
-      type            = "g6-dedicated-8"
+      type            = "g6-dedicated-4"
       image           = "linode/ubuntu22.04"
     }
     
     "akamai.dallas" = {
       datacenter_name = "akamai.dallas"
-      type            = "g6-dedicated-8"
+      type            = "g6-dedicated-4"
       image           = "linode/ubuntu22.04"
     }
 
