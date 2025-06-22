e1b0531e2cdad70606667c1808046eed5fe15b4b
Glenn Fiedler <glenn@networknext.com>
2025-06-21 22:48:54 -0400

    fix

--- relay/xdp/relay_main.c
+++ relay/xdp/relay_main.c
@@ -270,11 +270,11 @@ struct session_stats main_update_timeouts( struct main_t * main )
             int result = bpf_map_lookup_elem( main->stats_fd, &current_key, &current_value );
             if ( result == 0 )
             {
-                stats.envelope_kbps_up += current_value->envelope_kbps_up;
-                stats.envelope_kbps_down += current_value->envelope_kbps_down;
-                timed_out = current_value->expire_timestamp <= current_timestamp;
-                session_id = current_value->session_id;
-                session_version = current_value->session_version;
+                stats.envelope_kbps_up += current_value.envelope_kbps_up;
+                stats.envelope_kbps_down += current_value.envelope_kbps_down;
+                timed_out = current_value.expire_timestamp <= current_timestamp;
+                session_id = current_value.session_id;
+                session_version = current_value.session_version;
             }
 
             next_key_result = bpf_map_get_next_key( main->session_map_fd, &current_key, &next_key );
