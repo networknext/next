4da6307922b09f262e8151a8b423e356bb89996d
Glenn Fiedler <glenn@networknext.com>
2025-07-05 19:29:29 -0400

    again

--- portal/src/views/SessionsView.vue
+++ portal/src/views/SessionsView.vue
@@ -24,7 +24,7 @@ mess// -------------------------------------------------------------------------
       </table>
     </div>
 
-    <div class="d-none d-md-block">
+    <div class="d-none d-md-block d-xxl-none">
       <table id="sessions_table" class="table table-striped table-hover">
         <thead>
           <tr>
@@ -50,7 +50,7 @@ mess// -------------------------------------------------------------------------
       </table>
     </div>
 
-    <div class="d-none d-xl-block">
+    <div class="d-none d-xxl-block">
       <table id="sessions_table" class="table table-striped table-hover">
         <thead>
           <tr>
