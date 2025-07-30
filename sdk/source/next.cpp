30942f6cbae7ffd12512af648c1bfb79469069ac
Glenn Fiedler <glenn@networknext.com>
2025-07-30 15:34:17 -0400

    fix spurious spam logs

--- sdk/source/next.cpp
+++ sdk/source/next.cpp
@@ -103,7 +103,7 @@ void next_quiet( bool flag )
     log_quiet = flag;
 }
 
-static int log_level = NEXT_LOG_LEVEL_INFO;
+static int log_level = NEXT_LOG_LEVEL_SPAM;
 
 void next_log_level( int level )
 {
