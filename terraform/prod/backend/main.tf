dbc8b461aea7caaabe8645fab24fe70b2e14641b
Glenn Fiedler <glenn@networknext.com>
2025-07-31 18:05:55 -0400

    disable test server

--- terraform/prod/backend/main.tf
+++ terraform/prod/backend/main.tf
@@ -1097,6 +1097,7 @@ module "raspberry_client" {
 
 # ----------------------------------------------------------------------------------------
 
+/*
 resource "google_compute_address" "test_server_address" {
   name = "test-server-address"
   region = var.test_server_region
@@ -1158,5 +1159,6 @@ output "test_server_address" {
   description = "The IP address of the test server"
   value = "${google_compute_address.test_server_address.address}:30000"
 }
+*/
 
 # ----------------------------------------------------------------------------------------
