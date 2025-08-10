ff0d85aa1a78acfa6c75ddd3caa622da3fa15c1c
Glenn Fiedler <glenn@networknext.com>
2025-08-10 14:22:31 -0400

    remove debug log

--- relay/xdp/relay.c
+++ relay/xdp/relay.c
@@ -73,9 +73,6 @@ int main( int argc, char *argv[] )
 
     printf( "Network Next Relay (release)\n" );
 
-    // todo
-    printf( "(With RTT=0 fix)\n" );
-
     fflush( stdout );
 
     signal( SIGINT,  interrupt_handler );
