3354bfe11aca264fd8f22e5abc258aa483b78454
Glenn Fiedler <glenn@networknext.com>
2025-09-05 11:24:17 -0400

    bring back regular server post-hang loop test last night

--- cmd/server/server.cpp
+++ cmd/server/server.cpp
@@ -36,11 +36,8 @@ int main()
 
     next_init( NULL, NULL ); 
 
-    srand( time(NULL) );
-
-    double restart_time = next_platform_time() + 1.0 * ( double(rand()) / double(RAND_MAX) );
-
     next_server_t * server = next_server_create( NULL, "127.0.0.1:30000", "0.0.0.0:30000", "local", server_packet_received );
+
     if ( server == NULL )
     {
         printf( "error: failed to create server\n" );
@@ -51,27 +48,21 @@ int main()
     {
         next_server_update( server );
 
-        next_platform_sleep( 0.01 );
-
-        if ( next_platform_time() >= restart_time )
+        if ( next_server_direct_only( server ) )
         {
-            next_printf( NEXT_LOG_LEVEL_INFO, "restarting server" );
-
-            next_server_destroy( server );
-
-            server = next_server_create( NULL, "127.0.0.1:30000", "0.0.0.0:30000", "local", server_packet_received );
-            if ( server == NULL )
-            {
-                printf( "error: failed to create server\n" );
-                return 1;
-            }
-
-            restart_time = next_platform_time() + 1.0 * ( double(rand()) / double(RAND_MAX) );
+            next_printf( NEXT_LOG_LEVEL_INFO, "detected server is in direct only mode. restarting..." );
+            break;
         }
+
+        next_platform_sleep( 0.001 );
+
+        fflush( stdout );
     }
+
+    next_server_flush( server );
     
     next_server_destroy( server );
-
+    
     next_term();
 
     return 0;
