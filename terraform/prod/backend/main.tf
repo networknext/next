d61d38c492e19c849f7f89041e0f1e95e4d4f959
Glenn Fiedler <glenn@networknext.com>
2025-09-15 11:57:47 -0400

    back to 1000 buckets

--- terraform/prod/backend/main.tf
+++ terraform/prod/backend/main.tf
@@ -823,7 +823,7 @@ module "session_cruncher" {
     sudo ./bootstrap.sh -t ${var.tag} -b ${local.google_artifacts_bucket} -a session_cruncher.tar.gz
     cat <<EOF > /app/app.env
     ENV=prod
-    NUM_BUCKETS=100
+    NUM_BUCKETS=1000
     ENABLE_REDIS_TIME_SERIES=true
     REDIS_TIME_SERIES_HOSTNAME="${module.redis_time_series.address}:6379"
     GOOGLE_PROJECT_ID=${local.google_project_id}
@@ -864,7 +864,7 @@ module "server_cruncher" {
     sudo ./bootstrap.sh -t ${var.tag} -b ${local.google_artifacts_bucket} -a server_cruncher.tar.gz
     cat <<EOF > /app/app.env
     ENV=prod
-    NUM_BUCKETS=100
+    NUM_BUCKETS=1000
     EOF
     sudo systemctl start app.service
   EOF1
@@ -899,7 +899,7 @@ module "server_backend" {
     sudo ./bootstrap.sh -t ${var.tag} -b ${local.google_artifacts_bucket} -a server_backend.tar.gz
     cat <<EOF > /app/app.env
     ENV=prod
-    NUM_BUCKETS=100
+    NUM_BUCKETS=1000
     UDP_PORT=40000
     UDP_BIND_ADDRESS="##########:40000"
     UDP_NUM_THREADS=8
