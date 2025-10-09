7b95a23379ba5197b093e2f8c32da780e173de23
Glenn Fiedler <glenn@networknext.com>
2025-10-09 19:32:41 -0400

    aggressive

--- portal/src/views/SessionsView.vue
+++ portal/src/views/SessionsView.vue
@@ -30,8 +30,8 @@
           <tr>
             <th>Session ID</th>
             <th>Country</th>
-            <th class="right_align">Direct RTT</th>
-            <th class="right_align">Accelerated RTT</th>
+            <th>Platform</th>
+            <th>Connection</th>
             <th class="right_align">Improvement</th>
           </tr>
         </thead>
@@ -39,8 +39,8 @@
           <tr v-for="item in data" :key='item'>
             <td class="fixed"> <router-link :to='"/session/" + item["Session ID"]'> {{ item["Session ID"] }} </router-link> </td>
             <td> {{ item["Country"] }} </td>
-            <td class="right_align"> {{ item["Direct RTT"] }} </td>
-            <td class="right_align"> {{ item["Accelerated RTT"] }} </td>
+            <td> {{ item["Platform"] }} </td>
+            <td> {{ item["Connection"] }} </td>
             <td class="green" v-if="item['Improvement'] != '--' && item['Improvement'] >= 10"> {{ item["Improvement"] }} ms</td>
             <td class="orange" v-else-if="item['Improvement'] != '--' && item['Improvement'] >= 5"> {{ item["Improvement"] }} ms</td>
             <td class="red" v-else-if="item['Improvement'] != '--' && item['Improvement'] > 0"> {{ item["Improvement"] }} ms</td>
@@ -88,8 +88,6 @@
             <th>Connection</th>
             <th>ISP</th>
             <th>Datacenter</th>
-            <th class="right_align">Direct RTT</th>
-            <th class="right_align">Accelerated RTT</th>
             <th class="right_align">Improvement</th>
           </tr>
         </thead>
